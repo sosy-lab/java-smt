749ee1c48db33bf1e6fe8069f75d076e4fac7ebf
George Karpenkov <george@metaworld.me>
2015-12-18 14:06:16 +0100

    Fix findbugs warnings.

--- src/org/sosy_lab/solver/basicimpl/tactics/NNFVisitor.java
+++ src/org/sosy_lab/solver/basicimpl/tactics/NNFVisitor.java
@@ -13,8 +13,6 @@ import java.util.List;
 import java.util.Map;
 
 class NNFVisitor extends BooleanFormulaTransformationVisitor {
-  boolean insideNot = false;
-
   private final NNFInsideNotVisitor insideNotVisitor;
   private final BooleanFormulaManager bfmgr;
 
