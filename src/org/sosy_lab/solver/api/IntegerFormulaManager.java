22a72dccf45770f6e65021bdf18fe461b0423001
George Karpenkov <george@metaworld.me>
2016-01-06 15:21:29 +0100

    The dangers of forgetting to add untracked files.

---
+++ src/org/sosy_lab/solver/api/IntegerFormulaManager.java
@@ -0,0 +1,12 @@
+package org.sosy_lab.solver.api;
+
+import org.sosy_lab.solver.api.NumeralFormula.IntegerFormula;
+
+/**
+ * Interface which operates over {@link IntegerFormula}s.
+ *
+ * Integer formulas always take integral formulas as arguments.
+ */
+public interface IntegerFormulaManager
+  extends NumeralFormulaManager<IntegerFormula, IntegerFormula> {
+}
---
+++ src/org/sosy_lab/solver/api/RationalFormulaManager.java
@@ -0,0 +1,12 @@
+package org.sosy_lab.solver.api;
+
+import org.sosy_lab.solver.api.NumeralFormula.RationalFormula;
+
+/**
+ * Interface for operating over {@link RationalFormula}.
+ *
+ * Rational formulas may take both integral and rational formulas as arguments.
+ */
+public interface RationalFormulaManager extends
+  NumeralFormulaManager<NumeralFormula, RationalFormula> {
+}
