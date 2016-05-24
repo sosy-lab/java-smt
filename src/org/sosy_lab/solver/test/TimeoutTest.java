bebd1e19cc6d67c59aa5326c589336513e709635
George Karpenkov <george@metaworld.me>
2016-05-24 16:04:25 +0200

    Tests that solvers can be interrupted.

--- src/org/sosy_lab/solver/test/TimeoutTest.java
+++ src/org/sosy_lab/solver/test/TimeoutTest.java
@@ -25,7 +25,6 @@ package org.sosy_lab.solver.test;
 
 import com.google.common.truth.TruthJUnit;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -34,21 +33,19 @@ import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
 import org.sosy_lab.solver.SolverContextFactory.Solvers;
+import org.sosy_lab.solver.api.BasicProverEnvironment;
 import org.sosy_lab.solver.api.BooleanFormula;
 import org.sosy_lab.solver.basicimpl.Fuzzer;
 import org.sosy_lab.solver.basicimpl.tactics.Tactic;
+
+import java.util.Random;
+import java.util.function.Supplier;
 
 /**
  * Check that timeout is handled gracefully.
  */
 @RunWith(Parameterized.class)
 public class TimeoutTest extends SolverBasedTest0 {
-  private Fuzzer fuzzer;
-
-  @Before
-  public void setUp() {
-    fuzzer = new Fuzzer(bmgr);
-  }
 
   @Parameters(name = "{0}")
   public static Object[] getAllSolvers() {
@@ -71,11 +68,50 @@ public class TimeoutTest extends SolverBasedTest0 {
         .withFailureMessage("Only Z3 has native tactics")
         .that(solverToUse())
         .isAnyOf(Solvers.Z3, Solvers.Z3JAVA);
+    Fuzzer fuzzer = new Fuzzer(bmgr, new Random(0));
     String msg = "ShutdownRequest";
     expectedEx.expect(InterruptedException.class);
     expectedEx.expectMessage(msg);
     BooleanFormula test = fuzzer.fuzz(20, 3);
     shutdownManager.requestShutdown(msg);
     mgr.applyTactic(test, Tactic.NNF);
+  }
+
+  @Test
+  public void testProverTimeout() throws Exception {
+    TruthJUnit.assume()
+        .withFailureMessage("Princess does not support interruption")
+        .that(solverToUse()).isNotEqualTo(Solvers.PRINCESS);
+    testBasicProverTimeout(() -> context.newProverEnvironment());
+  }
+
+  @Test
+  public void testInterpolationProverTimeout() throws Exception {
+    TruthJUnit.assume()
+        .withFailureMessage("Princess does not support interruption")
+        .that(solverToUse()).isNotEqualTo(Solvers.PRINCESS);
+    testBasicProverTimeout(() -> context.newProverEnvironmentWithInterpolation());
+  }
+
+  @SuppressWarnings("CheckReturnValue")
+  private void testBasicProverTimeout(Supplier<BasicProverEnvironment<?>> proverConstructor)
+      throws Exception {
+    Fuzzer f = new Fuzzer(bmgr, new Random(0));
+    expectedEx.expect(InterruptedException.class);
+    Thread t = new Thread() {
+      public void run() {
+        try {
+          sleep(1);
+          shutdownManager.requestShutdown("Shutdown Request");
+        } catch (InterruptedException pE) {
+          throw new UnsupportedOperationException("Unexpected interrupt");
+        }
+      }
+    };
+    try (BasicProverEnvironment<?> pe = proverConstructor.get()) {
+      pe.push(f.fuzz(10000, 100));
+      t.start();
+      pe.isUnsat();
+    }
   }
 }
