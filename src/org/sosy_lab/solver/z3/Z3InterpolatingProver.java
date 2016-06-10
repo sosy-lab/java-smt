sosy-lab/java-smt

commit 19a6fe1a34eb105a4384193ec7f7a6c3a8674dc7

Use Z3 JNI bindings instead of our own ones.
This gives us the best of both worlds: optimal performance and not
having a technical debt of having to update the JNI generation script
each time Z3 API changes.
Their constant declarations are better as well, done as proper Java
enums.
A logical next step is to get rid of libz3j.so altogether, and of the
python script used to generate it.

src/org/sosy_lab/solver/z3/Z3InterpolatingProver.java
@@ -19,37 +19,18 @@
  */
 package org.sosy_lab.solver.z3;
 
-import static org.sosy_lab.solver.z3.Z3NativeApi.ast_vector_get;
-import static org.sosy_lab.solver.z3.Z3NativeApi.dec_ref;
-import static org.sosy_lab.solver.z3.Z3NativeApi.get_interpolant;
-import static org.sosy_lab.solver.z3.Z3NativeApi.inc_ref;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_and;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_interpolant;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_params;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_solver;
-import static org.sosy_lab.solver.z3.Z3NativeApi.solver_assert;
-import static org.sosy_lab.solver.z3.Z3NativeApi.solver_check;
-import static org.sosy_lab.solver.z3.Z3NativeApi.solver_dec_ref;
-import static org.sosy_lab.solver.z3.Z3NativeApi.solver_get_model;
-import static org.sosy_lab.solver.z3.Z3NativeApi.solver_get_num_scopes;
-import static org.sosy_lab.solver.z3.Z3NativeApi.solver_get_proof;
-import static org.sosy_lab.solver.z3.Z3NativeApi.solver_inc_ref;
-import static org.sosy_lab.solver.z3.Z3NativeApi.solver_pop;
-import static org.sosy_lab.solver.z3.Z3NativeApi.solver_push;
-import static org.sosy_lab.solver.z3.Z3NativeApi.solver_set_params;
-import static org.sosy_lab.solver.z3.Z3NativeApi.solver_to_string;
-
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.google.common.primitives.Longs;
+import com.microsoft.z3.Native;
+import com.microsoft.z3.enumerations.Z3_lbool;
 
 import org.sosy_lab.common.ShutdownNotifier;
 import org.sosy_lab.solver.api.BooleanFormula;
 import org.sosy_lab.solver.api.InterpolatingProverEnvironment;
-import org.sosy_lab.solver.z3.Z3NativeApiConstants.Z3_LBOOL;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
@@ -68,29 +49,29 @@ class Z3InterpolatingProver extends Z3AbstractProver<Long>
   Z3InterpolatingProver(
       Z3FormulaCreator creator, long z3params, ShutdownNotifier pShutdownNotifier) {
     super(creator, pShutdownNotifier);
-    this.z3solver = mk_solver(z3context);
-    solver_inc_ref(z3context, z3solver);
-    solver_set_params(z3context, z3solver, z3params);
+    this.z3solver = Native.mkSolver(z3context);
+    Native.solverIncRef(z3context, z3solver);
+    Native.solverSetParams(z3context, z3solver, z3params);
   }
 
   @Override
   public void pop() {
     Preconditions.checkState(!closed);
-    Preconditions.checkState(solver_get_num_scopes(z3context, z3solver) >= 1);
+    Preconditions.checkState(Native.solverGetNumScopes(z3context, z3solver) >= 1);
     level--;
 
     assertedFormulas.removeLast();
-    solver_pop(z3context, z3solver, 1);
+    Native.solverPop(z3context, z3solver, 1);
   }
 
   @Override
   public Long addConstraint(BooleanFormula f) {
     Preconditions.checkState(!closed);
     long e = creator.extractInfo(f);
-    inc_ref(z3context, e);
-    solver_assert(z3context, z3solver, e);
+    Native.incRef(z3context, e);
+    Native.solverAssert(z3context, z3solver, e);
     assertedFormulas.addLast(e);
-    dec_ref(z3context, e);
+    Native.decRef(z3context, e);
     return e;
   }
 
@@ -98,16 +79,16 @@ class Z3InterpolatingProver extends Z3AbstractProver<Long>
   public void push() {
     Preconditions.checkState(!closed);
     level++;
-    solver_push(z3context, z3solver);
+    Native.solverPush(z3context, z3solver);
   }
 
   @Override
   public boolean isUnsat() throws Z3SolverException, InterruptedException {
     Preconditions.checkState(!closed);
-    int result = solver_check(z3context, z3solver);
+    int result = Native.solverCheck(z3context, z3solver);
     shutdownNotifier.shutdownIfNecessary();
-    Preconditions.checkState(result != Z3_LBOOL.Z3_L_UNDEF.status);
-    return result == Z3_LBOOL.Z3_L_FALSE.status;
+    Preconditions.checkState(result != Z3_lbool.Z3_L_UNDEF.toInt());
+    return result == Z3_lbool.Z3_L_FALSE.toInt();
   }
 
   @Override
@@ -127,7 +108,7 @@ class Z3InterpolatingProver extends Z3AbstractProver<Long>
     // binary interpolant is a sequence interpolant of only 2 elements
     return Iterables.getOnlyElement(
         getSeqInterpolants(
-            ImmutableList.<Set<Long>>of(
+            ImmutableList.of(
                 Sets.newHashSet(formulasOfA), Sets.newHashSet(formulasOfB))));
   }
 
@@ -150,8 +131,11 @@ class Z3InterpolatingProver extends Z3AbstractProver<Long>
 
     // build conjunction of each partition
     for (int i = 0; i < partitionedFormulas.size(); i++) {
-      long conjunction = mk_and(z3context, Longs.toArray(partitionedFormulas.get(i)));
-      inc_ref(z3context, conjunction);
+      long conjunction = Native.mkAnd(
+          z3context,
+          partitionedFormulas.get(i).size(),
+          Longs.toArray(partitionedFormulas.get(i)));
+      Native.incRef(z3context, conjunction);
       conjunctionFormulas[i] = conjunction;
     }
 
@@ -175,7 +159,7 @@ class Z3InterpolatingProver extends Z3AbstractProver<Long>
           children.add(0, stack.pollLast().getInterpolationPoint());
         }
         children.add(conjunctionFormulas[i]); // add the node itself
-        conjunction = mk_and(z3context, Longs.toArray(children));
+        conjunction = Native.mkAnd(z3context, 2, Longs.toArray(children));
       }
 
       final long interpolationPoint;
@@ -185,10 +169,10 @@ class Z3InterpolatingProver extends Z3AbstractProver<Long>
         Preconditions.checkState(currentSubtree == 0, "subtree of root should start at 0.");
         Preconditions.checkState(stack.isEmpty(), "root should be the last element in the stack.");
       } else {
-        interpolationPoint = mk_interpolant(z3context, conjunction);
+        interpolationPoint = Native.mkInterpolant(z3context, conjunction);
       }
 
-      inc_ref(z3context, interpolationPoint);
+      Native.incRef(z3context, interpolationPoint);
       interpolationFormulas[i] = interpolationPoint;
       stack.addLast(new Z3TreeInterpolant(currentSubtree, interpolationPoint));
       lastSubtree = currentSubtree;
@@ -200,15 +184,15 @@ class Z3InterpolatingProver extends Z3AbstractProver<Long>
     Preconditions.checkState(
         stack.isEmpty(), "root should have been the last element in the stack.");
 
-    final long proof = solver_get_proof(z3context, z3solver);
-    inc_ref(z3context, proof);
+    final long proof = Native.solverGetProof(z3context, z3solver);
+    Native.incRef(z3context, proof);
 
     long interpolationResult =
-        get_interpolant(
+        Native.getInterpolant(
             z3context,
             proof, //refutation of premises := proof
             root, // last element is end of chain (root of tree), pattern := interpolation tree
-            mk_params(z3context));
+            Native.mkParams(z3context));
 
     shutdownNotifier.shutdownIfNecessary();
 
@@ -217,16 +201,17 @@ class Z3InterpolatingProver extends Z3AbstractProver<Long>
     // so we only need to copy them
     final List<BooleanFormula> result = new ArrayList<>();
     for (int i = 0; i < partitionedFormulas.size() - 1; i++) {
-      result.add(creator.encapsulateBoolean(ast_vector_get(z3context, interpolationResult, i)));
+      result.add(creator.encapsulateBoolean(Native.astVectorGet(z3context, interpolationResult,
+          i)));
     }
 
     // cleanup
-    dec_ref(z3context, proof);
+    Native.decRef(z3context, proof);
     for (long partition : conjunctionFormulas) {
-      dec_ref(z3context, partition);
+      Native.decRef(z3context, partition);
     }
     for (long partition : interpolationFormulas) {
-      dec_ref(z3context, partition);
+      Native.decRef(z3context, partition);
     }
 
     return result;
@@ -234,7 +219,7 @@ class Z3InterpolatingProver extends Z3AbstractProver<Long>
 
   @Override
   protected long getZ3Model() {
-    return solver_get_model(z3context, z3solver);
+    return Native.solverGetModel(z3context, z3solver);
   }
 
   @Override
@@ -247,7 +232,7 @@ class Z3InterpolatingProver extends Z3AbstractProver<Long>
     assertedFormulas.clear();
 
     //TODO solver_reset(z3context, z3solver);
-    solver_dec_ref(z3context, z3solver);
+    Native.solverDecRef(z3context, z3solver);
     closed = true;
   }
 
@@ -272,6 +257,6 @@ class Z3InterpolatingProver extends Z3AbstractProver<Long>
   @Override
   public String toString() {
     Preconditions.checkState(!closed);
-    return solver_to_string(z3context, z3solver);
+    return Native.solverToString(z3context, z3solver);
   }
 }

src/org/sosy_lab/solver/z3java/Z3InterpolatingProver.java
@@ -113,7 +113,7 @@ class Z3InterpolatingProver extends Z3AbstractProver<Expr>
     // calc difference: formulasOfB := assertedFormulas - formulasOfA
     // we have to handle equal formulas on the stack,
     // so we copy the whole stack and remove the formulas of A once.
-    final List<Expr> formulasOfB = Lists.<Expr>newLinkedList(assertedFormulas);
+    final List<Expr> formulasOfB = Lists.newLinkedList(assertedFormulas);
     for (Expr af : formulasOfA) {
       boolean check = formulasOfB.remove(af); // remove only first occurrence
       assert check : "formula from A must be part of all asserted formulas";
@@ -122,7 +122,7 @@ class Z3InterpolatingProver extends Z3AbstractProver<Expr>
     // binary interpolant is a sequence interpolant of only 2 elements
     return Iterables.getOnlyElement(
         getSeqInterpolants(
-            ImmutableList.<Set<Expr>>of(
+            ImmutableList.of(
                 Sets.newHashSet(formulasOfA), Sets.newHashSet(formulasOfB))));
   }
 

src/org/sosy_lab/solver/api/FunctionDeclarationKind.java
@@ -37,6 +37,12 @@ public enum FunctionDeclarationKind {
 
   // Simple arithmetic,
   // they work across integers and rationals.
+
+  /**
+   * Unary minus.
+   */
+  UMINUS,
+
   /**
    * Subtraction over integers and rationals.
    */

src/org/sosy_lab/solver/z3/Z3ArrayFormulaManager.java
@@ -19,7 +19,7 @@
  */
 package org.sosy_lab.solver.z3;
 
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_eq;
+import com.microsoft.z3.Native;
 
 import org.sosy_lab.solver.api.Formula;
 import org.sosy_lab.solver.api.FormulaType;
@@ -37,12 +37,12 @@ class Z3ArrayFormulaManager extends AbstractArrayFormulaManager<Long, Long, Long
 
   @Override
   protected Long select(Long pArray, Long pIndex) {
-    return Z3NativeApi.mk_select(z3context, pArray, pIndex);
+    return Native.mkSelect(z3context, pArray, pIndex);
   }
 
   @Override
   protected Long store(Long pArray, Long pIndex, Long pValue) {
-    return Z3NativeApi.mk_store(z3context, pArray, pIndex, pValue);
+    return Native.mkStore(z3context, pArray, pIndex, pValue);
   }
 
   @Override
@@ -58,6 +58,6 @@ class Z3ArrayFormulaManager extends AbstractArrayFormulaManager<Long, Long, Long
 
   @Override
   protected Long equivalence(Long pArray1, Long pArray2) {
-    return mk_eq(z3context, pArray1, pArray2);
+    return Native.mkEq(z3context, pArray1, pArray2);
   }
 }

src/org/sosy_lab/solver/z3/Z3BitvectorFormulaManager.java
@@ -19,34 +19,7 @@
  */
 package org.sosy_lab.solver.z3;
 
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_bv_sort;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_bvadd;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_bvand;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_bvashr;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_bvlshr;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_bvmul;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_bvneg;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_bvnot;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_bvor;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_bvsdiv;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_bvshl;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_bvsle;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_bvslt;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_bvsrem;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_bvsub;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_bvudiv;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_bvule;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_bvult;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_bvurem;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_bvxor;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_concat;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_eq;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_extract;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_int64;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_numeral;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_sign_ext;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_true;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_zero_ext;
+import com.microsoft.z3.Native;
 
 import org.sosy_lab.solver.basicimpl.AbstractBitvectorFormulaManager;
 
@@ -63,33 +36,33 @@ class Z3BitvectorFormulaManager extends AbstractBitvectorFormulaManager<Long, Lo
 
   @Override
   public Long concat(Long pFirst, Long pSecond) {
-    return mk_concat(z3context, pFirst, pSecond);
+    return Native.mkConcat(z3context, pFirst, pSecond);
   }
 
   @Override
   public Long extract(Long pFirst, int pMsb, int pLsb, boolean pSigned) {
-    return mk_extract(z3context, pMsb, pLsb, pFirst);
+    return Native.mkExtract(z3context, pMsb, pLsb, pFirst);
   }
 
   @Override
   public Long extend(Long pNumber, int pExtensionBits, boolean pSigned) {
     if (pSigned) {
-      return mk_sign_ext(z3context, pExtensionBits, pNumber);
+      return Native.mkSignExt(z3context, pExtensionBits, pNumber);
     } else {
-      return mk_zero_ext(z3context, pExtensionBits, pNumber);
+      return Native.mkZeroExt(z3context, pExtensionBits, pNumber);
     }
   }
 
   @Override
   public Long makeBitvectorImpl(int pLength, long pI) {
-    long sort = mk_bv_sort(z3context, pLength);
-    return mk_int64(z3context, pI, sort);
+    long sort = Native.mkBvSort(z3context, pLength);
+    return Native.mkInt64(z3context, pI, sort);
   }
 
   @Override
   protected Long makeBitvectorImpl(int pLength, BigInteger pI) {
-    long sort = mk_bv_sort(z3context, pLength);
-    return mk_numeral(z3context, pI.toString(), sort);
+    long sort = Native.mkBvSort(z3context, pLength);
+    return Native.mkNumeral(z3context, pI.toString(), sort);
   }
 
   @Override
@@ -104,100 +77,100 @@ class Z3BitvectorFormulaManager extends AbstractBitvectorFormulaManager<Long, Lo
   @Override
   public Long shiftRight(Long number, Long toShift, boolean signed) {
     if (signed) {
-      return mk_bvashr(z3context, number, toShift);
+      return Native.mkBvashr(z3context, number, toShift);
     } else {
-      return mk_bvlshr(z3context, number, toShift);
+      return Native.mkBvlshr(z3context, number, toShift);
     }
   }
 
   @Override
   public Long shiftLeft(Long number, Long toShift) {
-    return mk_bvshl(z3context, number, toShift);
+    return Native.mkBvshl(z3context, number, toShift);
   }
 
   @Override
   public Long not(Long pBits) {
-    return mk_bvnot(z3context, pBits);
+    return Native.mkBvnot(z3context, pBits);
   }
 
   @Override
   public Long and(Long pBits1, Long pBits2) {
-    return mk_bvand(z3context, pBits1, pBits2);
+    return Native.mkBvand(z3context, pBits1, pBits2);
   }
 
   @Override
   public Long or(Long pBits1, Long pBits2) {
-    return mk_bvor(z3context, pBits1, pBits2);
+    return Native.mkBvor(z3context, pBits1, pBits2);
   }
 
   @Override
   public Long xor(Long pBits1, Long pBits2) {
-    return mk_bvxor(z3context, pBits1, pBits2);
+    return Native.mkBvxor(z3context, pBits1, pBits2);
   }
 
   @Override
   public Long negate(Long pNumber) {
-    return mk_bvneg(z3context, pNumber);
+    return Native.mkBvneg(z3context, pNumber);
   }
 
   @Override
   public Long add(Long pNumber1, Long pNumber2) {
-    return mk_bvadd(z3context, pNumber1, pNumber2);
+    return Native.mkBvadd(z3context, pNumber1, pNumber2);
   }
 
   @Override
   public Long subtract(Long pNumber1, Long pNumber2) {
-    return mk_bvsub(z3context, pNumber1, pNumber2);
+    return Native.mkBvsub(z3context, pNumber1, pNumber2);
   }
 
   @Override
   public Long divide(Long pNumber1, Long pNumber2, boolean signed) {
     if (signed) {
-      return mk_bvsdiv(z3context, pNumber1, pNumber2);
+      return Native.mkBvsdiv(z3context, pNumber1, pNumber2);
     } else {
-      return mk_bvudiv(z3context, pNumber1, pNumber2);
+      return Native.mkBvudiv(z3context, pNumber1, pNumber2);
     }
   }
 
   @Override
   public Long modulo(Long pNumber1, Long pNumber2, boolean signed) {
     if (signed) {
-      return mk_bvsrem(z3context, pNumber1, pNumber2);
+      return Native.mkBvsrem(z3context, pNumber1, pNumber2);
     } else {
-      return mk_bvurem(z3context, pNumber1, pNumber2);
+      return Native.mkBvurem(z3context, pNumber1, pNumber2);
     }
   }
 
   @Override
   protected Long modularCongruence(Long pNumber1, Long pNumber2, long pModulo) {
-    return mk_true(z3context);
+    return Native.mkTrue(z3context);
   }
 
   @Override
   public Long multiply(Long pNumber1, Long pNumber2) {
-    return mk_bvmul(z3context, pNumber1, pNumber2);
+    return Native.mkBvmul(z3context, pNumber1, pNumber2);
   }
 
   @Override
   public Long equal(Long pNumber1, Long pNumber2) {
-    return mk_eq(z3context, pNumber1, pNumber2);
+    return Native.mkEq(z3context, pNumber1, pNumber2);
   }
 
   @Override
   public Long lessThan(Long pNumber1, Long pNumber2, boolean signed) {
     if (signed) {
-      return mk_bvslt(z3context, pNumber1, pNumber2);
+      return Native.mkBvslt(z3context, pNumber1, pNumber2);
     } else {
-      return mk_bvult(z3context, pNumber1, pNumber2);
+      return Native.mkBvult(z3context, pNumber1, pNumber2);
     }
   }
 
   @Override
   public Long lessOrEquals(Long pNumber1, Long pNumber2, boolean signed) {
     if (signed) {
-      return mk_bvsle(z3context, pNumber1, pNumber2);
+      return Native.mkBvsle(z3context, pNumber1, pNumber2);
     } else {
-      return mk_bvule(z3context, pNumber1, pNumber2);
+      return Native.mkBvule(z3context, pNumber1, pNumber2);
     }
   }
 

src/org/sosy_lab/solver/z3/Z3BooleanFormulaManager.java
@@ -19,20 +19,11 @@
  */
 package org.sosy_lab.solver.z3;
 
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_and;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_eq;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_false;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_implies;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_ite;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_not;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_or;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_true;
-import static org.sosy_lab.solver.z3.Z3NativeApi.mk_xor;
-import static org.sosy_lab.solver.z3.Z3NativeApiConstants.Z3_OP_FALSE;
-import static org.sosy_lab.solver.z3.Z3NativeApiConstants.Z3_OP_TRUE;
-import static org.sosy_lab.solver.z3.Z3NativeApiConstants.isOP;
+import static org.sosy_lab.solver.z3.Z3FormulaCreator.isOP;
 
 import com.google.common.primitives.Longs;
+import com.microsoft.z3.Native;
+import com.microsoft.z3.enumerations.Z3_decl_kind;
 
 import org.sosy_lab.solver.basicimpl.AbstractBooleanFormulaManager;
 
@@ -56,64 +47,64 @@ class Z3BooleanFormulaManager extends AbstractBooleanFormulaManager<Long, Long, 
   @Override
   protected Long makeBooleanImpl(boolean pValue) {
     if (pValue) {
-      return mk_true(z3context);
+      return Native.mkTrue(z3context);
     } else {
-      return mk_false(z3context);
+      return Native.mkFalse(z3context);
     }
   }
 
   @Override
   protected Long not(Long pParam) {
-    return mk_not(z3context, pParam);
+    return Native.mkNot(z3context, pParam);
   }
 
   @Override
   protected Long and(Long pParam1, Long pParam2) {
-    return mk_and(z3context, pParam1, pParam2);
+    return Native.mkAnd(z3context, 2, new long[]{pParam1, pParam2});
   }
 
   @Override
   protected Long or(Long pParam1, Long pParam2) {
-    return mk_or(z3context, pParam1, pParam2);
+    return Native.mkOr(z3context, 2, new long[]{pParam1, pParam2});
   }
 
   @Override
   protected Long orImpl(Collection<Long> params) {
-    return mk_or(z3context, params.size(), Longs.toArray(params));
+    return Native.mkOr(z3context, params.size(), Longs.toArray(params));
   }
 
   @Override
   protected Long andImpl(Collection<Long> params) {
-    return mk_and(z3context, params.size(), Longs.toArray(params));
+    return Native.mkAnd(z3context, params.size(), Longs.toArray(params));
   }
 
   @Override
   protected Long xor(Long pParam1, Long pParam2) {
-    return mk_xor(z3context, pParam1, pParam2);
+    return Native.mkXor(z3context, pParam1, pParam2);
   }
 
   @Override
   protected Long equivalence(Long pBits1, Long pBits2) {
-    return mk_eq(z3context, pBits1, pBits2);
+    return Native.mkEq(z3context, pBits1, pBits2);
   }
 
   @Override
   protected Long implication(Long pBits1, Long pBits2) {
-    return mk_implies(z3context, pBits1, pBits2);
+    return Native.mkImplies(z3context, pBits1, pBits2);
   }
 
   @Override
   protected boolean isTrue(Long pParam) {
-    return isOP(z3context, pParam, Z3_OP_TRUE);
+    return isOP(z3context, pParam, Z3_decl_kind.Z3_OP_TRUE.toInt());
   }
 
   @Override
   protected boolean isFalse(Long pParam) {
-    return isOP(z3context, pParam, Z3_OP_FALSE);
+    return isOP(z3context, pParam, Z3_decl_kind.Z3_OP_FALSE.toInt());
   }
 
   @Override
   protected Long ifThenElse(Long pCond, Long pF1, Long pF2) {
-    return mk_ite(z3context, pCond, pF1, pF2);
+    return Native.mkIte(z3context, pCond, pF1, pF2);
   }
 }

commit 03cfa0cf92bcb81c6518e0eb10b4dff4851cb290

bugfix for Z3-tree-interpolation,
Bug was introduced in 19a6fe1a34eb105a4384193ec7f7a6c3a8674dc7 .

src/org/sosy_lab/solver/z3/Z3InterpolatingProver.java
@@ -165,7 +165,7 @@ class Z3InterpolatingProver extends Z3AbstractProver<Long>
           children.add(0, stack.pollLast().getInterpolationPoint());
         }
         children.add(conjunctionFormulas[i]); // add the node itself
-        conjunction = Native.mkAnd(z3context, 2, Longs.toArray(children));
+        conjunction = Native.mkAnd(z3context, children.size(), Longs.toArray(children));
       }
 
       final long interpolationPoint;