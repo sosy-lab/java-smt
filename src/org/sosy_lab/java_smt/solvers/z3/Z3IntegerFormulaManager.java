71679f3eb41b5681de25de225507f48825ea2d82
George Karpenkov <george@metaworld.me>
2016-08-26 19:17:49 +0200

    Add forgotten intermediate incRefs to Z3#modularCongruence
    In corner cases they lead to very obscure difficult-to-debug errors.

--- src/org/sosy_lab/java_smt/solvers/z3/Z3IntegerFormulaManager.java
+++ src/org/sosy_lab/java_smt/solvers/z3/Z3IntegerFormulaManager.java
@@ -55,10 +55,29 @@ class Z3IntegerFormulaManager extends Z3NumeralFormulaManager<IntegerFormula, In
 
   @Override
   protected Long modularCongruence(Long pNumber1, Long pNumber2, long pModulo) {
-    // ((_ divisible n) x)   <==>   (= x (* n (div x n)))
     long n = makeNumberImpl(pModulo);
-    long x = subtract(pNumber1, pNumber2);
-    return Native.mkEq(
-        z3context, x, Native.mkMul(z3context, 2, new long[] {n, Native.mkDiv(z3context, x, n)}));
+    Native.incRef(z3context, n);
+    try {
+      return modularCongruence0(pNumber1, pNumber2, makeNumberImpl(pModulo));
+    } finally {
+      Native.decRef(z3context, n);
+    }
   }
+  protected Long modularCongruence0(Long pNumber1, Long pNumber2, Long n) {
+    // ((_ divisible n) x)   <==>   (= x (* n (div x n)))
+    long x = subtract(pNumber1, pNumber2);
+    Native.incRef(z3context, x);
+    long div = Native.mkDiv(z3context, x, n);
+    Native.incRef(z3context, div);
+    long mul = Native.mkMul(z3context, 2, new long[] {n, div});
+    Native.incRef(z3context, mul);
+    try {
+      return Native.mkEq(z3context, x, mul);
+    } finally {
+      Native.decRef(z3context, x);
+      Native.decRef(z3context, div);
+      Native.decRef(z3context, mul);
+    }
+  }
+
 }
