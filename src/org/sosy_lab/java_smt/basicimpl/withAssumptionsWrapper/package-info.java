2518c18bacd4b710be629be9af5d93660e26b0d5
Philipp Wendler <uni@philippwendler.de>
2018-06-15 10:40:57 +0200

    Fail build on Javadoc warning and fix one warning.

--- src/org/sosy_lab/java_smt/basicimpl/withAssumptionsWrapper/package-info.java
+++ src/org/sosy_lab/java_smt/basicimpl/withAssumptionsWrapper/package-info.java
@@ -20,11 +20,9 @@
 
 /**
  * Wrapper-classes to guarantee identical behavior for all solvers. If a solver does not support
- * {@link BasicProverEnvironment#isUnsatWithAssumptions}, we wrap it in a (subclass of)
- * BasicProverWithAssumptionsWrapper, whose task it is to keep the assumptions as long on the
- * solver's stack as no other operation accesses it. It allows to compute interpolants and unsat
- * cores. without direct support from the solver.
+ * {@link org.sosy_lab.java_smt.api.BasicProverEnvironment#isUnsatWithAssumptions}, we wrap it in a
+ * (subclass of) BasicProverWithAssumptionsWrapper, whose task it is to keep the assumptions as long
+ * on the solver's stack as no other operation accesses it. It allows to compute interpolants and
+ * unsat cores. without direct support from the solver.
  */
 package org.sosy_lab.java_smt.basicimpl.withAssumptionsWrapper;
-
-import org.sosy_lab.java_smt.api.BasicProverEnvironment;
