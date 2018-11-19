6c5714308968305bda406928e1808208d5bce085
emslander <emslander@github.com>
2018-11-19 14:20:16 +0100

    remove test, package-info.java and UfElimination.java

--- src/org/sosy_lab/java_smt/solvers/bdd/package-info.java
+++
@@ -1,5 +0,0 @@
-@javax.annotation.CheckReturnValue
-@javax.annotation.ParametersAreNonnullByDefault
-@org.sosy_lab.common.annotations.FieldsAreNonnullByDefault
-@org.sosy_lab.common.annotations.ReturnValuesAreNonnullByDefault
-package org.sosy_lab.java_smt.utils;
