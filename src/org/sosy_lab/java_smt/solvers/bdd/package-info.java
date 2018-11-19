b5b230d3f6337964bc748d2ac6e14ba7645c8f44
emslander <emslander@github.com>
2018-11-19 11:42:33 +0100

    util inhalt

---
+++ src/org/sosy_lab/java_smt/solvers/bdd/package-info.java
@@ -0,0 +1,5 @@
+@javax.annotation.CheckReturnValue
+@javax.annotation.ParametersAreNonnullByDefault
+@org.sosy_lab.common.annotations.FieldsAreNonnullByDefault
+@org.sosy_lab.common.annotations.ReturnValuesAreNonnullByDefault
+package org.sosy_lab.java_smt.utils;
