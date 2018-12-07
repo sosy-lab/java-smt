49459380687c6339a16b39582d64c056d42c8c50
Karlheinz Friedberger <kfriedberger@gmail.com>
2018-12-07 10:38:29 +0100

    fix example program: model-generation needs to be enabled.

--- src/org/sosy_lab/java_smt/example/OptimizationFormulaWeights.java
+++ src/org/sosy_lab/java_smt/example/OptimizationFormulaWeights.java
@@ -17,6 +17,7 @@ import org.sosy_lab.java_smt.api.NumeralFormula.IntegerFormula;
 import org.sosy_lab.java_smt.api.OptimizationProverEnvironment;
 import org.sosy_lab.java_smt.api.OptimizationProverEnvironment.OptStatus;
 import org.sosy_lab.java_smt.api.SolverContext;
+import org.sosy_lab.java_smt.api.SolverContext.ProverOptions;
 import org.sosy_lab.java_smt.api.SolverException;
 
 /**
@@ -39,7 +40,8 @@ public class OptimizationFormulaWeights {
 
     try (SolverContext context =
             SolverContextFactory.createSolverContext(config, logger, notifier, solver);
-        OptimizationProverEnvironment prover = context.newOptimizationProverEnvironment()) {
+        OptimizationProverEnvironment prover =
+            context.newOptimizationProverEnvironment(ProverOptions.GENERATE_MODELS)) {
 
       BooleanFormulaManager bmgr = context.getFormulaManager().getBooleanFormulaManager();
       IntegerFormulaManager imgr = context.getFormulaManager().getIntegerFormulaManager();
