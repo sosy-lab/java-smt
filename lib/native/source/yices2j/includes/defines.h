08dc5f3ed24687d9b34cbfe72dd21c22e10d5df6
Michael Obermeier <michael.obermeier@campus.lmu.de>
2019-10-12 12:06:02 +0200

    Cleanup unused macros in defines.h

--- lib/native/source/yices2j/includes/defines.h
+++ lib/native/source/yices2j/includes/defines.h
@@ -12,13 +12,13 @@
 
 typedef void jvoid; // for symmetry to jint, jlong etc.
 
-// Macros for defining JNI functions which call Mathsat
+// Macros for defining JNI functions which call Yices
 // Use them as follows:
 //
-// DEFINE_FUNC(java_return_type, escaped_name_without_msat) WITH_X_ARGS(java_arg_types)
-// for each arg a definition like STRUCT_ARG(msat_arg_type, position)
-// CALLX(msat_return_type, function_name_without_msat)
-// return definition like STRUCT_RETURN depending on the return type
+// DEFINE_FUNC(java_return_type, escaped_name_without_yices) WITH_X_ARGS(java_arg_types)
+// for each arg a definition like TERM_ARG(position)
+// CALLX(yices_return_type, function_name_without_yices)
+// return definition like TERM_RETURN depending on the return type
 
 
 #define DEFINE_FUNC(jreturn, func_escaped) \
@@ -50,22 +50,6 @@ typedef void jvoid; // for symmetry to jint, jlong etc.
 #define NULL_ARG(mtype, num) \
 		mtype *m_arg##num = NULL;
 
-#define STRUCT_ARG(mtype, num) \
-  if (arg##num == 0) { \
-    throwException(jenv, "java/lang/IllegalArgumentException", "Null passed to MathSAT"); \
-    return 0; \
-  } \
-  mtype m_arg##num; \
-  m_arg##num.repr = (void *)((size_t)arg##num);
-
-#define STRUCT_ARG_VOID(mtype, num) \
-  if (arg##num == 0) { \
-    throwException(jenv, "java/lang/IllegalArgumentException", "Null passed to MathSAT"); \
-    return; \
-  } \
-  mtype m_arg##num; \
-  m_arg##num.repr = (void *)((size_t)arg##num);
-
 #define STRING_ARG(num) \
   char * m_arg##num = (char *)(*jenv)->GetStringUTFChars(jenv, arg##num, NULL); \
   if (m_arg##num == NULL) { \
@@ -83,42 +67,6 @@ typedef void jvoid; // for symmetry to jint, jlong etc.
         goto out##num; \
       } \
  }
-
-#define MPZ_ARG(num) \
-  mpz_t m_arg##num; \
-  char *tmp_str = (char *)(*jenv)->GetStringUTFChars(jenv, arg##num, NULL);\
-  mpz_init_set_str(m_arg##num, tmp_str, 10); \
-  (*jenv)->ReleaseStringUTFChars(jenv, arg##num, tmp_str);
-
-#define STRUCT_ARRAY_ARG(mtype, num) \
-  mtype * m_arg##num; \
-  { \
-    size_t sz = (size_t)((*jenv)->GetArrayLength(jenv, arg##num)); \
-    m_arg##num = (mtype *)malloc(sizeof(mtype) * sz); \
-    if (m_arg##num == NULL) { \
-      throwException(jenv, "java/lang/OutOfMemoryError", "Cannot allocate native memory for calling Yices"); \
-      goto out##num##a; \
-    } \
-    \
-    jlong *tmp = (jlong *)((*jenv)->GetLongArrayElements(jenv, arg##num, NULL)); \
-    if (tmp == NULL) { \
-      goto out##num##b; \
-    } \
-    \
-    size_t i; \
-    for (i = 0; i < sz; ++i) { \
-      m_arg##num[i].repr = (void *)((size_t)tmp[i]); \
-        if (m_arg##num[i].repr == NULL) { \
-        throwException(jenv, "java/lang/IllegalArgumentException", "Null passed to MathSAT"); \
-        goto out##num##b; \
-      } \
-    } \
-    (*jenv)->ReleaseLongArrayElements(jenv, arg##num, tmp, 0); \
-  }
-
-#define STRUCT_POINTER_ARG(mtype, num) \
-  mtype s_arg##num; \
-  mtype * m_arg##num = &s_arg##num; 
 
 //may cause memory problems
 #define INT_ARRAY_ARG(mtype, num) \
@@ -202,10 +150,6 @@ typedef void jvoid; // for symmetry to jint, jlong etc.
   m_arg##num->node_id = arg##id; \
   m_arg##num->node_tag = arg##tag; \
 
-#define STRUCT_ARRAY_OUTPUT_ARG(num) \
-  size_t s_arg##num = 0; \
-  size_t *m_arg##num = &s_arg##num;
-
 #define MPQ_ARG(num) \
   mpq_t m_arg##num; \
   mpq_init(m_arg##num); \
@@ -229,14 +173,6 @@ typedef void jvoid; // for symmetry to jint, jlong etc.
   (*jenv)->ReleaseStringUTFChars(jenv, arg##num, m_arg##num); \
   out##num:
 
-#define FREE_MPZ_ARG(num) \
-  mpz_clear(m_arg##num);
-
-#define FREE_STRUCT_ARRAY_ARG(num) \
-  out##num##b: \
-  free(m_arg##num); \
-  out##num##a:
-
 #define FREE_INT_ARRAY_ARG(num) \
   (*jenv)->ReleaseIntArrayElements(jenv, arg##num, m_arg##num, 0); \
   out##num:
@@ -245,25 +181,6 @@ typedef void jvoid; // for symmetry to jint, jlong etc.
   (*jenv)->ReleaseLongArrayElements(jenv, arg##num, m_arg##num, 0); \
   out##num:
 
-#define PUT_STRUCT_POINTER_ARG(num) \
-  if (!(*jenv)->ExceptionCheck(jenv)) { \
-    (*jenv)->SetLongArrayRegion(jenv, arg##num, 0, 1, (jlong *)&(s_arg##num)); \
-  }
-
-#define STRUCT_RETURN \
-  if (retval.repr == NULL) { \
-    throwException(jenv, "java/lang/IllegalArgumentException", "MathSAT returned null"); \
-  } \
-  return (jlong)((size_t)(retval.repr)); \
-}
-
-#define STRUCT_RETURN_WITH_ENV \
-  if (retval.repr == NULL) { \
-    const char *msg = msat_last_error_message(m_arg1); \
-    throwException(jenv, "java/lang/IllegalArgumentException", msg); \
-  } \
-  return (jlong)((size_t)(retval.repr)); \
-}
 //may cause memory leak through yices_error_string
 #define INT_RETURN \
    if (retval <= 0 && yices_error_code() != 0){ \
@@ -289,26 +206,6 @@ typedef void jvoid; // for symmetry to jint, jlong etc.
     return NULL; \
   } \
   PLAIN_STRING_RETURN
-
-#define CONST_STRING_RETURN \
-  if (retval == NULL) { \
-    const char *msg = msat_last_error_message(m_arg1); \
-    throwException(jenv, "java/lang/IllegalArgumentException", msg); \
-    return NULL; \
-  } \
-  jstring jretval = NULL; \
-  if (!(*jenv)->ExceptionCheck(jenv)) { \
-    jretval = (*jenv)->NewStringUTF(jenv, retval); \
-  } \
-  return jretval; \
-}
-
-#define FAILURE_CODE_RETURN \
-  if (retval != 0) { \
-    const char *msg = msat_last_error_message(m_arg1); \
-    throwException(jenv, "java/lang/IllegalArgumentException", msg); \
-  } \
-}
 
 //may cause memory leak through yices_error_string
 #define POINTER_RETURN \
@@ -394,45 +291,6 @@ typedef void jvoid; // for symmetry to jint, jlong etc.
       (*jenv)->SetIntArrayRegion(jenv, jretval, 0, 2, yval); \
     } \
   } \
-  return jretval; \
-}
-
-
-/**
- * This assumes that mathsat allocated an array,
- * returned the pointer and stored the size in the argument arg_num
- * (which was declared with STRUCT_ARRAY_OUTPUT_ARG).
- * It also assumes that the first argument is an environment.
- */
-#define RETURN_STRUCT_ARRAY(arg_num) \
-  jlongArray jretval = NULL; \
-  if ((*jenv)->ExceptionCheck(jenv)) { \
-    goto out; \
-  } \
-  if (retval == NULL) { \
-    const char *msg = msat_last_error_message(m_arg1); \
-    throwException(jenv, "java/lang/IllegalArgumentException", msg); \
-    goto out; \
-  } \
-  \
-  jlong *jarr = malloc(sizeof(jlong) * s_arg##arg_num); \
-  if (jarr == NULL) { \
-    throwException(jenv, "java/lang/OutOfMemoryError", "Cannot allocate native memory for passing return value from Mathsat"); \
-    goto out; \
-  } \
-  size_t i; \
-  for (i = 0; i < s_arg##arg_num; ++i) { \
-      jarr[i] = (jlong)((size_t)retval[i].repr); \
-  } \
-  jretval = (*jenv)->NewLongArray(jenv, s_arg##arg_num); \
-  if (jretval != NULL) { \
-    (*jenv)->SetLongArrayRegion(jenv, jretval, 0, s_arg##arg_num, jarr); \
-  } \
-  free(jarr); \
-  \
-  out: \
-  msat_free(retval); \
-  \
   return jretval; \
 }
 
@@ -537,126 +395,41 @@ typedef jlong jjparam;
 #define PARAM_ARG_VOID(num) POINTER_ARG(param_t, num)
 #define PARAM_RETURN POINTER_RETURN
 
-// max term count > Integer_MAX_VALUE?
 typedef jint jjterm;
 #define TERM_ARG(num) SIMPLE_ARG(term_t, num)
 #define TERM_ARG_VOID(num) SIMPLE_ARG(term_t, num)
 #define TERM_RETURN INT_RETURN
 
 typedef jlong jjmodel;
-#define MODEL_ARG(num) POINTER_ARG(model_t, num) //STRUCT_ARG(msat_model, num)
-#define MODEL_ARG_VOID(num) POINTER_ARG(model_t, num)//STRUCT_ARG_VOID(msat_model, num)
-#define MODEL_RETURN POINTER_RETURN//STRUCT_RETURN_WITH_ENV
+#define MODEL_ARG(num) POINTER_ARG(model_t, num)
+#define MODEL_ARG_VOID(num) POINTER_ARG(model_t, num)
+#define MODEL_RETURN POINTER_RETURN
 
 typedef jintArray jjtermArray;
-#define TERM_ARRAY_ARG(num) INT_ARRAY_ARG(term_t, num) //STRUCT_ARRAY_ARG(term_t, num)
-#define FREE_TERM_ARRAY_ARG(num) FREE_INT_ARRAY_ARG(num) //FREE_STRUCT_ARRAY_ARG(num)
-#define TERM_ARRAY_OUTPUT_ARG(num) STRUCT_ARRAY_OUTPUT_ARG(num)
-#define RETURN_TERM_ARRAY(arg_num) RETURN_STRUCT_ARRAY(arg_num)
-#define TERM_POINTER_ARG(num) STRUCT_POINTER_ARG(term_t, num)
-#define PUT_TERM_POINTER_ARG(num) PUT_STRUCT_POINTER_ARG(num)
-
-typedef jlong jjdecl;
-#define DECL_ARG(num) STRUCT_ARG(msat_decl, num)
-#define DECL_RETURN STRUCT_RETURN_WITH_ENV
-
-typedef jlong jjmodel_iterator;
-#define MODEL_ITERATOR_ARG(num) STRUCT_ARG(msat_model_iterator, num)
-#define MODEL_ITERATOR_ARG_VOID(num) STRUCT_ARG_VOID(msat_model_iterator, num)
-#define MODEL_ITERATOR_RETURN STRUCT_RETURN_WITH_ENV
+#define TERM_ARRAY_ARG(num) INT_ARRAY_ARG(term_t, num)
+#define FREE_TERM_ARRAY_ARG(num) FREE_INT_ARRAY_ARG(num)
 
 typedef jint jjtype;
 #define TYPE_ARG(num) SIMPLE_ARG(type_t, num)
 #define TYPE_RETURN INT_RETURN
-/*  if (retval == -1){ \
- *   const char *msg = yices_error_string(); \
- *   throwException(jenv, "java/lang/IllegalArgumentException", msg); \
- *   return -1; \
- * } \ 
- *INT_RETURN
- */
-// above code might leak memory of error string
 
 typedef jintArray jjtypeArray;
-#define TYPE_ARRAY_ARG(num) INT_ARRAY_ARG(type_t, num)//Possible type conflict STRUCT_ARRAY_ARG(type_t, num)
-#define FREE_TYPE_ARRAY_ARG(num) FREE_INT_ARRAY_ARG(num)//FREE_STRUCT_ARRAY_ARG(num)
-#define TYPE_POINTER_ARG(num) STRUCT_POINTER_ARG(msat_type, num)
+#define TYPE_ARRAY_ARG(num) INT_ARRAY_ARG(type_t, num)
+#define FREE_TYPE_ARRAY_ARG(num) FREE_INT_ARRAY_ARG(num)
 
 typedef jint jjboolean;
 #define BOOLEAN_RETURN INT_RETURN
-/*    if (retval == 0 && yices_error_code != 0){ \
- *   const char *msg = yices_error_string(); \
- *   throwException(jenv, "java/lang/IllegalArgumentException", msg); \
- *   return -1; \
- * } \
- * 
- *INT_RETURN
- */
-// above code might leak memory of error string
-
-typedef jlong jjobjective_iterator;
-#define OBJECTIVE_ITERATOR_ARG(num) STRUCT_ARG(msat_objective_iterator, num)
-#define OBJECTIVE_ITERATOR_ARG_VOID(num) STRUCT_ARG_VOID(msat_objective_iterator, num)
-#define OBJECTIVE_ITERATOR_RETURN STRUCT_RETURN_WITH_ENV
-
-typedef jlong jjobjective;
-#define OBJECTIVE_ARG(num) STRUCT_ARG(msat_objective, num)
-#define OBJECTIVE_ARG_VOID(num) STRUCT_ARG_VOID(msat_objective, num)
-#define OBJECTIVE_RETURN STRUCT_RETURN_WITH_ENV
-
-typedef jlongArray jjobjectiveArray;
-#define OBJECTIVE_POINTER_ARG(num) STRUCT_POINTER_ARG(msat_objective, num)
-#define PUT_OBJECTIVE_POINTER_ARG(num) PUT_STRUCT_POINTER_ARG(num)
 
 typedef jlong jjyval;
 typedef jint jjnodeid;
 typedef jint jjnodetag;
 
-typedef jvoid jjfailureCode;
-
-typedef jobject jjnamedtermswrapper;
-
 // Abbreviations for common combinations of return and argument types
-//
-// Parameter explanation:
-// func: the name of the Mathsat function (without msat_)
-// func_escaped: the escaped variant of func
-// jreturn: return type in Java world
-// mreturn: return type of Mathsat C function
-// margX: Java type of argument X
-// jargX: Mathsat type of argument X
-
-#define i_func1s(func, func_escaped, mreturn, marg1) \
-  DEFINE_FUNC(int, func_escaped) WITH_ONE_ARG(long) \
-  STRUCT_ARG(marg1, 1) \
-  CALL1(mreturn, func) \
-  INT_RETURN
-
-#define make_term_constant(func, func_escaped) \
-  DEFINE_FUNC(jterm, 1make_##func_escaped) WITH_ONE_ARG(jenv) \
-  ENV_ARG(1) \
-  CALL1(term_t, make_##func) \
-  TERM_RETURN
 
 #define get_yices_type(name) \
   DEFINE_FUNC(jtype, 1##name##_1type) WITHOUT_ARGS \
   CALL0(type_t, ##name##_type) \
   TYPE_RETURN
-
-#define make_term_unary(name) \
-  DEFINE_FUNC(jterm, 1make_1##name) WITH_TWO_ARGS(jenv, jterm) \
-  ENV_ARG(1) \
-  TERM_ARG(2) \
-  CALL2(term_t, make_##name) \
-  TERM_RETURN
-
-#define make_term_binary(name) \
-  DEFINE_FUNC(jterm, 1make_1##name) WITH_THREE_ARGS(jenv, jterm, jterm) \
-  ENV_ARG(1) \
-  TERM_ARG(2) \
-  TERM_ARG(3) \
-  CALL3(term_t, make_##name) \
-  TERM_RETURN
 
 #define yices_arith(name) \
   DEFINE_FUNC(jterm, 1arith_1##name##_1atom) WITH_ONE_ARG(jterm) \
