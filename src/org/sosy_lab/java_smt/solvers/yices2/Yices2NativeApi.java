a1fdc010f9411cdf2f50201ba3dbd3a0cfbc56f4
Karlheinz Friedberger <kfriedberger@gmail.com>
2019-12-22 23:46:02 +0100

    formatting.

--- src/org/sosy_lab/java_smt/solvers/yices2/Yices2NativeApi.java
+++ src/org/sosy_lab/java_smt/solvers/yices2/Yices2NativeApi.java
@@ -768,7 +768,7 @@ class Yices2NativeApi {
         throw new IllegalArgumentException(
             String.format(
                 "Can't create variable with name '%s' and type '%s' "
-                + "as it would omit a variable with type '%s'",
+                    + "as it would omit a variable with type '%s'",
                 name, yices_type_to_string(type), yices_type_to_string(termFromNameType)));
       }
     }
