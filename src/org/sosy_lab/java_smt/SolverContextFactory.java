sosy-lab/java-smt

commit e2e70837b49e69f51933f45a93e9d8a81f19e095

fix some smaller coding issues, mostly just optimization.

src/org/sosy_lab/java_smt/SolverContextFactory.java
@@ -152,7 +152,7 @@ public class SolverContextFactory {
   }
 
   private static String getFileExtension(String path) {
-    int lastIndexOf = path.lastIndexOf(".");
+    int lastIndexOf = path.indexOf('.');
     return (lastIndexOf == -1) ? "" : path.substring(lastIndexOf);
   }
 

src/org/sosy_lab/java_smt/example/FormulaClassifier.java
@@ -137,48 +137,48 @@ public class FormulaClassifier {
   @Override
   public String toString() {
     // build logic string
-    String logic = "";
+    StringBuilder logic = new StringBuilder();
     if (!v.hasQuantifiers) {
-      logic += "QF_";
+      logic.append("QF_");
     }
     if (v.hasArrays) {
-      logic += "A";
+      logic.append("A");
     }
     if (v.hasUFs) {
-      logic += "UF";
+      logic.append("UF");
     }
     if (v.hasBVs) {
-      logic += "BV";
+      logic.append("BV");
     }
     if (v.nonLinearArithmetic || v.linearArithmetic) {
       if (v.hasInts && v.hasReals) {
         if (v.nonLinearArithmetic) {
-          logic += "N";
+          logic.append("N");
         } else if (v.linearArithmetic) {
-          logic += "L";
+          logic.append("L");
         }
-        logic += "IRA";
+        logic.append("IRA");
       } else if (v.hasInts) {
         if (v.nonLinearArithmetic) {
-          logic += "N";
+          logic.append("N");
         } else if (v.linearArithmetic) {
-          logic += "L";
+          logic.append("L");
         }
-        logic += "IA";
+        logic.append("IA");
       } else if (v.hasReals) {
         if (v.nonLinearArithmetic) {
-          logic += "N";
+          logic.append("N");
         } else if (v.linearArithmetic) {
-          logic += "L";
+          logic.append("L");
         }
-        logic += "RA";
+        logic.append("RA");
       }
     }
     if (v.hasFloats) {
       // TODO forthcoming, see http://smtlib.cs.uiowa.edu/logics.shtml
-      logic += "FP";
+      logic.append("FP");
     }
-    return logic;
+    return logic.toString();
   }
 
   private static class AtomCollector extends DefaultBooleanFormulaVisitor<TraversalProcess> {

src/org/sosy_lab/java_smt/solvers/mathsat5/Mathsat5NativeApi.java
@@ -195,7 +195,7 @@ class Mathsat5NativeApi {
           throw new SolverException(msg);
         }
 
-        String code = (resultCode == MSAT_UNKNOWN) ? "\"unknown\"" : resultCode + "";
+        String code = (resultCode == MSAT_UNKNOWN) ? "\"unknown\"" : String.valueOf(resultCode);
         throw new IllegalStateException(
             "msat_solve returned " + code + (msg != null ? ": " + msg : ""));
     }

src/org/sosy_lab/java_smt/solvers/princess/PrincessQuantifiedFormulaManager.java
@@ -53,7 +53,7 @@ class PrincessQuantifiedFormulaManager
   public IExpression mkQuantifier(Quantifier q, List<IExpression> vars, IExpression body) {
     checkArgument(body instanceof IFormula);
     ap.terfor.conjunctions.Quantifier pq = (q == Quantifier.FORALL) ? ALL$.MODULE$ : EX$.MODULE$;
-    if (vars.size() == 0) {
+    if (vars.isEmpty()) {
 
       // Body already contains bound variables.
       return new IQuantified(pq, (IFormula) body);

src/org/sosy_lab/java_smt/solvers/z3/Z3QuantifiedFormulaManager.java
@@ -38,7 +38,7 @@ class Z3QuantifiedFormulaManager extends AbstractQuantifiedFormulaManager<Long, 
 
   @Override
   public Long mkQuantifier(Quantifier q, List<Long> pVariables, Long pBody) {
-    if (pVariables.size() == 0) {
+    if (pVariables.isEmpty()) {
       throw new IllegalArgumentException("List of quantified variables can not be empty");
     }
     return Native.mkQuantifierConst(

commit 2e4f7805b3c80bb298babec260b8eb91b76bd983

fix bug introduced in e2e70837

src/org/sosy_lab/java_smt/SolverContextFactory.java
@@ -152,7 +152,7 @@ public class SolverContextFactory {
   }
 
   private static String getFileExtension(String path) {
-    int lastIndexOf = path.indexOf('.');
+    int lastIndexOf = path.lastIndexOf('.');
     return (lastIndexOf == -1) ? "" : path.substring(lastIndexOf);
   }
 