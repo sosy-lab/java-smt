cd80d20775ecc2f7f947f8f400b979e4c1ba1cda
baier <baier@baier-VirtualBox>
2020-02-06 20:54:08 +0100

    New Librarys for Boolector Version 3.1.0 with all SAT Solvers
    Cadical/MiniSAT/CryptoMiniSAT now working and included
    New Default Solver Cadical

--- src/org/sosy_lab/java_smt/solvers/boolector/BoolectorSolverContext.java
+++ src/org/sosy_lab/java_smt/solvers/boolector/BoolectorSolverContext.java
@@ -46,7 +46,8 @@ public final class BoolectorSolverContext extends AbstractSolverContext {
     LINGELING,
     PICOSAT,
     MINISAT,
-    CADICAL
+    CADICAL,
+
   }
 
   @Options(prefix = "solver.boolector")
@@ -154,10 +155,6 @@ public final class BoolectorSolverContext extends AbstractSolverContext {
     BoolectorSettings settings = new BoolectorSettings(config);
 
     Preconditions.checkNotNull(settings.satSolver);
-    // TODO implement non-incremental stack-handling in the TheoremProver.
-    Preconditions.checkArgument(
-        settings.satSolver != SatSolver.CADICAL,
-        "CaDiCal is not usable with JavaSMT, because it does not support incremental mode.");
     BtorJNI.boolector_set_sat_solver(btor, settings.satSolver.name());
     // Default Options to enable multiple SAT, auto cleanup on close, incremental mode
     BtorJNI.boolector_set_opt(btor, BtorOption.BTOR_OPT_MODEL_GEN.getValue(), 2);
