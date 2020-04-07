bc91acc4721a3327ba43aa97f512d2a0200518d7
Karlheinz Friedberger <kfriedberger@gmail.com>
2020-04-07 12:39:45 +0200

    Cleanup: simplify object-based types to primitive ones.

--- src/org/sosy_lab/java_smt/solvers/boolector/BoolectorFormulaCreator.java
+++ src/org/sosy_lab/java_smt/solvers/boolector/BoolectorFormulaCreator.java
@@ -270,7 +270,7 @@ public class BoolectorFormulaCreator extends FormulaCreator<Long, Long, Long, Lo
     // To get the correct type, we check the width of the term (== 1 means bool).
     int width = BtorJNI.boolector_get_width(getEnv(), term);
     if (width == 1) {
-      Long longValue = parseBigInt(value).longValue();
+      long longValue = parseBigInt(value).longValue();
       if (longValue == 1) {
         return true;
       } else if (longValue == 0) {
