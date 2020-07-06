9e12ffb4fb1544a25f27260156f28a4b64352b88
baierd <baier-daniel@gmx.net>
2020-07-06 17:43:29 +0200

    Better Table

--- src/org/sosy_lab/java_smt/example/ListAllSolversWithFeatures.java
+++ src/org/sosy_lab/java_smt/example/ListAllSolversWithFeatures.java
@@ -23,6 +23,8 @@ import static org.sosy_lab.java_smt.api.SolverContext.ProverOptions.GENERATE_UNS
 
 import com.google.common.collect.ImmutableList;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Supplier;
 import org.sosy_lab.common.ShutdownNotifier;
@@ -50,74 +52,28 @@ public class ListAllSolversWithFeatures {
 
   public static void main(String[] args) throws SolverException, InterruptedException {
 
-    ListAllSolversWithFeatures listAllSolversWithFeatures = new ListAllSolversWithFeatures();
-    List<List<String>> infoArray = listAllSolversWithFeatures.getInformationForAllSolvers();
-    listAllSolversWithFeatures.printGridToStdOut(infoArray);
+    ListAllSolversWithFeatures laswf = new ListAllSolversWithFeatures();
+    RowBuilder rowBuilder = laswf.new RowBuilder();
+    laswf.getInformationForAllSolvers(rowBuilder);
+    System.out.println(rowBuilder.toString());
   }
 
   public ListAllSolversWithFeatures() {}
-
-  /**
-   * Prints the infoArray to StdOut in a nice table using the formating method (through printf()).
-   * Note that this conforms to the row/columns specified in getInformationForAllSolvers().
-   */
-  private void printGridToStdOut(List<List<String>> infoArray) {
-    if (infoArray == null || infoArray.get(0).size() == 0) {
-      System.out.println("Could not find any installed SMT-Solvers.");
-      return;
-    }
-
-    int maxSolverLength = lineLength(infoArray, 0);
-    int maxVersionLength = lineLength(infoArray, 1);
-    int maxTheoriesLength = lineLength(infoArray, 2);
-    int maxFeaturesLength = lineLength(infoArray, 3);
-    String infoColumn =
-        "| %-"
-            + maxSolverLength
-            + "s | %-"
-            + maxVersionLength
-            + "s | %-"
-            + maxTheoriesLength
-            + "s | %-"
-            + maxFeaturesLength
-            + "s |%n";
-    String seperatorColumn =
-        "+-"
-            + "-".repeat(maxSolverLength)
-            + "-+-"
-            + "-".repeat(maxVersionLength)
-            + "-+-"
-            + "-".repeat(maxTheoriesLength)
-            + "-+-"
-            + "-".repeat(maxFeaturesLength)
-            + "-+%n";
-
-    System.out.printf(seperatorColumn);
-    System.out.printf(infoColumn, "Solver", "Version", "Theories", "Features");
-    System.out.printf(seperatorColumn);
-
-    for (List<String> s : infoArray) {
-      System.out.printf(infoColumn, s.get(0), s.get(1), s.get(2), s.get(3));
-    }
-
-    System.out.printf(seperatorColumn);
-  }
 
   /**
    * Gathers information about every available solver into a 2-Dimensional array representing rows
    * and columns of the later table. Columns represent (in order): Solver, Version, Theories,
    * Features. Rows represent the solvers available to the installed JavaSMT.
    */
-  private List<List<String>> getInformationForAllSolvers()
+  private void getInformationForAllSolvers(
+      RowBuilder rowBuilder)
       throws SolverException, InterruptedException {
-    List<List<String>> info = new ArrayList<>();
     for (Solvers s : Solvers.values()) {
-      List<String> solverInfo = getSolverInformation(s);
-      if (solverInfo != null && solverInfo.size() == 4) {
-        info.add(solverInfo);
+      SolverInfo solverInfo = getSolverInformation(s);
+      if (solverInfo != null) {
+        rowBuilder.addSolver(solverInfo);
       }
     }
-    return info;
   }
 
   /**
@@ -129,9 +85,9 @@ public class ListAllSolversWithFeatures {
    *     following order: SolverName, SolverVersion, SolverTheories, SolverFeatures. String[] length
    *     0 if invalid solver.
    */
-  private List<String> getSolverInformation(Solvers solver)
+  private SolverInfo getSolverInformation(
+      Solvers solver)
       throws SolverException, InterruptedException {
-    List<String> info = new ArrayList<>();
 
     Configuration config = Configuration.defaultConfiguration();
     LogManager logger = LogManager.createNullLogManager();
@@ -140,18 +96,19 @@ public class ListAllSolversWithFeatures {
     try (SolverContext context =
         SolverContextFactory.createSolverContext(config, logger, notifier, solver)) {
 
-      info.add(solver.name());
+      Solvers name = solver;
 
-      info.add(context.getVersion());
+      String version = context.getVersion();
 
-      info.add(getTheories(context));
+      String theories = String.join(", ", getTheories(context));
 
-      info.add(getFeatures(context));
+      String features = String.join(", ", getFeatures(context));
+
+      return new SolverInfo(name, version, theories, features);
     } catch (InvalidConfigurationException e) {
       // Catches missing solvers
+      return null;
     }
-
-    return info;
   }
 
   /**
@@ -162,7 +119,8 @@ public class ListAllSolversWithFeatures {
    * @return String with the features of the entered solver separated by a comma. Empty if none
    *     available.
    */
-  private String getFeatures(SolverContext context) throws SolverException, InterruptedException {
+  private List<String> getFeatures(SolverContext context)
+      throws SolverException, InterruptedException {
     List<String> features = new ArrayList<>();
 
     // Optimization: Will throw UnsupportedOperationException in creation of prover if not
@@ -173,6 +131,7 @@ public class ListAllSolversWithFeatures {
         features.add("Optimization");
       }
     } catch (UnsupportedOperationException e) {
+      // ignore, feature is not supported.
     }
 
     // Interpolation: Will throw UnsupportedOperationException in creation of prover if not
@@ -183,6 +142,7 @@ public class ListAllSolversWithFeatures {
         features.add("Interpolation");
       }
     } catch (UnsupportedOperationException e) {
+      // ignore, feature is not supported.
     }
 
     // UnsatCore: throws UnsupportedOperationException if not available.
@@ -193,6 +153,7 @@ public class ListAllSolversWithFeatures {
         features.add("UnsatCore");
       }
     } catch (UnsupportedOperationException e) {
+      // ignore, feature is not supported.
     } catch (Exception e) {
       features.add("UnsatCore");
     }
@@ -202,6 +163,7 @@ public class ListAllSolversWithFeatures {
         context.newProverEnvironment(GENERATE_UNSAT_CORE_OVER_ASSUMPTIONS)) {
       prover.unsatCoreOverAssumptions(null);
     } catch (UnsupportedOperationException e) {
+      // ignore, feature is not supported.
     } catch (NullPointerException e) {
       features.add("UnsatCore /w Assumption");
     }
@@ -230,6 +192,7 @@ public class ListAllSolversWithFeatures {
           features.add("AllSAT");
         }
       } catch (UnsupportedOperationException e) {
+        // ignore, feature is not supported.
       }
     }
 
@@ -240,10 +203,11 @@ public class ListAllSolversWithFeatures {
         prover.isUnsatWithAssumptions(ImmutableList.of());
         features.add("Assumptions");
       } catch (UnsupportedOperationException e) {
+        // ignore, feature is not supported.
       }
     }
 
-    return String.join(",", features);
+    return features;
   }
 
   /**
@@ -254,7 +218,7 @@ public class ListAllSolversWithFeatures {
    * @return String of all supported theories except Booleans, separated by a comma. Empty if none
    *     available.
    */
-  private String getTheories(SolverContext context) {
+  private List<String> getTheories(SolverContext context) {
     List<String> theories = new ArrayList<>();
     FormulaManager mgr = context.getFormulaManager();
 
@@ -268,7 +232,7 @@ public class ListAllSolversWithFeatures {
     addIfAvailable(theories, mgr::getUFManager, "UF");
     addIfAvailable(theories, mgr::getSLFormulaManager, "Seperation-Logic");
 
-    return String.join(",", theories);
+    return theories;
   }
 
   /**
@@ -289,23 +253,164 @@ public class ListAllSolversWithFeatures {
     }
   }
 
-  /**
-   * Checks the infoArray for the length of the given column.
-   *
-   * @param columnToCheck column of the infoArray you want the length for.
-   * @return length of String in column, but at least 8 (max length of the descriptor Strings).
-   */
-  private int lineLength(List<List<String>> infoArray, int columnToCheck) {
-    if (columnToCheck > 3 || columnToCheck < 0) {
-      return 8;
+  private class SolverInfo {
+    private Solvers solver;
+    private String solverVersion;
+    private String solverTheories;
+    private String solverFeatures;
+
+    /**
+     * Saves the information of an solver.
+     *
+     * @param solver Solvers enum object for a solver.
+     * @param solverVersion Solver version.
+     * @param solverTheories Solver theories.
+     * @param solverFeatures Solver features.
+     */
+    public SolverInfo(
+        Solvers solver,
+        String solverVersion,
+        String solverTheories,
+        String solverFeatures) {
+      this.solver = solver;
+      this.solverVersion = solverVersion;
+      this.solverTheories = solverTheories;
+      this.solverFeatures = solverFeatures;
     }
-    int length = 8;
-    for (List<String> s : infoArray) {
-      int checkedLength = s.get(columnToCheck).length();
-      if (length < checkedLength) {
-        length = checkedLength;
+
+    public String getName() {
+      return solver.name();
+    }
+
+    public String getVersion() {
+      return solverVersion;
+    }
+
+    public String getTheories() {
+      return solverTheories;
+    }
+
+    public String getFeatures() {
+      return solverFeatures;
+    }
+  }
+
+  private class RowBuilder {
+    private List<String> lines = new ArrayList<>();
+    // Minimum number of lines so that you can be sure a solver was added
+    private final int MIN_NUM_OF_LINES = 4;
+    private final int SOLVER_COLUMN_WIDTH = 15;
+    private final int VERSION_COLUMN_WIDTH = 40;
+    private final int THEORIES_COLUMN_WIDTH = 25;
+    private final int FEATURES_COLUMN_WIDTH = 25;
+
+    private String infoColumn =
+        "| %-"
+            + SOLVER_COLUMN_WIDTH
+            + "s | %-"
+            + VERSION_COLUMN_WIDTH
+            + "s | %-"
+            + THEORIES_COLUMN_WIDTH
+            + "s | %-"
+            + FEATURES_COLUMN_WIDTH
+            + "s |%n";
+
+    private String seperatorLine =
+        "+-"
+            + "-".repeat(SOLVER_COLUMN_WIDTH)
+            + "-+-"
+            + "-".repeat(VERSION_COLUMN_WIDTH)
+            + "-+-"
+            + "-".repeat(THEORIES_COLUMN_WIDTH)
+            + "-+-"
+            + "-".repeat(FEATURES_COLUMN_WIDTH)
+            + "-+%n";
+
+    /**
+     *
+     */
+    public RowBuilder() {
+      lines.add(String.format(seperatorLine));
+      lines.add(String.format(infoColumn, "Solver", "Version", "Theories", "Features"));
+      lines.add(String.format(seperatorLine));
+    }
+
+    /**
+     * Takes a SolverInfo object and splits it into lines which are added to the final String.
+     *
+     * @param solverInfo the solver with information you want added.
+     */
+    public void addSolver(SolverInfo solverInfo) {
+      List<String> nameLines = new LinkedList<>();
+      List<String> versionLines = getLines(solverInfo.getVersion(), VERSION_COLUMN_WIDTH);
+      List<String> TheoriesLines = getLines(solverInfo.getTheories(), THEORIES_COLUMN_WIDTH);
+      List<String> FeaturesLines = getLines(solverInfo.getFeatures(), FEATURES_COLUMN_WIDTH);
+
+      nameLines.add(solverInfo.getName());
+
+      int maxLines =
+          Math.max(Math.max(versionLines.size(), TheoriesLines.size()), FeaturesLines.size());
+
+      padLines(nameLines, maxLines);
+      padLines(versionLines, maxLines);
+      padLines(TheoriesLines, maxLines);
+      padLines(FeaturesLines, maxLines);
+
+      for (int i = 0; i < maxLines; i++) {
+        String nameL = nameLines.get(i);
+        String versionL = versionLines.get(i);
+        String theoriesL = TheoriesLines.get(i);
+        String featuresL = FeaturesLines.get(i);
+
+        lines.add(String.format(infoColumn, nameL, versionL, theoriesL, featuresL));
+      }
+
+      lines.add(String.format(seperatorLine));
+    }
+
+    private void padLines(List<String> linesToPad, int amountOfLines) {
+      if (linesToPad.size() == amountOfLines) {
+        return;
+      }
+      int loop = amountOfLines - linesToPad.size();
+      for (int i = 0; i < loop; i++) {
+        if (Math.floorMod(i, 2) == 1) {
+          linesToPad.add(0, "");
+        } else {
+          linesToPad.add(linesToPad.size(), "");
+        }
       }
     }
-    return length;
+
+    private List<String> getLines(String lineToSplit, int maxLength) {
+      List<String> versionSplit =
+          new LinkedList<>(Arrays.asList(lineToSplit.split(" ")));
+      List<String> versionLines = new LinkedList<>();
+      String line = versionSplit.get(0);
+      int lineCounter = line.length();
+      for (int i = 1; i < versionSplit.size(); i++) {
+        String current = versionSplit.get(i);
+        lineCounter += current.length() + 1;
+
+        if ((lineCounter - 1) > maxLength - 1) {
+          lineCounter = current.length() + 1;
+          versionLines.add(line);
+          line = current;
+        } else {
+          line = line + " " + current;
+        }
+      }
+      versionLines.add(line);
+      return versionLines;
+    }
+
+    @Override
+    public String toString() {
+      if (lines.size() < MIN_NUM_OF_LINES - 1) {
+        return "Could not find any installed SMT-Solvers.";
+      }
+      return String.join("", lines);
+    }
+
   }
 }
