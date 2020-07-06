adf15eaeb6d66984cc3f55248256c1337a3ebe73
Karlheinz Friedberger <kfriedberger@gmail.com>
2020-07-06 21:03:45 +0200

    rename example program.
    and change license to be reuse compliant.

--- src/org/sosy_lab/java_smt/example/SolverOverviewTable.java
+++ src/org/sosy_lab/java_smt/example/SolverOverviewTable.java
@@ -1,22 +1,12 @@
-/*
- *  JavaSMT is an API wrapper for a collection of SMT solvers.
- *  This file is part of JavaSMT.
- *
- *  Copyright (C) 2007-2020  Dirk Beyer
- *  All rights reserved.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
+// This file is part of JavaSMT,
+// JavaSMT is an API wrapper for a collection of SMT solvers.:
+// https://github.com/sosy-lab/java-smt
+//
+// Copyright (C) 2007-2020  Dirk Beyer
+// SPDX-FileCopyrightText: 2020 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package org.sosy_lab.java_smt.example;
 
 import static org.sosy_lab.java_smt.api.SolverContext.ProverOptions.GENERATE_UNSAT_CORE_OVER_ASSUMPTIONS;
@@ -51,13 +41,13 @@ import org.sosy_lab.java_smt.api.SolverException;
  * prints them to StdOut in a nice table.
  */
 // TODO: find shorter name
-public class ListAllSolversWithFeatures {
+public class SolverOverviewTable {
 
   public static void main(String[] args) throws SolverException, InterruptedException {
 
     final List<SolverInfo> infos = new ArrayList<>();
     for (Solvers s : Solvers.values()) {
-      SolverInfo solverInfo = new ListAllSolversWithFeatures().getSolverInformation(s);
+      SolverInfo solverInfo = new SolverOverviewTable().getSolverInformation(s);
       if (solverInfo != null) {
         infos.add(solverInfo);
       }
@@ -72,7 +62,7 @@ public class ListAllSolversWithFeatures {
     System.out.println(rowBuilder);
   }
 
-  private ListAllSolversWithFeatures() {}
+  private SolverOverviewTable() {}
 
   /**
    * Checks for solver-name, version, theories and features.
