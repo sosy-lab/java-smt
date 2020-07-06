64eb3ae87304ae33bc187779a4c9e4af3a2c410b
Karlheinz Friedberger <kfriedberger@gmail.com>
2020-07-06 21:38:54 +0200

    fix newline in String.

--- src/org/sosy_lab/java_smt/example/SolverOverviewTable.java
+++ src/org/sosy_lab/java_smt/example/SolverOverviewTable.java
@@ -292,15 +292,16 @@ public class SolverOverviewTable {
             + "s |%n";
 
     private static final String SEPERATOR_LINE =
-        "+-"
-            + "-".repeat(SOLVER_COLUMN_WIDTH)
-            + "-+-"
-            + "-".repeat(VERSION_COLUMN_WIDTH)
-            + "-+-"
-            + "-".repeat(THEORIES_COLUMN_WIDTH)
-            + "-+-"
-            + "-".repeat(FEATURES_COLUMN_WIDTH)
-            + "-+%n";
+        String.format(
+            "+-"
+                + "-".repeat(SOLVER_COLUMN_WIDTH)
+                + "-+-"
+                + "-".repeat(VERSION_COLUMN_WIDTH)
+                + "-+-"
+                + "-".repeat(THEORIES_COLUMN_WIDTH)
+                + "-+-"
+                + "-".repeat(FEATURES_COLUMN_WIDTH)
+                + "-+%n");
 
     /** The constructor builds the header of the table. */
     RowBuilder() {
