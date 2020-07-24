de4a8e0de23362202eca14b5fcc8de0ac939df3f
Karlheinz Friedberger <kfriedberger@gmail.com>
2020-07-24 14:56:00 +0200

    add test for crashing MathSAT5.
    Checking the return value of the method does not help us,
    even if the check is in the JNI code itself.
    
    See #199 for the code.

--- src/org/sosy_lab/java_smt/solvers/mathsat5/Mathsat5NativeApiTest.java
+++ src/org/sosy_lab/java_smt/solvers/mathsat5/Mathsat5NativeApiTest.java
@@ -14,6 +14,8 @@ import static org.sosy_lab.java_smt.solvers.mathsat5.Mathsat5NativeApi.msat_chec
 import static org.sosy_lab.java_smt.solvers.mathsat5.Mathsat5NativeApi.msat_create_config;
 import static org.sosy_lab.java_smt.solvers.mathsat5.Mathsat5NativeApi.msat_create_env;
 import static org.sosy_lab.java_smt.solvers.mathsat5.Mathsat5NativeApi.msat_destroy_config;
+import static org.sosy_lab.java_smt.solvers.mathsat5.Mathsat5NativeApi.msat_destroy_model_iterator;
+import static org.sosy_lab.java_smt.solvers.mathsat5.Mathsat5NativeApi.msat_from_smtlib2;
 import static org.sosy_lab.java_smt.solvers.mathsat5.Mathsat5NativeApi.msat_get_integer_type;
 import static org.sosy_lab.java_smt.solvers.mathsat5.Mathsat5NativeApi.msat_get_rational_type;
 import static org.sosy_lab.java_smt.solvers.mathsat5.Mathsat5NativeApi.msat_make_asin;
@@ -28,8 +30,12 @@ import static org.sosy_lab.java_smt.solvers.mathsat5.Mathsat5NativeApi.msat_make
 import static org.sosy_lab.java_smt.solvers.mathsat5.Mathsat5NativeApi.msat_make_sin;
 import static org.sosy_lab.java_smt.solvers.mathsat5.Mathsat5NativeApi.msat_make_times;
 import static org.sosy_lab.java_smt.solvers.mathsat5.Mathsat5NativeApi.msat_make_variable;
+import static org.sosy_lab.java_smt.solvers.mathsat5.Mathsat5NativeApi.msat_model_create_iterator;
+import static org.sosy_lab.java_smt.solvers.mathsat5.Mathsat5NativeApi.msat_model_iterator_has_next;
+import static org.sosy_lab.java_smt.solvers.mathsat5.Mathsat5NativeApi.msat_model_iterator_next;
 import static org.sosy_lab.java_smt.solvers.mathsat5.Mathsat5NativeApi.msat_pop_backtrack_point;
 import static org.sosy_lab.java_smt.solvers.mathsat5.Mathsat5NativeApi.msat_push_backtrack_point;
+import static org.sosy_lab.java_smt.solvers.mathsat5.Mathsat5NativeApi.msat_set_option_checked;
 import static org.sosy_lab.java_smt.solvers.mathsat5.Mathsat5NativeApi.msat_term_get_type;
 import static org.sosy_lab.java_smt.solvers.mathsat5.Mathsat5NativeApi.msat_term_is_pi;
 import static org.sosy_lab.java_smt.solvers.mathsat5.Mathsat5NativeApi.msat_type_equals;
@@ -60,7 +66,10 @@ public class Mathsat5NativeApiTest extends Mathsat5AbstractNativeApiTest {
   @Before
   public void createEnvironment() {
     long cfg = msat_create_config();
+
+    msat_set_option_checked(cfg, "model_generation", "true");
     // msat_set_option_checked(cfg, "theory.la.split_rat_eq", "false");
+
     env = msat_create_env(cfg);
     msat_destroy_config(cfg);
 
@@ -256,5 +265,100 @@ public class Mathsat5NativeApiTest extends Mathsat5AbstractNativeApiTest {
 
   private void checkIntegerType(long term) {
     assertThat(msat_type_equals(msat_term_get_type(term), msat_get_integer_type(env))).isTrue();
+  }
+
+  private static final String QUERY =
+      "(declare-fun __VERIFIER_nondet_int!2@ () Int)"
+          + "(declare-fun |main::length@3| () Int)"
+          + "(declare-fun |__ADDRESS_OF___VERIFIER_successful_alloc_*void#1@| () Int)"
+          + "(declare-fun |main::arr@3| () Int)"
+          + "(declare-fun |main::a@2| () Int)"
+          + "(declare-fun *int@1 () (Array Int Int))"
+          + "(declare-fun *int@2 () (Array Int Int))"
+          + "(declare-fun |main::__CPAchecker_TMP_0@2| () Int)"
+          + "(declare-fun |main::a@3| () Int)"
+          + "(define-fun v8 () Int 0)"
+          + "(define-fun v13 () Int 4)"
+          + "(define-fun v14 () Int __VERIFIER_nondet_int!2@)"
+          + "(define-fun v15 () Int |main::length@3|)"
+          + "(define-fun v16 () Bool (= v14 v15))"
+          + "(define-fun v17 () Int 1)"
+          + "(define-fun v18 () Bool (<= v17 v15))"
+          + "(define-fun v22 () Bool (and v16 v18))"
+          + "(define-fun v30 () Int |__ADDRESS_OF___VERIFIER_successful_alloc_*void#1@|)"
+          + "(define-fun v31 () Bool (<= v30 v8))"
+          + "(define-fun v32 () Bool (not v31))"
+          + "(define-fun v33 () Bool ((_ divisible 4) (- v30 v8)))"
+          + "(define-fun v35 () Int (- 4))"
+          + "(define-fun v36 () Bool (<= v30 v35))"
+          + "(define-fun v37 () Bool (not v36))"
+          + "(define-fun v41 () Int |main::arr@3|)"
+          + "(define-fun v42 () Bool (= v30 v41))"
+          + "(define-fun v43 () Bool (and v32 v33))"
+          + "(define-fun v44 () Bool (and v37 v43))"
+          + "(define-fun v48 () Bool (= v41 v8))"
+          + "(define-fun v51 () Bool (not v48))"
+          + "(define-fun v54 () Int (- 1))"
+          + "(define-fun v56 () Int |main::a@2|)"
+          + "(define-fun v57 () Int (* v54 v56))"
+          + "(define-fun v58 () Int (+ v41 v57))"
+          + "(define-fun v2300 () (Array Int Int) *int@1)"
+          + "(define-fun v2304 () (Array Int Int) *int@2)"
+          + "(define-fun v6120 () Int (select v2300 v56))"
+          + "(define-fun v6121 () Int (select v2300 v41))"
+          + "(define-fun v6122 () Bool (= v6120 v6121))"
+          + "(define-fun v6123 () Bool (not v6122))"
+          + "(define-fun v6135 () Int |main::__CPAchecker_TMP_0@2|)"
+          + "(define-fun v6136 () Bool (= v56 v6135))"
+          + "(define-fun v6139 () Int |main::a@3|)"
+          + "(define-fun v6140 () Int (* v54 v6139))"
+          + "(define-fun v6141 () Int (+ v56 v6140))"
+          + "(define-fun v6142 () Bool (= v6141 v13))"
+          + "(define-fun v6144 () Int (+ v6120 v6121))"
+          + "(define-fun v6145 () (Array Int Int) (store v2300 v56 v6144))"
+          + "(define-fun v6146 () Bool (= v2304 v6145))"
+          + "(define-fun v18733 () Int (* v13 v15))"
+          + "(define-fun v18735 () Int (+ v18733 v30))"
+          + "(define-fun v18736 () Bool (<= v18735 v8))"
+          + "(define-fun v18737 () Bool (not v18736))"
+          + "(define-fun v18738 () Bool (and v44 v18737))"
+          + "(define-fun v18739 () Bool (and v42 v18738))"
+          + "(define-fun v18740 () Bool (and v22 v18739))"
+          + "(define-fun v18741 () Bool (and v51 v18740))"
+          + "(define-fun v18744 () Int (+ v18733 v58))"
+          + "(define-fun v18745 () Bool (= v18744 v13))"
+          + "(define-fun v18746 () Bool (and v18741 v18745))"
+          + "(define-fun v18747 () Bool (and v6123 v18746))"
+          + "(define-fun v18748 () Bool (and v6146 v18747))"
+          + "(define-fun v18749 () Bool (and v6136 v18748))"
+          + "(define-fun v18750 () Bool (and v6142 v18749))"
+          + "(assert v18750)";
+
+  // TODO The next method crashes with MathSAT5 version 5.6.3,
+  // The bug is reported, we need to check this with the next release.
+  @Ignore
+  public void modelIteratorCrash()
+      throws IllegalStateException, InterruptedException, SolverException {
+
+    long parsed_formula = msat_from_smtlib2(env, QUERY);
+    msat_assert_formula(env, parsed_formula);
+
+    boolean isSat = msat_check_sat(env);
+    assertThat(isSat).isTrue();
+
+    long iter = msat_model_create_iterator(env);
+    while (msat_model_iterator_has_next(iter)) {
+      long[] key = new long[1];
+      long[] value = new long[1];
+      // System.out.println("before crash");
+      @SuppressWarnings("unused")
+      boolean check = msat_model_iterator_next(iter, key, value); // crash here
+      // System.out.println(" " + check);
+      // String k = msat_term_repr(key[0]);
+      // System.out.println("after crash");
+      // String v = msat_term_repr(value[0]);
+      // System.out.println(k + " := " + v);
+    }
+    msat_destroy_model_iterator(iter);
   }
 }
