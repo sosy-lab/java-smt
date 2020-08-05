0d775b64e9285b4b9d7257c115986ed5b08ab46a
Karlheinz Friedberger <kfriedberger@gmail.com>
2020-08-05 08:49:56 +0200

    MathSAT5 context supports loading the Windows library.

--- src/org/sosy_lab/java_smt/solvers/mathsat5/Mathsat5NativeApiTest.java
+++ src/org/sosy_lab/java_smt/solvers/mathsat5/Mathsat5NativeApiTest.java
@@ -45,7 +45,6 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
-import org.sosy_lab.common.NativeLibraries;
 import org.sosy_lab.java_smt.api.SolverException;
 
 public class Mathsat5NativeApiTest extends Mathsat5AbstractNativeApiTest {
@@ -57,7 +56,7 @@ public class Mathsat5NativeApiTest extends Mathsat5AbstractNativeApiTest {
   @BeforeClass
   public static void loadMathsat() {
     try {
-      NativeLibraries.loadLibrary("mathsat5j");
+      Mathsat5SolverContext.loadLibrary();
     } catch (UnsatisfiedLinkError e) {
       throw new AssumptionViolatedException("MathSAT5 is not available", e);
     }
