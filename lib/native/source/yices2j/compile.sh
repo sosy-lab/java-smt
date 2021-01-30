325fb2b6ccdfd8ddb78ce867df6b8c9b28671c23
Karlheinz Friedberger <kfriedberger@gmail.com>
2021-01-30 12:56:41 +0100

    adding documentation info compile script.
    and making compile scripts more similar in structure.

--- lib/native/source/yices2j/compile.sh
+++ lib/native/source/yices2j/compile.sh
@@ -42,6 +42,7 @@ GMP_LIB_DIR=$GMP_HEADER_DIR/.libs
 GPERF_HEADER_DIR=$RELATIVE_ROOT_DIR/"$3"
 GPERF_LIB_DIR=$GPERF_HEADER_DIR/lib
 
+# check requirements
 if [ ! -f "$YICES_LIB_DIR/libyices.a" ]; then
     echo "You need to specify the directory with the downloaded and compiled Yices on the command line!"
     echo "Can not find $YICES_LIB_DIR/libyices.a"
@@ -50,7 +51,7 @@ fi
 if [ ! -f "$GMP_LIB_DIR/libgmp.a" ]; then
     echo "You need to specify the GMP directory on the command line!"
     echo "Can not find $GMP_LIB_DIR/libgmp.a"
-	exit 1
+    exit 1
 fi
 if [ ! -f "$GPERF_LIB_DIR/libgp.a" ]; then
     echo "You need to specify the GPERF directory on the command line!"
@@ -63,43 +64,49 @@ OBJ_FILES="org_sosy_1lab_java_1smt_solvers_yices2_Yices2NativeApi.o"
 
 OUT_FILE="libyices2j.so"
 
-echo "Compiling the C wrapper code and creating the \"$OUT_FILE\" library"
+echo "Compiling the C wrapper code and creating the \"$OUT_FILE\" library..."
 
 # This will compile the JNI wrapper part, given the JNI and the Yices header files
 gcc -g -std=gnu99 -Wall -Wextra -Wpedantic -Wno-return-type -Wno-unused-parameter \
     $JNI_HEADERS -I$YICES_SRC_DIR -I$GMP_HEADER_DIR -I$GPERF_HEADER_DIR $SRC_FILES -fPIC -c
+
 echo "Compilation Done"
+echo "Linking libraries together into one file..."
 
 # This will link together the file produced above, the Yices library, the GMP library and the standard libraries.
 # Everything except the standard libraries is included statically.
-# The result is a shared library.
+# The result is a single shared library containing all necessary components.
 if [ `uname -m` = "x86_64" ]; then
-	gcc -Wall -g -o $OUT_FILE -shared -Wl,-soname,libyices2j.so \
+    gcc -Wall -g -o $OUT_FILE -shared -Wl,-soname,libyices2j.so \
     -L. -L$YICES_LIB_DIR -L$GMP_LIB_DIR -L$GPERF_LIB_DIR \
     -I$GMP_HEADER_DIR -I$GPERF_HEADER_DIR $OBJ_FILES -Wl,-Bstatic \
     -lyices -lgmpxx -lgmp -lgp -static-libstdc++ -lstdc++ \
     -Wl,-Bdynamic -lc -lm -Wl,--version-script=libyices2j.version
 else
-	gcc -Wall -g -o ${OUT_FILE} -shared -Wl,-soname,libyices2j.so \
+    # TODO compiling for/on a 32bit system was not done for quite a long time. We should drop it.
+    gcc -Wall -g -o ${OUT_FILE} -shared -Wl,-soname,libyices2j.so \
     -L${YICES_LIB_DIR} -L${GMP_LIB_DIR} -L${GPERF_LIB_DIR} \
     -I${GMP_HEADER_DIR} -I${GPERF_HEADER_DIR} ${OBJ_FILES} \
     -Wl,-Bstatic -lyices -lgmpxx -lgmp -Wl,-Bdynamic -lc -lm -lstdc++
 fi
 
-
-if [ $? -eq 0 ]; then
-	strip ${OUT_FILE}
-else
-	echo "There was a problem during compilation of
-	\"org_sosy_1lab_java_1smt_solvers_yices2_Yices2NativeApi.c\""
-	exit 1
+if [ $? -ne 0 ]; then
+    echo "There was a problem during compilation of \"org_sosy_1lab_java_1smt_solvers_yices2_Yices2NativeApi.c\""
+    exit 1
 fi
+
+echo "Linking Done"
+echo "Reducing file size by dropping unused symbols..."
+
+strip ${OUT_FILE}
+
+echo "Reduction Done"
 
 MISSING_SYMBOLS="$(readelf -Ws ${OUT_FILE} | grep NOTYPE | grep GLOBAL | grep UND)"
 if [ ! -z "$MISSING_SYMBOLS" ]; then
-	echo "Warning: There are the following unresolved dependencies in libyices2j.so:"
-	readelf -Ws ${OUT_FILE} | grep NOTYPE | grep GLOBAL | grep UND
-	exit 1
+    echo "Warning: There are the following unresolved dependencies in libyices2j.so:"
+    readelf -Ws ${OUT_FILE} | grep NOTYPE | grep GLOBAL | grep UND
+    exit 1
 fi
 
 echo "All Done"
