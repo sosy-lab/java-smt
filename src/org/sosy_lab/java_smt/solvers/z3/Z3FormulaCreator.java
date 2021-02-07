0ff3c711e9844e8ca3b9cfd7e9038c36ce0bd1b7
Karlheinz Friedberger <kfriedberger@gmail.com>
2021-02-07 21:23:58 +0100

    fix checkstyle warning.

--- src/org/sosy_lab/java_smt/solvers/z3/Z3FormulaCreator.java
+++ src/org/sosy_lab/java_smt/solvers/z3/Z3FormulaCreator.java
@@ -31,7 +31,7 @@ import java.util.IdentityHashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import javax.annotation.Nullable;
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.sosy_lab.common.ShutdownNotifier;
 import org.sosy_lab.common.configuration.Configuration;
 import org.sosy_lab.common.configuration.InvalidConfigurationException;
