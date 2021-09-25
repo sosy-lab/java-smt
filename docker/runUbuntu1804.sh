0efbd562e268a31a42c109b6c861641da1f92eb9
Karlheinz Friedberger <kfriedberger@gmail.com>
2021-09-25 10:43:40 +0200

    adding license for Reuse.

--- docker/runUbuntu1804.sh
+++ docker/runUbuntu1804.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# This file is part of JavaSMT,
+# an API wrapper for a collection of SMT solvers:
+# https://github.com/sosy-lab/java-smt
+#
+# SPDX-FileCopyrightText: 2021 Dirk Beyer <https://www.sosy-lab.org>
+#
+# SPDX-License-Identifier: Apache-2.0
+
 # specific for my system:
 # JavaSMT and all solver files are located in the directory "workspace".
 WORKSPACE=$HOME/workspace
