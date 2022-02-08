7aa725e94abbf11d0f7e0d341667f7f20fda4a5a
baierd <baier-daniel@gmx.net>
2022-02-08 19:11:34 +0100

    Add CVC5UFManager

---
+++ src/org/sosy_lab/java_smt/solvers/cvc5/CVC5UFManager.java
@@ -0,0 +1,21 @@
+// This file is part of JavaSMT,
+// an API wrapper for a collection of SMT solvers:
+// https://github.com/sosy-lab/java-smt
+//
+// SPDX-FileCopyrightText: 2020 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.sosy_lab.java_smt.solvers.cvc5;
+
+import io.github.cvc5.api.Solver;
+import io.github.cvc5.api.Sort;
+import io.github.cvc5.api.Term;
+import org.sosy_lab.java_smt.basicimpl.AbstractUFManager;
+
+class CVC5UFManager extends AbstractUFManager<Term, Term, Sort, Solver> {
+
+  CVC5UFManager(CVC5FormulaCreator pCreator) {
+    super(pCreator);
+  }
+}
