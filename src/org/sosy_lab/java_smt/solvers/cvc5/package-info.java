a0491aaba32082e262fc615b34eda31a15807f09
baierd <baier-daniel@gmx.net>
2022-02-08 19:07:57 +0100

    Add CVC5 package and package-info

---
+++ src/org/sosy_lab/java_smt/solvers/cvc5/package-info.java
@@ -0,0 +1,10 @@
+// This file is part of JavaSMT,
+// an API wrapper for a collection of SMT solvers:
+// https://github.com/sosy-lab/java-smt
+//
+// SPDX-FileCopyrightText: 2020 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+/** Interface to the SMT solver CVC5 (based on CVC5s own Java bindings). */
+package org.sosy_lab.java_smt.solvers.cvc5;
