e00cda3b49ba54db715632b2fe31ba811bf0eb52
baierd <baier-daniel@gmx.net>
2022-03-22 19:02:59 +0100

    Add rounding mode visitation to the visitor for CVC5

--- src/org/sosy_lab/java_smt/solvers/cvc5/CVC5FormulaCreator.java
+++ src/org/sosy_lab/java_smt/solvers/cvc5/CVC5FormulaCreator.java
@@ -331,6 +331,9 @@ public class CVC5FormulaCreator extends FormulaCreator<Term, Sort, Solver, Term>
       } else if (f.isFloatingPointValue()) {
         return visitor.visitConstant(formula, f.toString());
 
+      } else if (f.getKind() == Kind.CONST_ROUNDINGMODE) {
+        return visitor.visitConstant(formula, f.toString());
+
       } else if (f.getKind() == Kind.VARIABLE) {
         // Bound and unbound vars are the same in CVC5!
         // BOUND vars are used for all vars that are bound to a quantifier in CVC5.
