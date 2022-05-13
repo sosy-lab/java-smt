e2d548bb636a21af25696de1dad0951e70fc039e
Karlheinz Friedberger <kfriedberger@gmail.com>
2022-05-13 20:48:07 +0200

    sync from SoSy project template.

--- build/deploy-gh-pages.sh
+++ build/deploy-gh-pages.sh
@@ -32,8 +32,8 @@ git init
 git add .
 git commit -m "Deploy to GitHub Pages"
 
-# Force push from the current repo's master branch to the remote
+# Force push from the current repo's HEAD to the remote
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" HEAD:gh-pages > /dev/null 2>&1
