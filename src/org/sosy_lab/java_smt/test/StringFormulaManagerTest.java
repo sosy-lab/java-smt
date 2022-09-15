ae3a80b9d36e84b6dfa1cde5fdb3ac421cc88159
Karlheinz Friedberger <kfriedberger@gmail.com>
2022-09-15 23:44:09 +0200

    improve unit test for allChar.

--- src/org/sosy_lab/java_smt/test/StringFormulaManagerTest.java
+++ src/org/sosy_lab/java_smt/test/StringFormulaManagerTest.java
@@ -148,23 +148,24 @@ public class StringFormulaManagerTest extends SolverBasedTest0 {
   public void testRegexAllCharUnicode() throws SolverException, InterruptedException {
     RegexFormula regexAllChar = smgr.allChar();
 
-    if (solverUnderTest == Solvers.CVC4) {
-      // CVC4 does not support Unicode characters
-      assertThatFormula(smgr.in(smgr.makeString("\\u0394"), regexAllChar)).isUnsatisfiable();
+    // Single characters.
+    assertThatFormula(smgr.in(smgr.makeString("\\u0394"), regexAllChar)).isSatisfiable();
+    assertThatFormula(smgr.in(smgr.makeString("\\u{1fa6a}"), regexAllChar)).isSatisfiable();
 
-    } else {
+    // Combining characters are not matched as one character.
+    assertThatFormula(smgr.in(smgr.makeString("a\\u0336"), regexAllChar)).isUnsatisfiable();
+    assertThatFormula(smgr.in(smgr.makeString("\\n"), regexAllChar)).isUnsatisfiable();
+
+    if (solverUnderTest != Solvers.CVC4) {
+      // CVC4 does not support Unicode characters.
       // Z3 and other solvers support Unicode characters in the theory of strings.
-      assertThatFormula(smgr.in(smgr.makeString("\\u0394"), regexAllChar)).isSatisfiable();
-      assertThatFormula(smgr.in(smgr.makeString("\\u{1fa6a}"), regexAllChar)).isSatisfiable();
       assertThatFormula(
               smgr.in(
                   smgr.makeVariable("x"), smgr.intersection(smgr.range('a', 'Δ'), regexAllChar)))
           .isSatisfiable();
       // Combining characters are not matched as one character.
-      assertThatFormula(smgr.in(smgr.makeString("a\\u0336"), regexAllChar)).isUnsatisfiable();
       // Non-ascii non-printable characters should use the codepoint representation
       assertThatFormula(smgr.in(smgr.makeString("Δ"), regexAllChar)).isUnsatisfiable();
-      assertThatFormula(smgr.in(smgr.makeString("\\n"), regexAllChar)).isUnsatisfiable();
     }
   }
 
