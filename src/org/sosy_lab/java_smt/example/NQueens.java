a133c65bb95b924e49d9b2b193d5945775bc4f5c
Athul Krishna <97959765+AKomplished-bug@users.noreply.github.com>
2023-03-29 09:59:48 +0530

    removed additional empty lines

--- src/org/sosy_lab/java_smt/example/NQueens.java
+++ src/org/sosy_lab/java_smt/example/NQueens.java
@@ -2,15 +2,13 @@ package org.sosy_lab.java_smt.example;
 /*
  *  JavaSMT is an API wrapper for a collection of SMT solvers.
  *  This file is part of JavaSMT.
- *
  *  Copyright (C) 2007-2016  Dirk Beyer
  *  All rights reserved.
- *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *   
+ *        http://www.apache.org/licenses/LICENSE-2.0
  *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,12 +17,11 @@ package org.sosy_lab.java_smt.example;
  *  limitations under the License.
  */
 
-/**
- * This example program solves a NQueens problem of given size and prints a possible solution.
+/**  This example program solves a NQueens problem of given size and prints a possible solution.
  *
- * <p>For example, the Queen can be placed in these ways for a field size of 4:
+ *   <p>For example, the Queen can be placed in these ways for a field size of 4:
  *
- * <pre>
+ *  <pre>
  *   .Q..
  *   ...Q
  *   Q...
@@ -98,8 +95,7 @@ abstract class NQueensSolver {
     abstract List<BooleanFormula> getRules(BooleanFormula[][] symbols, SolverContext context);
     abstract Boolean getValue(BooleanFormula[][] symbols, Model model, int row, int col);
 
-    /**
-     * Solves the N-Queens problem for the given board size and returns a possible solution.
+    /** Solves the N-Queens problem for the given board size and returns a possible solution.
      * Returns <code>Null</code> if no solution exists.
      */
     @Nullable
@@ -134,8 +130,7 @@ class NQueen extends NQueensSolver {
         this.n = n;
     }
 
-    /**
-     * prepare symbols: one symbol for each of the N*N cells.
+    /** prepare symbols: one symbol for each of the N*N cells.
      */
     @Override
     BooleanFormula[][] getSymbols() {
@@ -147,32 +142,27 @@ class NQueen extends NQueensSolver {
         }
         return symbols;
     }
-    /**
-
-     * This method generates a list of rules that represent the constraints for the N-Queens problem
-
+    /** This method generates a list of rules that represent the constraints for the N-Queens problem
      * @param symbols: a 2D boolean array representing the placement of the queens on the board
-
      * @param context: SolverContext instance
 
-     * @return List<BooleanFormula>: a list of boolean formulas that represent the constraints for
-     *  the problem
-     */
+      * @return List<BooleanFormula>: a list of boolean formulas that represent the constraints for
+      *  the problem
+      */
     @Override
     public List<BooleanFormula> getRules(BooleanFormula[][] symbols, SolverContext context) {
         List<BooleanFormula> rules = new ArrayList<>();
         int n = symbols.length;
 
-        /*Rule 1: At least one queen per row,
+        /* Rule 1: At least one queen per row,
         or we can say make sure that there are N Queens on the board
-        *
         */
         for (BooleanFormula[] rowSymbols : symbols) {
             List<BooleanFormula> clause = new ArrayList<>(Arrays.asList(rowSymbols).subList(0, n));
             rules.add(this.bmgr.or(clause));
         }
 
-        /*Rule 2: Add constraints to ensure that at most one queen is placed in each row.
+        /* Rule 2: Add constraints to ensure that at most one queen is placed in each row.
          * For n=4:
          *   0123
          * 0 ----
