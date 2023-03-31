2b28bddda7d383004573582d70ffe3abeb409301
Athul0609 <athulkrishna8781@gmail.com>
2023-03-31 13:54:00 +0530

    Final changes

--- src/org/sosy_lab/java_smt/example/NQueens.java
+++ src/org/sosy_lab/java_smt/example/NQueens.java
@@ -58,7 +58,7 @@ public class NQueens {
     Solvers solver = Solvers.SMTINTERPOL;
     try (SolverContext context = SolverContextFactory.createSolverContext(config,
         logger, notifier, solver)) {
-      try(Scanner sc = new Scanner(System.in,
+      try (Scanner sc = new Scanner(System.in,
           Charset.defaultCharset().name())) {
         //Takes input from the user for number of queens to be placed
         System.out.println("Enter the number of Queens to be "
