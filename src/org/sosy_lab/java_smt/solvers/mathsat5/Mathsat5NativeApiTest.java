4d86760b78cf7249c72ee9b639ec1d0efe13fbae
Karlheinz Friedberger <kfriedberger@gmail.com>
2023-05-09 20:34:13 +0200

    disable enum logic for MathSAT5 until we get a new release.

--- src/org/sosy_lab/java_smt/solvers/mathsat5/Mathsat5NativeApiTest.java
+++ src/org/sosy_lab/java_smt/solvers/mathsat5/Mathsat5NativeApiTest.java
@@ -482,6 +482,7 @@ public class Mathsat5NativeApiTest extends Mathsat5AbstractNativeApiTest {
   }
 
   @Test
+  @Ignore // disable until we have MathSAT 5.6.9 or later
   public void enumTypeTest() throws SolverException, InterruptedException {
     String[] colors = {"blue", "red", "green"};
 
