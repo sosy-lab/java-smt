008dcacfb78c12a736ed7c138bc30b5ac67c790c
Daniel Raffler <daniel-raffler@gmx.de>
2023-05-14 02:36:50 +0200

    Fixed checkstyle errors

--- src/org/sosy_lab/java_smt/solvers/opensmt/OpenSmtNativeAPITest.java
+++ src/org/sosy_lab/java_smt/solvers/opensmt/OpenSmtNativeAPITest.java
@@ -6,14 +6,28 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-package org.sosy_lob.java_smt.solvers.opensmt;
+package org.sosy_lab.java_smt.solvers.opensmt;
 
-import static org.junit.Assert.assertTrue;
+import static com.google.common.truth.Truth.assertThat;
 
-import org.sosy_lab.common.NativeLibraries;
+import opensmt.ArithLogic;
+import opensmt.InterpolationContext;
+import opensmt.ItpAlgorithm;
+import opensmt.Logic;
+import opensmt.MainSolver;
+import opensmt.OpenSmt;
+import opensmt.PTRef;
+import opensmt.SMTConfig;
+import opensmt.SRef;
+import opensmt.SymRef;
+import opensmt.TemplateFunction;
+import opensmt.VectorInt;
+import opensmt.VectorPTRef;
+import opensmt.VectorSRef;
+import opensmt.opensmt_logic;
+import opensmt.sstat;
 import org.junit.Test;
-
-import opensmt.*;
+import org.sosy_lab.common.NativeLibraries;
 
 public class OpenSmtNativeAPITest {
   static {
@@ -21,17 +35,15 @@ public class OpenSmtNativeAPITest {
     NativeLibraries.loadLibrary("opensmtjava");
   }
 
-  /**
-   * Return free variables
-   */
+  /** Return free variables. */
   private VectorPTRef variables(Logic logic, PTRef term) {
-    if(logic.isVar(term)) {
+    if (logic.isVar(term)) {
       VectorPTRef vars = new VectorPTRef();
       vars.add(term);
       return vars;
     } else {
       VectorPTRef vars = new VectorPTRef();
-      for(PTRef sub : logic.getSubterms(term)) {
+      for (PTRef sub : logic.getSubterms(term)) {
         VectorPTRef res = variables(logic, sub);
         vars.addAll(res);
       }
@@ -39,47 +51,48 @@ public class OpenSmtNativeAPITest {
     }
   }
 
-  /**
-   * Verify that interpolant conditions are fullfilled
-   * See VerificationUtils.h in OpenSmt
-   */
+  /** Verify that interpolant conditions are fullfilled See VerificationUtils.h in OpenSmt. */
   private boolean verifyInterpolant(Logic logic, PTRef partA, PTRef partB, PTRef interpol) {
     SMTConfig config = new SMTConfig();
     MainSolver solver = new MainSolver(logic, config, "opensmt-verifier");
-    
+
     // Check A ⊨ I
     solver.push(logic.mkNot(logic.mkImpl(partA, interpol)));
-    if(!solver.check().isFalse()) return false;
+    if (!solver.check().isFalse()) {
+      return false;
+    }
     solver.pop();
-    
+
     // Check I ⊨ ¬B
     solver.push(logic.mkNot(logic.mkImpl(interpol, logic.mkNot(partB))));
-    if(!solver.check().isFalse()) return false;
+    if (!solver.check().isFalse()) {
+      return false;
+    }
     solver.pop();
-    
-    VectorPTRef vars_partA = variables(logic, partA);
-    VectorPTRef vars_partB = variables(logic, partB);
-    VectorPTRef vars_interpol = variables(logic, interpol);
+
+    VectorPTRef varsPartA = variables(logic, partA);
+    VectorPTRef varsPartB = variables(logic, partB);
+    VectorPTRef varsInterpol = variables(logic, interpol);
 
     // Check symb(I) ⊆ symb(A) ∩ symb(B)
-    return vars_partA.containsAll(vars_interpol) && vars_partB.containsAll(vars_interpol);
-    }
-  
+    return varsPartA.containsAll(varsInterpol) && varsPartB.containsAll(varsInterpol);
+  }
+
   @Test
   public void testBooleanLogic() {
     OpenSmt osmt = new OpenSmt(opensmt_logic.qf_bool, "opensmt-test", false);
     Logic logic = osmt.getLogic();
 
     // a ∧ ¬a
-    PTRef a  = logic.mkBoolVar("a");
-    PTRef na = logic.mkNot(a);
-    PTRef f  = logic.mkAnd(a, na);
-    
+    PTRef varA = logic.mkBoolVar("a");
+    PTRef notA = logic.mkNot(varA);
+    PTRef f = logic.mkAnd(varA, notA);
+
     MainSolver mainSolver = osmt.getMainSolver();
     mainSolver.push(f);
-    
+
     sstat r = mainSolver.check();
-    assertTrue(r.isFalse());
+    assertThat(r.isFalse()).isTrue();
   }
 
   @Test
@@ -88,44 +101,44 @@ public class OpenSmtNativeAPITest {
     Logic logic = osmt.getLogic();
 
     // Declare sort U
-    SRef mysort = logic.declareUninterpretedSort("U");
+    SRef mySort = logic.declareUninterpretedSort("U");
 
     // Declare variable a, b
-    PTRef var_a = logic.mkVar(mysort, "a");
-    PTRef var_b = logic.mkVar(mysort, "b");
+    PTRef varA = logic.mkVar(mySort, "a");
+    PTRef varB = logic.mkVar(mySort, "b");
 
     // Term a=b
-    PTRef f0 = logic.mkEq(var_a, var_b);
-    
+    PTRef f0 = logic.mkEq(varA, varB);
+
     // Declare function f(x)
-    VectorSRef sig_f = new VectorSRef();
-    sig_f.add(mysort);
-    SymRef uf_f = logic.declareFun("f", mysort, sig_f);
+    VectorSRef sigF = new VectorSRef();
+    sigF.add(mySort);
+    SymRef funF = logic.declareFun("f", mySort, sigF);
 
     // Instantiate f for a
-    VectorPTRef arg_fa = new VectorPTRef();
-    arg_fa.add(var_a);
-    PTRef app_fa = logic.mkUninterpFun(uf_f, arg_fa);
+    VectorPTRef argFa = new VectorPTRef();
+    argFa.add(varA);
+    PTRef appFa = logic.mkUninterpFun(funF, argFa);
 
     // Instantiate f for b
-    VectorPTRef arg_fb = new VectorPTRef();
-    arg_fb.add(var_b);
-    PTRef app_fb = logic.mkUninterpFun(uf_f, arg_fb);
-    
+    VectorPTRef argFb = new VectorPTRef();
+    argFb.add(varB);
+    PTRef appFb = logic.mkUninterpFun(funF, argFb);
+
     // Term f(a)≠f(b)
     VectorPTRef dist = new VectorPTRef();
-    dist.add(app_fa);
-    dist.add(app_fb);
+    dist.add(appFa);
+    dist.add(appFb);
     PTRef f1 = logic.mkDistinct(dist);
-    
+
     MainSolver mainSolver = osmt.getMainSolver();
     mainSolver.push(f0);
     mainSolver.push(f1);
-    
+
     sstat r = mainSolver.check();
-    assertTrue(r.isFalse());
+    assertThat(r.isFalse()).isTrue();
   }
-  
+
   @Test
   public void testUninterpretedFunctionInterpol() {
     OpenSmt osmt = new OpenSmt(opensmt_logic.qf_uf, "opensmt-test", true);
@@ -135,17 +148,17 @@ public class OpenSmtNativeAPITest {
     SRef mysort = logic.declareUninterpretedSort("U");
 
     // Declare variable a, b, c
-    PTRef var_a = logic.mkVar(mysort, "a");
-    PTRef var_b = logic.mkVar(mysort, "b");
-    PTRef var_c = logic.mkVar(mysort, "c");
+    PTRef varA = logic.mkVar(mysort, "a");
+    PTRef varB = logic.mkVar(mysort, "b");
+    PTRef varC = logic.mkVar(mysort, "c");
 
     // Term a=b ∧ b=c
-    PTRef f0 = logic.mkAnd(logic.mkEq(var_a, var_b), logic.mkEq(var_b, var_c));
-    
+    PTRef f0 = logic.mkAnd(logic.mkEq(varA, varB), logic.mkEq(varB, varC));
+
     // Term a≠c
     VectorPTRef dist = new VectorPTRef();
-    dist.add(var_a);
-    dist.add(var_c);
+    dist.add(varA);
+    dist.add(varC);
     PTRef f1 = logic.mkDistinct(dist);
 
     MainSolver mainSolver = osmt.getMainSolver();
@@ -153,142 +166,143 @@ public class OpenSmtNativeAPITest {
     mainSolver.push(f1);
 
     sstat r = mainSolver.check();
-    assertTrue(r.isFalse());
-    
+    assertThat(r.isFalse()).isTrue();
+
     InterpolationContext context = mainSolver.getInterpolationContext();
     VectorInt mask = new VectorInt();
     mask.add(0);
-    
+
     // Verify interpolant
     PTRef interpol = context.getSingleInterpolant(mask);
-    assertTrue(verifyInterpolant(logic, f0, f1, interpol));
+    assertThat(verifyInterpolant(logic, f0, f1, interpol)).isTrue();
   }
-  
+
   @Test
   public void testLinearIntegerLogic() {
     OpenSmt osmt = new OpenSmt(opensmt_logic.qf_lia, "opensmt-test", false);
     ArithLogic logic = osmt.getLIALogic();
 
     // Declare variables
-    PTRef var_a = logic.mkIntVar("a");
-    PTRef var_c = logic.mkIntVar("c");
+    PTRef varA = logic.mkIntVar("a");
+    PTRef varC = logic.mkIntVar("c");
 
     // Create integer constants
-    PTRef const_0 = logic.mkIntConst("0");
-    PTRef const_3 = logic.mkIntConst("3");
+    PTRef const0 = logic.mkIntConst("0");
+    PTRef const3 = logic.mkIntConst("3");
 
     // Terms a+3 < c, c ≥ 0
-    PTRef f_0 = logic.mkLt(logic.mkPlus(var_a, const_3), var_c);
-    PTRef f_1 = logic.mkGeq(var_c, const_0);
-    
+    PTRef f0 = logic.mkLt(logic.mkPlus(varA, const3), varC);
+    PTRef f1 = logic.mkGeq(varC, const0);
+
     MainSolver mainSolver = osmt.getMainSolver();
-    mainSolver.push(f_0);
-    mainSolver.push(f_1);
-    
+    mainSolver.push(f0);
+    mainSolver.push(f1);
+
     sstat r = mainSolver.check();
-    assertTrue(r.isTrue());
+    assertThat(r.isTrue()).isTrue();
   }
-  
+
   @Test
   public void testLinearIntegerInterpol() {
     OpenSmt osmt = new OpenSmt(opensmt_logic.qf_lia, "opensmt-test", true);
     ArithLogic logic = osmt.getLIALogic();
 
     // Declare variables
-    PTRef var_a = logic.mkIntVar("a");
-    PTRef var_b = logic.mkIntVar("b");
-    PTRef var_c = logic.mkIntVar("c");
+    PTRef varA = logic.mkIntVar("a");
+    PTRef varB = logic.mkIntVar("b");
+    PTRef varC = logic.mkIntVar("c");
 
     // Term a>b ∧ b>c
-    PTRef f0 = logic.mkAnd(logic.mkGt(var_a, var_b), logic.mkGt(var_b, var_c));
+    PTRef f0 = logic.mkAnd(logic.mkGt(varA, varB), logic.mkGt(varB, varC));
 
     // Term c>a
-    PTRef f1 = logic.mkGt(var_c, var_a);
+    PTRef f1 = logic.mkGt(varC, varA);
 
     MainSolver solver = osmt.getMainSolver();
     solver.push(f0);
     solver.push(f1);
-    
+
     sstat r = solver.check();
-    assertTrue(r.isFalse());
-    
+    assertThat(r.isFalse()).isTrue();
+
     InterpolationContext context = solver.getInterpolationContext();
     VectorInt mask = new VectorInt();
     mask.add(0);
-    
+
     // Verify interpolant
     PTRef interpol = context.getSingleInterpolant(mask);
-    assertTrue(verifyInterpolant(logic, f0, f1, interpol));
-    
+    assertThat(verifyInterpolant(logic, f0, f1, interpol)).isTrue();
+
     // Switch interpolation algorithm
     SMTConfig config = osmt.getConfig();
     config.setLRAInterpolationAlgorithm(ItpAlgorithm.getLraDecomposingWeak());
-    
+
     // Verify second interpolant
-    PTRef interpol_dweak = context.getSingleInterpolant(mask);
-    assertTrue(verifyInterpolant(logic, f0, f1, interpol_dweak));
+    PTRef interpolDweak = context.getSingleInterpolant(mask);
+    assertThat(verifyInterpolant(logic, f0, f1, interpolDweak)).isTrue();
   }
-  
+
   @Test
   public void testIntegerArray() {
     // TODO: add array test
   }
-  
+
   @Test
   public void testFormulaIntrospection() {
     OpenSmt osmt = new OpenSmt(opensmt_logic.qf_lia, "opensmt-test", false);
     ArithLogic logic = osmt.getLIALogic();
-    
+
     // Declare variables
-    PTRef var_a = logic.mkIntVar("a");
-    PTRef var_b = logic.mkIntVar("b");
-    PTRef var_c = logic.mkIntVar("c");
-    
-    // Term a+b < -c 
-    PTRef f = logic.mkLt(logic.mkPlus(var_a, var_b), logic.mkNeg(var_c));
+    PTRef varA = logic.mkIntVar("a");
+    PTRef varB = logic.mkIntVar("b");
+    PTRef varC = logic.mkIntVar("c");
+
+    // Term a+b < -c
+    PTRef f = logic.mkLt(logic.mkPlus(varA, varB), logic.mkNeg(varC));
 
     // Check that there are 3 variables in the term
     VectorPTRef vars = variables(logic, f);
-    assertTrue(vars.size() == 3);
+    assertThat(vars.size() == 3).isTrue();
   }
-  
+
   @Test
   public void testFunctionTemplates() {
     OpenSmt osmt = new OpenSmt(opensmt_logic.qf_lia, "opensmt-test", false);
     ArithLogic logic = osmt.getLIALogic();
-    
+
     // Define function negate(a) = -1*a
 
     // FIXME: This will fail the test. Are formal arguments not scoped?
     // PTRef negate_a = logic.mkIntVar("a");
-    
-    PTRef negate_a = logic.mkIntVar("negate_a");
-    PTRef negate_body = logic.mkTimes(logic.getTerm_IntMinusOne(), negate_a);
-    
-    VectorPTRef negate_args = new VectorPTRef();
-    negate_args.add(negate_a);
-    TemplateFunction negate_tf = new TemplateFunction("negate", negate_args, logic.getSort_int(), negate_body);
-    
+
+    PTRef negateA = logic.mkIntVar("negate_a");
+    PTRef negateBody = logic.mkTimes(logic.getTerm_IntMinusOne(), negateA);
+
+    VectorPTRef negateArgs = new VectorPTRef();
+    negateArgs.add(negateA);
+    TemplateFunction negateF =
+        new TemplateFunction("negate", negateArgs, logic.getSort_int(), negateBody);
+
     // Declare variable a
-    PTRef var_a = logic.mkIntVar("a");
-    
+    PTRef varA = logic.mkIntVar("a");
+
     // Instantiate negate for a
-    VectorPTRef arg_0 = new VectorPTRef();
-    arg_0.add(var_a);
-    PTRef app_0 = logic.instantiateFunctionTemplate(negate_tf, arg_0);
-    
+    VectorPTRef arg0 = new VectorPTRef();
+    arg0.add(varA);
+    PTRef app0 = logic.instantiateFunctionTemplate(negateF, arg0);
+
     // Instantiate negate for negate(a)
-    VectorPTRef arg_1 = new VectorPTRef();
-    arg_1.add(app_0);
-    PTRef app_1 = logic.instantiateFunctionTemplate(negate_tf, arg_1);
-    
+    VectorPTRef arg1 = new VectorPTRef();
+    arg1.add(app0);
+    PTRef app1 = logic.instantiateFunctionTemplate(negateF, arg1);
+
     // Term negate(negate(a)) ≠ a
-    PTRef f = logic.mkNot(logic.mkEq(app_1, var_a));
-    
+    PTRef f = logic.mkNot(logic.mkEq(app1, varA));
+
     MainSolver solver = osmt.getMainSolver();
     solver.push(f);
-    
-    sstat r = solver.check();    
-    assertTrue(r.isFalse());
-  }  
+
+    sstat r = solver.check();
+    assertThat(r.isFalse()).isTrue();
+  }
 }
--- src/org/sosy_lab/java_smt/solvers/opensmt/package-info.java
+++ src/org/sosy_lab/java_smt/solvers/opensmt/package-info.java
@@ -11,4 +11,4 @@
 @javax.annotation.ParametersAreNonnullByDefault
 @org.sosy_lab.common.annotations.FieldsAreNonnullByDefault
 @org.sosy_lab.common.annotations.ReturnValuesAreNonnullByDefault
-package org.sosy_lab.java_smt.solvers.opemsmt;
+package org.sosy_lab.java_smt.solvers.opensmt;
