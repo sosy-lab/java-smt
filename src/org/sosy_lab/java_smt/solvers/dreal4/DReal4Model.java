4af6a01fec61dd586c5ad85ae6ce3f8d0fe7c0b1
Julius <julius.brehme@googlemail.com>
2023-08-05 16:40:24 +0200

    added evalImpl

--- src/org/sosy_lab/java_smt/solvers/dreal4/DReal4Model.java
+++ src/org/sosy_lab/java_smt/solvers/dreal4/DReal4Model.java
@@ -20,12 +20,19 @@
 
 package org.sosy_lab.java_smt.solvers.dreal4;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
+import java.util.HashMap;
+import java.util.Map;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.sosy_lab.java_smt.basicimpl.AbstractModel;
 import org.sosy_lab.java_smt.solvers.dreal4.drealjni.Box;
 import org.sosy_lab.java_smt.solvers.dreal4.drealjni.Context;
+import org.sosy_lab.java_smt.solvers.dreal4.drealjni.Expression;
+import org.sosy_lab.java_smt.solvers.dreal4.drealjni.Formula;
+import org.sosy_lab.java_smt.solvers.dreal4.drealjni.Variable;
 import org.sosy_lab.java_smt.solvers.dreal4.drealjni.Variable.Type;
+import org.sosy_lab.java_smt.solvers.dreal4.drealjni.dreal;
 
 
 public class DReal4Model extends AbstractModel<DRealTerm<?, ?>, Type, Context> {
@@ -46,9 +53,61 @@ public class DReal4Model extends AbstractModel<DRealTerm<?, ?>, Type, Context> {
     return null;
   }
 
+  // This will always return True, different solution?
   @Override
   protected @Nullable DRealTerm<?, ?> evalImpl(DRealTerm<?, ?> formula) {
-    return null;
+    Preconditions.checkState(formula.isFormula());
+    Formula f = formula.getFormula();
+    HashMap<Variable, Double> result = extractResults();
+    for (Map.Entry<Variable, Double> entry : result.entrySet()) {
+      Variable var = entry.getKey();
+      Double res = entry.getValue();
+      if (res.isNaN()) {
+        // When is result "EMPTY"?
+        return null;
+      } else {
+        if (var.get_type() == Type.BOOLEAN) {
+          if (res > 0) {
+            f.Substitute(var, Formula.True());
+          } else {
+            f.Substitute(var, Formula.False());
+          }
+        } else {
+          f.Substitute(var, new Expression(res));
+        }
+      }
+    }
+    return new DRealTerm<>(f, formula.getType(), formula.getFormulaKind());
   }
 
+
+  /**
+   * This function extracts the results. The function iterates through the model (Box) and gets the
+   * variable and calls getResult to get the value associated with the variable
+   * @return HashMap with variable as key and result as Double as value
+   */
+  private HashMap<Variable, Double> extractResults() {
+    HashMap<Variable, Double> result = new HashMap<>();
+    Variable var;
+    String res;
+    for (int i = 0; i < model.size(); i++) {
+      var = model.variable(i);
+      res = dreal.getResult(model, i);
+      result.put(var, parseResult(res));
+    }
+    return result;
+  }
+
+  private double parseResult(String string) {
+    if (string.equals("EMPTY")) {
+      return Double.NaN;
+    } else if (string.equals("ENTIRE")) {
+      // probably unnecassary, and what should I return?
+      return new Double(1);
+    } else {
+      String[] numbers = string.split(",");
+      return new Double(numbers[0] + "." + numbers[1]);
+
+    }
+  }
 }
