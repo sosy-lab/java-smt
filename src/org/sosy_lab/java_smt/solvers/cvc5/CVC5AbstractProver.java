73d49a494308eab58b436a1191e03cf3df490c2b
RSoegtrop <112477861+RSoegtrop@users.noreply.github.com>
2023-08-23 15:07:29 +0200

    removed formula getter

--- src/org/sosy_lab/java_smt/solvers/cvc5/CVC5AbstractProver.java
+++ src/org/sosy_lab/java_smt/solvers/cvc5/CVC5AbstractProver.java
@@ -239,8 +239,4 @@ public class CVC5AbstractProver<T> extends AbstractProverWithAllSat<T> {
     Preconditions.checkState(!closed);
     return assertedFormulas.size() - 1;
   }
-
-  public Deque<List<Term>> getAssertedFormulas() {
-    return assertedFormulas;
-  }
 }
