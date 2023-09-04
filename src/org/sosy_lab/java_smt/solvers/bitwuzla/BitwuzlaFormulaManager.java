31643f25d92743f8709e35d9c95283fa84715496
H-Lindner <heinrich_lindner@tutanota.com>
2023-09-04 23:42:01 +0200

    Added dumping

--- src/org/sosy_lab/java_smt/solvers/bitwuzla/BitwuzlaFormulaManager.java
+++ src/org/sosy_lab/java_smt/solvers/bitwuzla/BitwuzlaFormulaManager.java
@@ -8,12 +8,27 @@
 
 package org.sosy_lab.java_smt.solvers.bitwuzla;
 
+import static org.sosy_lab.java_smt.solvers.yices2.Yices2NativeApi.YICES_APP_TERM;
+import static org.sosy_lab.java_smt.solvers.yices2.Yices2NativeApi.yices_bvtype_size;
+import static org.sosy_lab.java_smt.solvers.yices2.Yices2NativeApi.yices_term_child;
+import static org.sosy_lab.java_smt.solvers.yices2.Yices2NativeApi.yices_term_constructor;
+import static org.sosy_lab.java_smt.solvers.yices2.Yices2NativeApi.yices_term_to_string;
+import static org.sosy_lab.java_smt.solvers.yices2.Yices2NativeApi.yices_type_children;
+import static org.sosy_lab.java_smt.solvers.yices2.Yices2NativeApi.yices_type_is_bitvector;
+import static org.sosy_lab.java_smt.solvers.yices2.Yices2NativeApi.yices_type_num_children;
+import static org.sosy_lab.java_smt.solvers.yices2.Yices2NativeApi.yices_type_of_term;
+import static org.sosy_lab.java_smt.solvers.yices2.Yices2NativeApi.yices_type_to_string;
+
 import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Map;
 import org.sosy_lab.common.Appender;
 import org.sosy_lab.common.Appenders;
 import org.sosy_lab.common.Appenders.AbstractAppender;
 import org.sosy_lab.java_smt.api.BooleanFormula;
 import org.sosy_lab.java_smt.api.Formula;
+import org.sosy_lab.java_smt.api.FormulaType;
 import org.sosy_lab.java_smt.basicimpl.AbstractFormulaManager;
 import org.sosy_lab.java_smt.solvers.bitwuzla.BitwuzlaFormula.BitwuzlaBooleanFormula;
 
@@ -142,13 +157,47 @@ final class BitwuzlaFormulaManager extends AbstractFormulaManager<Long, Long, Lo
   }
 
   @Override
-  public Appender dumpFormula(Long t) {
+  public Appender dumpFormula(Long pTerm) {
+    assert getFormulaCreator().getFormulaType(pTerm) == FormulaType.BooleanType
+        : "Only BooleanFormulas may be dumped";
+    return new Appenders.AbstractAppender() {
 
-    StringBuilder out = new StringBuilder();
-    out.append(bitwuzlaJNI.bitwuzla_term_to_string(t));
-    Appender result = Appenders.createAppender(out);
+      @Override
+      public void appendTo(Appendable out) throws IOException {
+        Map<String, Formula> varsAndUFs =
+            extractVariablesAndUFs(getFormulaCreator().encapsulateWithTypeOf(pTerm));
+        for (Map.Entry<String, Formula> entry : varsAndUFs.entrySet()) {
+          final long currentTerm = ((BitwuzlaFormula) entry.getValue()).getTerm();
+          final long currentType = bitwuzlaJNI.bitwuzla_term_get_sort(currentTerm);
 
-    return result;
+
+          if (bitwuzlaJNI.bitwuzla_term_is_fun(currentTerm)){
+            long[] pDomainSize = new long[1];
+            long pArrayDomainTypes = bitwuzlaJNI.bitwuzla_term_fun_get_domain_sorts(currentTerm,
+                pDomainSize);
+            long numberOfArgs = pDomainSize[0];
+
+            out.append("(declare-fun ");
+            out.append(entry.getKey());
+            out.append(" (");
+            for (int i = 0; i<numberOfArgs; i++) {
+              out.append(bitwuzlaJNI.bitwuzla_sort_to_string(bitwuzlaJNI.BitwuzlaSortArray_getitem(pArrayDomainTypes, i)));
+              out.append(" ");
+            }
+            out.append(") ");
+            out.append(bitwuzlaJNI.bitwuzla_sort_to_string(bitwuzlaJNI.bitwuzla_term_fun_get_codomain_sort(currentTerm)));
+            out.append(")\n");
+          } else {
+            out.append("(declare-const ");
+            out.append(entry.getKey());
+            out.append(" ");
+            out.append(bitwuzlaJNI.bitwuzla_sort_to_string(bitwuzlaJNI.bitwuzla_term_get_sort(currentTerm)));
+            out.append(")\n");
+          }
+        out.append("(assert ").append(bitwuzlaJNI.bitwuzla_term_to_string(pTerm)).append(")");
+      }
+    };
+  };
   }
 
   static long getBitwuzlaTerm(Formula pT) {
