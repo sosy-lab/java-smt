717905ee8812a0ece7a7c3e60e94ff64dcfddf13
BaierD <daniel.baier@sosy.ifi.lmu.de>
2023-10-05 13:59:52 +0200

    Bitwuzla: Add some requirements to UFManagerTests (we should add more tests not based on integers here!)

--- src/org/sosy_lab/java_smt/test/UFManagerTest.java
+++ src/org/sosy_lab/java_smt/test/UFManagerTest.java
@@ -294,6 +294,7 @@ public class UFManagerTest extends SolverBasedTest0.ParameterizedSolverBasedTest
     requireBooleanArgument();
     createAndCallUF("fooBool1", FormulaType.BooleanType, bmgr.makeTrue());
     createAndCallUF("fooBool2", FormulaType.BooleanType, bmgr.makeTrue(), bmgr.makeTrue());
+    requireIntegers();
     createAndCallUF(
         "fooBool3", FormulaType.IntegerType, bmgr.makeTrue(), bmgr.makeTrue(), bmgr.makeTrue());
 
@@ -330,6 +331,7 @@ public class UFManagerTest extends SolverBasedTest0.ParameterizedSolverBasedTest
   @Test
   public void testDeclareAndCallUFWithBVArgs() {
     requireBitvectors();
+    requireIntegers();
     createAndCallUF("fooBV1", FormulaType.getBitvectorTypeWithSize(5), bvmgr.makeBitvector(3, 3L));
     requireBooleanArgument();
     createAndCallUF(
