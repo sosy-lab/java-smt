043e722852752a4cb169a0a7dbb5774755414b07
BaierD <daniel.baier@sosy.ifi.lmu.de>
2023-10-05 18:46:54 +0200

    Bitwuzla: Temporarly disable parsing tests for Bitwuzla

--- src/org/sosy_lab/java_smt/test/SolverBasedTest0.java
+++ src/org/sosy_lab/java_smt/test/SolverBasedTest0.java
@@ -288,7 +288,7 @@ public abstract class SolverBasedTest0 {
     assume()
         .withMessage("Solver %s does not support parsing formulae", solverToUse())
         .that(solverToUse())
-        .isNoneOf(Solvers.CVC4, Solvers.BOOLECTOR, Solvers.YICES2, Solvers.CVC5);
+        .isNoneOf(Solvers.CVC4, Solvers.BOOLECTOR, Solvers.YICES2, Solvers.CVC5, Solvers.BITWUZLA);
   }
 
   protected void requireModel() {
