509db181f7492511e5249ab3d6e6287fc49736c2
Daniel Raffler <daniel-raffler@gmx.de>
2023-12-15 00:31:37 +0100

    Added a minimal test case for bug #339.

---
+++ src/org/sosy_lab/java_smt/test/Bug339Test.java
@@ -0,0 +1,76 @@
+package org.sosy_lab.java_smt.test;
+
+import static com.google.common.truth.TruthJUnit.assume;
+import static org.sosy_lab.java_smt.test.ProverEnvironmentSubject.assertThat;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import org.junit.Test;
+import org.sosy_lab.java_smt.SolverContextFactory.Solvers;
+import org.sosy_lab.java_smt.api.BasicProverEnvironment;
+import org.sosy_lab.java_smt.api.BooleanFormula;
+import org.sosy_lab.java_smt.api.SolverException;
+
+public class Bug339Test extends SolverBasedTest0.ParameterizedSolverBasedTest0 {
+  private final ExecutorService executor = Executors.newSingleThreadExecutor();
+
+  @Test
+  public void brokenTest() throws InterruptedException, ExecutionException {
+    assume().that(solverToUse()).isEqualTo(Solvers.MATHSAT5);
+
+    HardIntegerFormulaGenerator gen = new HardIntegerFormulaGenerator(imgr, bmgr);
+    BooleanFormula formula = gen.generate(8);
+
+    try (BasicProverEnvironment<?> prover = context.newProverEnvironment()) {
+      prover.push(formula);
+
+      Future<?> task1 =
+          executor.submit(
+              () -> {
+                try {
+                  // FIXME: Exception for MathSAT (bug #339)
+                  // java.lang.StackOverflowError
+                  //   at org.sosy_lab.java_smt.solvers.mathsat5.Mathsat5NativeApi.msat_solve
+                  //       (Native Method)
+                  //   at org.sosy_lab.java_smt.solvers.mathsat5.Mathsat5NativeApi.msat_check_sat
+                  //       (Mathsat5NativeApi.java:156)
+                  //   at org.sosy_lab.java_smt.solvers.mathsat5.Mathsat5AbstractProver.isUnsat
+                  //       (Mathsat5AbstractProver.java:106)
+                  //   at org.sosy_lab.java_smt.test.ProverEnvironmentSubject.isUnsatisfiable
+                  //       (ProverEnvironmentSubject.java:67)
+                  //   at ..
+                  assertThat(prover).isUnsatisfiable();
+
+                } catch (SolverException | InterruptedException pE) {
+                  throw new RuntimeException(pE);
+                }
+              });
+
+      assert task1.get() == null;
+    }
+  }
+
+  @Test
+  public void fixedTest() throws InterruptedException, ExecutionException {
+    // Create the ProverEnvironment on the thread that uses it
+    assume().that(solverToUse()).isEqualTo(Solvers.MATHSAT5);
+
+    HardIntegerFormulaGenerator gen = new HardIntegerFormulaGenerator(imgr, bmgr);
+    BooleanFormula formula = gen.generate(8);
+
+    Future<?> task1 =
+        executor.submit(
+            () -> {
+              try (BasicProverEnvironment<?> prover = context.newProverEnvironment()) {
+                prover.push(formula);
+                assertThat(prover).isUnsatisfiable();
+              } catch (SolverException | InterruptedException pE) {
+                throw new RuntimeException(pE);
+              }
+            });
+
+      assert task1.get() == null;
+  }
+}
