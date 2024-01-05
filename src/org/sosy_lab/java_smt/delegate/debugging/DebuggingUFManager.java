1ee178afd8c3db97a91935a4e99741e447a0b1a9
Daniel Raffler <daniel-raffler@gmx.de>
2024-01-05 09:57:41 +0100

    Added UFManager

---
+++ src/org/sosy_lab/java_smt/delegate/debugging/DebuggingUFManager.java
@@ -0,0 +1,79 @@
+// This file is part of JavaSMT,
+// an API wrapper for a collection of SMT solvers:
+// https://github.com/sosy-lab/java-smt
+//
+// SPDX-FileCopyrightText: 2023 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.sosy_lab.java_smt.delegate.debugging;
+
+import java.util.List;
+import java.util.Set;
+import org.sosy_lab.java_smt.api.Formula;
+import org.sosy_lab.java_smt.api.FormulaType;
+import org.sosy_lab.java_smt.api.FunctionDeclaration;
+import org.sosy_lab.java_smt.api.UFManager;
+
+public class DebuggingUFManager extends FormulaChecks implements UFManager {
+  private final UFManager delegate;
+
+  public DebuggingUFManager(UFManager pDelegate, Set<Formula> pLocalFormulas) {
+    super(pLocalFormulas);
+    delegate = pDelegate;
+  }
+
+  @Override
+  public <T extends Formula> FunctionDeclaration<T> declareUF(
+      String name, FormulaType<T> returnType, List<FormulaType<?>> args) {
+    // TODO: FunctionDeclarations and FormulaTypes should probably be tracked too?
+    assertThreadLocal();
+    return delegate.declareUF(name, returnType, args);
+  }
+
+  @Override
+  public <T extends Formula> FunctionDeclaration<T> declareUF(
+      String name, FormulaType<T> returnType, FormulaType<?>... args) {
+    assertThreadLocal();
+    return delegate.declareUF(name, returnType, args);
+  }
+
+  @Override
+  public <T extends Formula> T callUF(
+      FunctionDeclaration<T> funcType, List<? extends Formula> args) {
+    assertThreadLocal();
+    for (Formula t : args) {
+      assertFormulaInContext(t);
+    }
+    return delegate.callUF(funcType, args);
+  }
+
+  @Override
+  public <T extends Formula> T callUF(FunctionDeclaration<T> funcType, Formula... args) {
+    assertThreadLocal();
+    for (Formula t : args) {
+      assertFormulaInContext(t);
+    }
+    return delegate.callUF(funcType, args);
+  }
+
+  @Override
+  public <T extends Formula> T declareAndCallUF(
+      String name, FormulaType<T> pReturnType, List<Formula> pArgs) {
+    assertThreadLocal();
+    for (Formula t : pArgs) {
+      assertFormulaInContext(t);
+    }
+    return delegate.declareAndCallUF(name, pReturnType, pArgs);
+  }
+
+  @Override
+  public <T extends Formula> T declareAndCallUF(
+      String name, FormulaType<T> pReturnType, Formula... pArgs) {
+    assertThreadLocal();
+    for (Formula t : pArgs) {
+      assertFormulaInContext(t);
+    }
+    return delegate.declareAndCallUF(name, pReturnType, pArgs);
+  }
+}
