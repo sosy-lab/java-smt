e35c103f45267d49d99c97760771525fc4a64993
Daniel Raffler <daniel-raffler@gmx.de>
2024-01-06 20:10:38 +0100

    Removed a TODO about checking that getModel only is called immediately after isUnsat. This may be better implemented in the solver backends.

--- src/org/sosy_lab/java_smt/delegate/debugging/DebuggingBasicProverEnvironment.java
+++ src/org/sosy_lab/java_smt/delegate/debugging/DebuggingBasicProverEnvironment.java
@@ -73,7 +73,6 @@ class DebuggingBasicProverEnvironment<T> extends FormulaChecks
   @SuppressWarnings("resource")
   @Override
   public Model getModel() throws SolverException {
-    // TODO: Check that isUnsat was called and returned false, and that the stack has not changed
     assertThreadLocal();
     return new DebuggingModel(delegate.getModel(), nodeManager);
   }
