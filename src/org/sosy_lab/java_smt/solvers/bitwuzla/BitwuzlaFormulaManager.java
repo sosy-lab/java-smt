b4a347e8507fcdbd4530a0ac054f15adf5cfa48b
Daniel Raffler <daniel-raffler@gmx.de>
2024-03-14 16:13:53 +0100

    Bitwuzla: Updated BitwuzlaFormulaManager.parse to use the new method from the Bitwuzla API. We're now able to keep our variable cache synchronized with the variable context of the Bitwuzla solver. This allows us to mix API calls with parser calls that all use the same variables.

--- src/org/sosy_lab/java_smt/solvers/bitwuzla/BitwuzlaFormulaManager.java
+++ src/org/sosy_lab/java_smt/solvers/bitwuzla/BitwuzlaFormulaManager.java
@@ -9,21 +9,26 @@
 package org.sosy_lab.java_smt.solvers.bitwuzla;
 
 import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
+import com.google.common.collect.Table;
+import com.google.common.collect.Table.Cell;
 import java.io.IOException;
+import java.util.List;
 import org.sosy_lab.common.Appender;
 import org.sosy_lab.common.Appenders;
 import org.sosy_lab.java_smt.api.BooleanFormula;
 import org.sosy_lab.java_smt.api.Formula;
 import org.sosy_lab.java_smt.basicimpl.AbstractFormulaManager;
 import org.sosy_lab.java_smt.solvers.bitwuzla.api.Bitwuzla;
+import org.sosy_lab.java_smt.solvers.bitwuzla.api.Map_TermTerm;
 import org.sosy_lab.java_smt.solvers.bitwuzla.api.Options;
 import org.sosy_lab.java_smt.solvers.bitwuzla.api.Parser;
 import org.sosy_lab.java_smt.solvers.bitwuzla.api.Sort;
 import org.sosy_lab.java_smt.solvers.bitwuzla.api.Term;
 import org.sosy_lab.java_smt.solvers.bitwuzla.api.Vector_Term;
 
-final class BitwuzlaFormulaManager
+public final class BitwuzlaFormulaManager
     extends AbstractFormulaManager<Term, Sort, Void, BitwuzlaDeclaration> {
   private final BitwuzlaFormulaCreator creator;
   private final Options bitwuzlaOption;
@@ -54,6 +59,50 @@ final class BitwuzlaFormulaManager
     bitwuzlaOption = pBitwuzlaOptions;
   }
 
+  // Split up a sequence of lisp expressions
+  // f.ex "(define-const a Int)(assert (= a 0)" becomes ["(define-const a Int)", "(assert (= a 0))"]
+  public static List<String> tokenize(String input) {
+    ImmutableList.Builder<String> builder = ImmutableList.builder();
+    int level = 0;
+    StringBuilder read = new StringBuilder();
+    for (char c : input.toCharArray()) {
+      if (level == 0) {
+        if (!Character.isWhitespace(c)) {
+          if (c == '(') {
+            read.append("(");
+            level++;
+          } else {
+            // All top-level expressions should have parentheses around them
+            throw new IllegalArgumentException();
+          }
+        }
+      } else {
+        read.append(c);
+        if (c == '(') {
+          level++;
+        }
+        if (c == ')') {
+          if (level == 1) {
+            builder.add(read.toString());
+            read = new StringBuilder();
+          }
+          level--;
+        }
+      }
+    }
+    if (level != 0) {
+      // Missing closing parenthesis
+      throw new IllegalArgumentException();
+    }
+    return builder.build();
+  }
+
+  // Check if the token is a function or variable declaration
+  public static boolean isDecl(String token) {
+    // TODO: How to handle function *definitions*? And are they supported by Bitwuzla?
+    return token.matches("\\(\\s*(declare-const|declare-fun).*");
+  }
+
   @Override
   public BooleanFormula parse(String formulaStr) throws IllegalArgumentException {
     // Strip the input string and remove everything but declarations and assertions
@@ -69,14 +118,90 @@ final class BitwuzlaFormulaManager
       s = s.replace("(exit)", "");
     }
 
-    Parser parser = new Parser(creator.getTermManager(), bitwuzlaOption);
-    parser.parse(s, true, false);
+    // Split the input string into a list of SMT-LIB2 commands
+    List<String> tokens = tokenize(s);
 
+    Table<String, Sort, Term> cache = creator.getCache();
+
+    // Process the declarations
+    ImmutableList.Builder<String> processed = ImmutableList.builder();
+    for (String token : tokens) {
+      if (isDecl(token)) {
+        Parser declParser = new Parser(creator.getTermManager(), bitwuzlaOption);
+        declParser.parse(token, true, false);
+        Term parsed = declParser.get_declared_funs().get(0);
+
+        // Check if the symbol is already defined in the variable cache
+        if (cache.containsRow(parsed.symbol())) {
+          if (!cache.contains(parsed.symbol(), parsed.sort())) {
+            // Sort of the definition that we parsed does not match the sort from the variable
+            // cache.
+            throw new IllegalArgumentException();
+          }
+          // Skip if it's just a redefinition
+          continue;
+        }
+      }
+      // Otherwise, keep the command
+      processed.add(token);
+    }
+
+    // Build SMT-LIB2 declarations for all variables in the cache
+    ImmutableList.Builder<String> builder = ImmutableList.builder();
+    for (Cell<String, Sort, Term> c : cache.cellSet()) {
+      String symbol = c.getRowKey();
+      List<Sort> args = ImmutableList.of();
+      Sort sort = c.getColumnKey();
+      if (sort.is_fun()) {
+        args = sort.fun_domain();
+        sort = sort.fun_codomain();
+      }
+      StringBuilder decl = new StringBuilder();
+      decl.append("(declare-fun").append(" ");
+      decl.append(symbol).append(" ");
+      decl.append("(");
+      for (Sort p : args) {
+        decl.append(p).append(" ");
+      }
+      decl.append(")").append(" ");
+      decl.append(sort);
+      decl.append(")");
+
+      builder.add(decl.toString());
+    }
+    String decls = String.join("\n", builder.build());
+    String input = String.join("\n", processed.build());
+
+    // Add the declarations to the input and parse everything
+    Parser parser = new Parser(creator.getTermManager(), bitwuzlaOption);
+    parser.parse(decls + input, true, false);
+
+    // After the run, get the final assertion from the parser
     Vector_Term assertions = parser.bitwuzla().get_assertions();
     Preconditions.checkArgument(
         !assertions.isEmpty(), "No assertion found in input string \"%s\"", formulaStr);
+    Term result = Iterables.getLast(assertions);
 
-    return creator.encapsulateBoolean(Iterables.getLast(assertions));
+    // Now get all symbols that were declared in the input
+    Vector_Term declared = parser.get_declared_funs();
+
+    // Process the symbols from the parser
+    Map_TermTerm subst = new Map_TermTerm();
+    for (Term term : declared) {
+      if (cache.containsRow(term.symbol())) {
+        // Symbol is from the context: add the original term to the substitution map
+        subst.put(term, cache.get(term.symbol(), term.sort()));
+      } else {
+        // Symbol is new, add it to the context
+        cache.put(term.symbol(), term.sort(), term);
+      }
+    }
+
+    // Substitude all symbols from the context with their original terms
+    result = creator.getTermManager().substitute_term(result, subst);
+
+    // Return the updated term
+    return creator.encapsulateBoolean(result);
   }
 
   @Override
