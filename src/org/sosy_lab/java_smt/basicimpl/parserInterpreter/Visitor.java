1cb60fd2ddc031894e6223388752a3f4b8a80650
Daniel Raffler <daniel-raffler@gmx.de>
2024-04-24 00:15:48 +0200

    Removed the line from the last commit. Checksyle and format-source can't agree on its proper indentation.

--- src/org/sosy_lab/java_smt/basicimpl/parserInterpreter/Visitor.java
+++ src/org/sosy_lab/java_smt/basicimpl/parserInterpreter/Visitor.java
@@ -361,7 +361,6 @@ public class Visitor extends smtlibv2BaseVisitor<Object> {
     List<Formula> operands = new ArrayList<>();
     getOperands(ctx, operands);
     switch (operator) {
-      // boolean operators
       case "and":
         try {
           List<BooleanFormula> booleanOperands =
