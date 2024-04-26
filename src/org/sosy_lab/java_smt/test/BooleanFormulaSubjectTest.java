9baa30931d0aa6e312efe632d2ad37e33d24b1f7
Daniel Raffler <daniel-raffler@gmx.de>
2024-04-26 12:46:02 +0200

    DReal: Use requireUnsatCore in a tests that used to check manually.

--- src/org/sosy_lab/java_smt/test/BooleanFormulaSubjectTest.java
+++ src/org/sosy_lab/java_smt/test/BooleanFormulaSubjectTest.java
@@ -9,7 +9,6 @@
 package org.sosy_lab.java_smt.test;
 
 import static com.google.common.truth.ExpectFailure.assertThat;
-import static com.google.common.truth.TruthJUnit.assume;
 import static org.sosy_lab.java_smt.test.BooleanFormulaSubject.booleanFormulasOf;
 
 import com.google.common.base.Throwables;
@@ -18,7 +17,6 @@ import com.google.common.truth.ExpectFailure.SimpleSubjectBuilderCallback;
 import com.google.common.truth.SimpleSubjectBuilder;
 import org.junit.Before;
 import org.junit.Test;
-import org.sosy_lab.java_smt.SolverContextFactory.Solvers;
 import org.sosy_lab.java_smt.api.BooleanFormula;
 import org.sosy_lab.java_smt.api.SolverException;
 
@@ -66,12 +64,7 @@ public class BooleanFormulaSubjectTest extends SolverBasedTest0.ParameterizedSol
 
   @Test
   public void testIsSatisfiableNo() {
-    // INFO: OpenSMT does not support unsat core
-    assume()
-        .withMessage("Solver does not support unsat core generation in a usable way")
-        .that(solverToUse())
-        .isNoneOf(Solvers.BOOLECTOR, Solvers.OPENSMT);
-
+    requireUnsatCore();
     AssertionError failure =
         expectFailure(whenTesting -> whenTesting.that(contradiction).isSatisfiable());
     assertThat(failure).factValue("which has unsat core").isNotEmpty();
