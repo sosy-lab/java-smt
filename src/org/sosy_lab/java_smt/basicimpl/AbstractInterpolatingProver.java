4b962a2b669e90746bec7c240bbd61e926c99886
Stefanie Sinner <stefanie_sinner@gmx.de>
2024-08-09 16:07:08 +0200

    Change AbstractInterpolatingProver class

--- src/org/sosy_lab/java_smt/basicimpl/AbstractInterpolatingProver.java
+++ src/org/sosy_lab/java_smt/basicimpl/AbstractInterpolatingProver.java
@@ -10,36 +10,6 @@
 
 package org.sosy_lab.java_smt.basicimpl;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
-import org.sosy_lab.common.ShutdownNotifier;
-import org.sosy_lab.java_smt.api.BooleanFormula;
-import org.sosy_lab.java_smt.api.BooleanFormulaManager;
-import org.sosy_lab.java_smt.api.InterpolatingProverEnvironment;
-import org.sosy_lab.java_smt.api.SolverContext.ProverOptions;
-import org.sosy_lab.java_smt.api.SolverException;
+public abstract class AbstractInterpolatingProver<F> {
 
-public abstract class AbstractInterpolatingProver<F> extends AbstractProverWithAllSat<F>
-    implements InterpolatingProverEnvironment<F> {
-
-  protected AbstractInterpolatingProver(
-      Set<ProverOptions> pOptions,
-      BooleanFormulaManager pBmgr,
-      ShutdownNotifier pShutdownNotifier) {
-    super(pOptions, pBmgr, pShutdownNotifier);
-  }
-
-  @Override
-  public BooleanFormula getInterpolant(Collection<F> formulasOfA)
-      throws SolverException, InterruptedException {
-    return null;
-  }
-
-  @Override
-  public List<BooleanFormula> getTreeInterpolants(
-      List<? extends Collection<F>> partitionedFormulas,
-      int[] startOfSubTree) throws SolverException, InterruptedException {
-    return List.of();
-  }
 }
