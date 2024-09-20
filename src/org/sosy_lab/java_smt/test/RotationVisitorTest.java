d04d25ca2649f59a14153ba1b0a313e462946301
Karlheinz Friedberger <kfriedberger@gmail.com>
2024-09-21 00:36:21 +0200

    #397: extend tests for bitvector rotation.

--- src/org/sosy_lab/java_smt/test/RotationVisitorTest.java
+++ src/org/sosy_lab/java_smt/test/RotationVisitorTest.java
@@ -12,37 +12,210 @@ package org.sosy_lab.java_smt.test;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.sosy_lab.java_smt.api.BitvectorFormula;
+import org.sosy_lab.java_smt.api.FunctionDeclarationKind;
+import org.sosy_lab.java_smt.test.SolverVisitorTest.FunctionDeclarationVisitorNoOther;
 
 public class RotationVisitorTest extends SolverBasedTest0.ParameterizedSolverBasedTest0 {
+
+  private BitvectorFormula a;
+  private BitvectorFormula y;
+
+  @Before
+  public void init() {
+    requireBitvectors();
+    requireVisitor();
+
+    a = bvmgr.makeVariable(8, "a");
+    y = bvmgr.makeBitvector(8, 1);
+  }
+
   @Test
   public void rotateLeftTest() {
-    requireBitvectors();
-    BitvectorFormula x = bvmgr.makeBitvector(8, 7);
-    assertThat(mgr.extractVariables(bvmgr.rotateLeft(x, 1))).isEmpty();
+    BitvectorFormula f = bvmgr.rotateLeft(a, y);
+
+    var variables = mgr.extractVariablesAndUFs(f);
+    assertThat(variables).hasSize(1);
+    assertThat(variables).containsKey("a");
+
+    var functions = mgr.visit(f, new FunctionDeclarationVisitorNoOther(mgr));
+    switch (solverToUse()) {
+      case CVC4:
+        assertThat(functions)
+            .containsExactly(
+                FunctionDeclarationKind.BV_OR,
+                FunctionDeclarationKind.BV_SHL,
+                FunctionDeclarationKind.ITE,
+                FunctionDeclarationKind.ITE,
+                FunctionDeclarationKind.EQ,
+                FunctionDeclarationKind.EQ,
+                FunctionDeclarationKind.BV_SMOD,
+                FunctionDeclarationKind.BV_SMOD,
+                FunctionDeclarationKind.BV_LSHR,
+                FunctionDeclarationKind.BV_SUB);
+        break;
+      case CVC5:
+      case PRINCESS:
+        assertThat(functions)
+            .containsExactly(
+                FunctionDeclarationKind.BV_OR,
+                FunctionDeclarationKind.BV_SHL,
+                FunctionDeclarationKind.BV_SMOD,
+                FunctionDeclarationKind.BV_SMOD,
+                FunctionDeclarationKind.BV_LSHR,
+                FunctionDeclarationKind.BV_SUB);
+        break;
+      case MATHSAT5:
+        assertThat(functions)
+            .containsExactly(
+                FunctionDeclarationKind.BV_OR,
+                FunctionDeclarationKind.BV_LSHR,
+                FunctionDeclarationKind.BV_SHL);
+        break;
+      case YICES2:
+        assertThat(functions)
+            .containsExactly(
+                FunctionDeclarationKind.BV_CONCAT,
+                FunctionDeclarationKind.BV_EXTRACT,
+                FunctionDeclarationKind.BV_EXTRACT,
+                FunctionDeclarationKind.BV_EXTRACT,
+                FunctionDeclarationKind.BV_EXTRACT,
+                FunctionDeclarationKind.BV_EXTRACT,
+                FunctionDeclarationKind.BV_EXTRACT,
+                FunctionDeclarationKind.BV_EXTRACT,
+                FunctionDeclarationKind.BV_EXTRACT);
+        break;
+      default:
+        assertThat(functions).containsExactly(FunctionDeclarationKind.BV_ROTATE_LEFT);
+    }
+
+    // TODO dumpFormula crashes with Princess
+    // System.out.println(mgr.dumpFormula(bvmgr.equal(a, f)));
   }
 
   @Test
   public void rotateRightTest() {
-    requireBitvectors();
-    BitvectorFormula x = bvmgr.makeBitvector(8, 7);
-    assertThat(mgr.extractVariables(bvmgr.rotateRight(x, 1))).isEmpty();
+    BitvectorFormula f = bvmgr.rotateRight(a, y);
+
+    var variables = mgr.extractVariablesAndUFs(f);
+    assertThat(variables).hasSize(1);
+    assertThat(variables).containsKey("a");
+
+    var functions = mgr.visit(f, new FunctionDeclarationVisitorNoOther(mgr));
+    switch (solverToUse()) {
+      case CVC4:
+        assertThat(functions)
+            .containsExactly(
+                FunctionDeclarationKind.BV_OR,
+                FunctionDeclarationKind.BV_LSHR,
+                FunctionDeclarationKind.ITE,
+                FunctionDeclarationKind.ITE,
+                FunctionDeclarationKind.EQ,
+                FunctionDeclarationKind.EQ,
+                FunctionDeclarationKind.BV_SMOD,
+                FunctionDeclarationKind.BV_SMOD,
+                FunctionDeclarationKind.BV_SHL,
+                FunctionDeclarationKind.BV_SUB);
+        break;
+      case CVC5:
+      case PRINCESS:
+        assertThat(functions)
+            .containsExactly(
+                FunctionDeclarationKind.BV_OR,
+                FunctionDeclarationKind.BV_SHL,
+                FunctionDeclarationKind.BV_SMOD,
+                FunctionDeclarationKind.BV_SMOD,
+                FunctionDeclarationKind.BV_LSHR,
+                FunctionDeclarationKind.BV_SUB);
+        break;
+      case MATHSAT5:
+        assertThat(functions)
+            .containsExactly(
+                FunctionDeclarationKind.BV_OR,
+                FunctionDeclarationKind.BV_LSHR,
+                FunctionDeclarationKind.BV_SHL);
+        break;
+      case YICES2:
+        assertThat(functions)
+            .containsExactly(
+                FunctionDeclarationKind.BV_CONCAT,
+                FunctionDeclarationKind.BV_EXTRACT,
+                FunctionDeclarationKind.BV_EXTRACT,
+                FunctionDeclarationKind.BV_EXTRACT,
+                FunctionDeclarationKind.BV_EXTRACT,
+                FunctionDeclarationKind.BV_EXTRACT,
+                FunctionDeclarationKind.BV_EXTRACT,
+                FunctionDeclarationKind.BV_EXTRACT,
+                FunctionDeclarationKind.BV_EXTRACT);
+        break;
+      default:
+        assertThat(functions).containsExactly(FunctionDeclarationKind.BV_ROTATE_RIGHT);
+    }
   }
 
   @Test
   public void rotateLeftIntegerTest() {
-    requireBitvectors();
-    BitvectorFormula x = bvmgr.makeBitvector(8, 7);
-    BitvectorFormula y = bvmgr.makeBitvector(8, 1);
-    assertThat(mgr.extractVariables(bvmgr.rotateLeft(x, y))).isEmpty();
+    BitvectorFormula f = bvmgr.rotateLeft(a, 1);
+
+    var variables = mgr.extractVariablesAndUFs(f);
+    assertThat(variables).hasSize(1);
+    assertThat(variables).containsKey("a");
+
+    var functions = mgr.visit(f, new FunctionDeclarationVisitorNoOther(mgr));
+    switch (solverToUse()) {
+      case PRINCESS:
+        assertThat(functions)
+            .containsExactly(FunctionDeclarationKind.BV_EXTRACT, FunctionDeclarationKind.BV_CONCAT);
+        break;
+      case YICES2:
+        assertThat(functions)
+            .containsExactly(
+                FunctionDeclarationKind.BV_CONCAT,
+                FunctionDeclarationKind.BV_EXTRACT,
+                FunctionDeclarationKind.BV_EXTRACT,
+                FunctionDeclarationKind.BV_EXTRACT,
+                FunctionDeclarationKind.BV_EXTRACT,
+                FunctionDeclarationKind.BV_EXTRACT,
+                FunctionDeclarationKind.BV_EXTRACT,
+                FunctionDeclarationKind.BV_EXTRACT,
+                FunctionDeclarationKind.BV_EXTRACT);
+        break;
+      default:
+        assertThat(functions).containsExactly(FunctionDeclarationKind.BV_ROTATE_LEFT_BY_INT);
+    }
   }
 
   @Test
   public void rotateRightIntegerTest() {
-    requireBitvectors();
-    BitvectorFormula x = bvmgr.makeBitvector(8, 7);
-    BitvectorFormula y = bvmgr.makeBitvector(8, 1);
-    assertThat(mgr.extractVariables(bvmgr.rotateRight(x, y))).isEmpty();
+    BitvectorFormula f = bvmgr.rotateRight(a, 1);
+
+    var variables = mgr.extractVariablesAndUFs(f);
+    assertThat(variables).hasSize(1);
+    assertThat(variables).containsKey("a");
+
+    var functions = mgr.visit(f, new FunctionDeclarationVisitorNoOther(mgr));
+    switch (solverToUse()) {
+      case PRINCESS:
+        assertThat(functions)
+            .containsExactly(FunctionDeclarationKind.BV_EXTRACT, FunctionDeclarationKind.BV_CONCAT);
+        break;
+      case YICES2:
+        assertThat(functions)
+            .containsExactly(
+                FunctionDeclarationKind.BV_CONCAT,
+                FunctionDeclarationKind.BV_EXTRACT,
+                FunctionDeclarationKind.BV_EXTRACT,
+                FunctionDeclarationKind.BV_EXTRACT,
+                FunctionDeclarationKind.BV_EXTRACT,
+                FunctionDeclarationKind.BV_EXTRACT,
+                FunctionDeclarationKind.BV_EXTRACT,
+                FunctionDeclarationKind.BV_EXTRACT,
+                FunctionDeclarationKind.BV_EXTRACT);
+        break;
+      default:
+        assertThat(functions).containsExactly(FunctionDeclarationKind.BV_ROTATE_RIGHT_BY_INT);
+    }
   }
 }
