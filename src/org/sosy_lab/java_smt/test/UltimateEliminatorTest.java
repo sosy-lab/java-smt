349ac269ee77b8af68031de775a8ed5b96d16d4f
Anastasia <a.gurevich@campus.lmu.de>
2024-11-27 18:34:18 +0100

    add dependencies for Ultimate Eliminator

--- src/org/sosy_lab/java_smt/test/UltimateEliminatorTest.java
+++ src/org/sosy_lab/java_smt/test/UltimateEliminatorTest.java
@@ -11,8 +11,8 @@
 package org.sosy_lab.java_smt.test;
 import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.smt.UltimateEliminator;
 import org.junit.Test;
-import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger;
-import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;
+//import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger;
+//import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;
 import de.uni_freiburg.informatik.ultimate.logic.Logics;
 import de.uni_freiburg.informatik.ultimate.logic.Script;
 import de.uni_freiburg.informatik.ultimate.logic.Term;
@@ -22,6 +22,6 @@ import de.uni_freiburg.informatik.ultimate.logic.Sort;
 
 public class UltimateEliminatorTest {
 
-  private IUltimateServiceProvider provider;
+  //private IUltimateServiceProvider provider;
 
 }
