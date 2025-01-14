53b303db83d54c00b6f8fb7fc6d11b8aeb6a9c53
Stefanie Sinner <stefanie_sinner@gmx.de>
2025-01-14 10:55:01 +0100

    Make sure that the sets of formulas for A and B cannot be null with Preconditions. Also make sure that interpolationStrategy is not null to remove the warning from the if-clause below. This may not be necessary, because it is already ensured that interpolationStrategy cannot be null at that time.

--- src/org/sosy_lab/java_smt/basicimpl/IndependentInterpolatingProverEnvironment.java
+++ src/org/sosy_lab/java_smt/basicimpl/IndependentInterpolatingProverEnvironment.java
@@ -131,6 +131,10 @@ public class IndependentInterpolatingProverEnvironment<TFormulaInfo, TType>
     Collection<BooleanFormula> formulasOfA = formulasAAndB.gotFormulasForA();
     Collection<BooleanFormula> formulasOfB = formulasAAndB.gotFormulasForB();
 
+    Preconditions.checkNotNull(formulasOfA);
+    Preconditions.checkNotNull(formulasOfB);
+    Preconditions.checkNotNull(interpolationStrategy);
+
     if (interpolationStrategy.equals(ProverOptions.GENERATE_MODEL_BASED_INTERPOLANTS)) {
       return computeModelBasedInterpolant(formulasOfA, formulasOfB);
     } else {
