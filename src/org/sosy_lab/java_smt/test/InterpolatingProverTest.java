b5de85e62351853cac72dbb14c787915b10457b2
Stefanie Sinner <stefanie_sinner@gmx.de>
2025-01-26 05:29:13 +0100

    Revert "ITP Tests: Assume Z3 run into timeout on the test binaryBVInterpolation1 using Model-Based Interpolation, since there are some time issues"
    This reverts commit f23b2c6d096fab106969ba1699e9ef05be111fa7.

--- src/org/sosy_lab/java_smt/test/InterpolatingProverTest.java
+++ src/org/sosy_lab/java_smt/test/InterpolatingProverTest.java
@@ -229,13 +229,6 @@ public class InterpolatingProverTest
 
     InterpolatingProverEnvironment<T> stack = newEnvironmentForTest();
 
-    if (interpolationStrategy == ProverOptions.GENERATE_MODEL_BASED_INTERPOLANTS) {
-      assume()
-          .withMessage("Solver %s run into timeout on this test", solverToUse())
-          .that(solverToUse())
-          .isNotEqualTo(Solvers.Z3);
-    }
-
     int i = index.getFreshId();
     int width = 8;
 
