3b2732977e33c6b41a532e5b5f85ee1c9e8e8de9
Stefanie Sinner <stefanie_sinner@gmx.de>
2025-01-26 05:28:56 +0100

    Revert "ITP Test: Skip test binaryInterpolation for BOOLECTOR, since the solver does not support integers, by adding requireIntegers() to the test method"
    This reverts commit 5d05d5a11c59a7f0a4e1d043f4b7a7a95c21e797.

--- src/org/sosy_lab/java_smt/test/InterpolatingProverTest.java
+++ src/org/sosy_lab/java_smt/test/InterpolatingProverTest.java
@@ -131,8 +131,6 @@ public class InterpolatingProverTest
 
   @Test
   public <T> void binaryInterpolation() throws SolverException, InterruptedException {
-    requireIntegers();
-
     InterpolatingProverEnvironment<T> stack = newEnvironmentForTest();
 
     int i = index.getFreshId();
