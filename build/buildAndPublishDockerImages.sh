49169b2a3d6b2320109204ba8e8bb99ee0bf6f57
Karlheinz Friedberger <kfriedberger@gmail.com>
2025-03-05 21:41:18 +0100

    CI and Docker: add script to manually build and push images for multiple platforms.

---
+++ build/buildAndPublishDockerImages.sh
@@ -0,0 +1,23 @@
+#
+# This file is part of JavaSMT,
+# an API wrapper for a collection of SMT solvers:
+# https://github.com/sosy-lab/java-smt
+#
+# SPDX-FileCopyrightText: 2025 Dirk Beyer <https://www.sosy-lab.org>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+JDK_VERSIONS="jdk-11 jdk-17 jdk-21 jdk-23"
+DOCKER_PLATFORMS="linux/amd64,linux/arm64"
+REGISTRY="registry.gitlab.com/sosy-lab/software/java-smt"
+
+# podman login registry.gitlab.com
+
+for JDK_VERSION in $JDK_VERSIONS; do
+  IMAGE=test:JDK_VERSION
+  DOCKER_FILE=gitlab-ci.Dockerfile.$JDK_VERSION
+  podman build --platform $DOCKER_PLATFORMS --manifest $IMAGE -f $DOCKER_FILE
+  podman tag test:$JDK $REGISTRY/$IMAGE
+  podman manifest push --all $REGISTRY/$IMAGE
+done
