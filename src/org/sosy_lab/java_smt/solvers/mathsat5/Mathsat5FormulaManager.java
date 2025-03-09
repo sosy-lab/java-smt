75bdfc0d30bae7815fc167b6b5d9912d8afc8ead
Anastasia-Gu <a.gurevich@campus.lmu.de>
2025-03-10 00:47:20 +0100

    revert restriction for dumpFormulaImpl in Mathsat5FormulaManager

--- src/org/sosy_lab/java_smt/solvers/mathsat5/Mathsat5FormulaManager.java
+++ src/org/sosy_lab/java_smt/solvers/mathsat5/Mathsat5FormulaManager.java
@@ -69,7 +69,8 @@ final class Mathsat5FormulaManager extends AbstractFormulaManager<Long, Long, Lo
 
   @Override
   public String dumpFormulaImpl(final Long f) {
-    assert getFormulaCreator().getFormulaType(f) != null : "Only Formulas may be dumped";
+    assert getFormulaCreator().getFormulaType(f) == FormulaType.BooleanType
+        : "Only BooleanFormulas may be dumped";
     return msat_to_smtlib2(getEnvironment(), f);
   }
 
