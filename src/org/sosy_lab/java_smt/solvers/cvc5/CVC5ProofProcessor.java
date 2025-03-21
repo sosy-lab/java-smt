87159e8b9e74e0625a1569131ac261910258579d
Gabriel Carpio <gabriel.carpio.99@gmail.com>
2025-03-21 19:46:49 +0100

    CVC5: Proofs are now processed and stored as `CVC5ProofNode`

--- src/org/sosy_lab/java_smt/solvers/cvc5/CVC5ProofProcessor.java
+++ src/org/sosy_lab/java_smt/solvers/cvc5/CVC5ProofProcessor.java
@@ -10,5 +10,91 @@
 
 package org.sosy_lab.java_smt.solvers.cvc5;
 
+import io.github.cvc5.CVC5ApiException;
+import io.github.cvc5.Proof;
+import io.github.cvc5.Term;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
+import org.sosy_lab.java_smt.api.Formula;
+import org.sosy_lab.java_smt.api.proofs.ProofRule;
+
 public class CVC5ProofProcessor {
+  private final CVC5SolverContext context;
+  private final CVC5FormulaCreator formulaCreator;
+  private final CVC5AbstractProver prover;
+
+  CVC5ProofProcessor(
+      CVC5SolverContext ctx, CVC5FormulaCreator creator, CVC5AbstractProver pProver) {
+    context = ctx;
+    formulaCreator = creator;
+    prover = pProver;
+  }
+
+  CVC5ProofNode fromCVC5Proof(Proof rootProof) throws CVC5ApiException {
+
+    Deque<Frame> stack = new ArrayDeque<>();
+
+    Map<Proof, CVC5ProofNode> computed = new HashMap<>();
+
+    stack.push(new Frame(rootProof));
+
+    while (!stack.isEmpty()) {
+      Frame frame = stack.peek();
+
+      // Skip processing the frame if its rule is "SCOPE"
+      // This rule seems to just help the processing by CVC5
+      if (!frame.visited && frame.proof.getRule().getValue() == 1) {
+        // Pop the SCOPE frame and push its children onto the stack
+        stack.pop();
+        frame.numChildren = rootProof.getChildren().length;
+        frame.visited = true;
+
+        for (int i = frame.numChildren; i >= 0; i--) {
+          Proof child = rootProof.getChildren()[i];
+          if (!computed.containsKey(child)) {
+            stack.push(new Frame(child));
+          }
+        }
+      } else {
+
+        stack.pop();
+        int numChildren = frame.numChildren;
+
+        CVC5ProofRule proofRule =
+            ProofRule.fromName(CVC5ProofRule.class, frame.proof.getRule().toString());
+        CVC5ProofNode pn = new CVC5ProofNode(proofRule, generateFormula(frame.proof));
+        for (int i = 0; i < numChildren - 1; i++) {
+          Proof child = frame.proof.getChildren()[i];
+
+          if (computed.containsKey(child)) {
+            pn.addChild(computed.get(child));
+          }
+        }
+        computed.put(frame.proof, pn);
+      }
+    }
+    return computed.get(rootProof);
+  }
+
+  private Formula generateFormula(Proof proof) {
+    Formula formula = null;
+    if (formula == null) {
+      Term term = proof.getResult();
+      formula = formulaCreator.encapsulate(formulaCreator.getFormulaType(term), term);
+    }
+    return formula;
+  }
+
+  private static class Frame {
+    final Proof proof;
+    int numChildren;
+    boolean visited;
+
+    Frame(Proof pProof) {
+      proof = pProof;
+      visited = false;
+    }
+  }
 }
--- src/org/sosy_lab/java_smt/solvers/cvc5/CVC5ProofsTest.java
+++ src/org/sosy_lab/java_smt/solvers/cvc5/CVC5ProofsTest.java
@@ -10,17 +10,24 @@
 
 package org.sosy_lab.java_smt.solvers.cvc5;
 
+import static com.google.common.truth.Truth.assertThat;
+
 import io.github.cvc5.CVC5ApiException;
+import io.github.cvc5.Kind;
+import io.github.cvc5.Proof;
 import io.github.cvc5.Solver;
+import io.github.cvc5.Sort;
+import io.github.cvc5.Term;
+import io.github.cvc5.TermManager;
 import org.junit.AssumptionViolatedException;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
+import org.junit.Test;
 import org.sosy_lab.common.NativeLibraries;
 
 @SuppressWarnings({"unchecked", "rawtypes", "unused", "static-access"})
-@Ignore
 public class CVC5ProofsTest {
+
   @BeforeClass
   public static void loadCVC5() {
     try {
@@ -30,22 +37,27 @@ public class CVC5ProofsTest {
     }
   }
 
+  private static TermManager tm;
+  private static Sort booleanSort;
   private Solver solver;
 
   @Before
   public void init() throws CVC5ApiException {
+    tm = new TermManager();
+    booleanSort = tm.getBooleanSort();
     solver = createEnvironment();
   }
 
   private static Solver createEnvironment() throws CVC5ApiException {
-    Solver newSolver = new Solver();
+
+    Solver newSolver = new Solver(tm);
     newSolver.setLogic("ALL");
 
     // options
     newSolver.setOption("incremental", "true");
     newSolver.setOption("produce-models", "true");
     newSolver.setOption("finite-model-find", "true");
-    newSolver.setOption("sets-ext", "true");
+    // newSolver.setOption("sets-ext", "true");
     newSolver.setOption("output-language", "smtlib2");
     newSolver.setOption("strings-exp", "true");
     newSolver.setOption("produce-proofs", "true");
@@ -53,4 +65,78 @@ public class CVC5ProofsTest {
     return newSolver;
   }
 
+  private void processProof(Proof proof, int depth, int childNumber) throws CVC5ApiException {
+    String indent = "  ".repeat(depth); // Indentation for readability
+
+    System.out.println(indent + "Proof " + childNumber + ":");
+    System.out.println(indent + "  Result: " + proof.getResult());
+    System.out.println(indent + "  Rule: " + proof.getRule());
+    System.out.println(indent + "  Num args: " + proof.getArguments().length);
+
+    // Display arguments
+    for (int j = 0; j < proof.getArguments().length; j++) {
+      System.out.println(indent + "    Arg " + j + ": " + proof.getArguments()[j]);
+    }
+
+    Proof[] proofChildren = proof.getChildren();
+    System.out.println(indent + "  Proof children length: " + proofChildren.length);
+
+    for (int i = 0; i < proofChildren.length; i++) {
+      System.out.println(indent + "  Child " + (i + 1) + " of Proof " + childNumber + ":");
+      processProof(proofChildren[i], depth + 1, i + 1);
+    }
+  }
+
+  @Test
+  public void proofTest() throws CVC5ApiException {
+    // (declare-fun q1 () Bool)
+    // (declare-fun q2 () Bool)
+    // (assert (or (not q1) q2))
+    // (assert q1)
+    // (assert (not q2))
+    Sort booleanSort = tm.getBooleanSort();
+    Term q1 = solver.declareFun("q1", new Sort[] {}, booleanSort);
+    Term q2 = solver.declareFun("q2", new Sort[] {}, booleanSort);
+
+    solver.assertFormula(tm.mkTerm(Kind.OR, (tm.mkTerm(Kind.NOT, q1)), q2));
+    solver.assertFormula(q1);
+    solver.assertFormula(tm.mkTerm(Kind.NOT, q2));
+
+    assertThat(solver.checkSat().isUnsat()).isTrue();
+
+    Proof[] proof = solver.getProof();
+
+    System.out.println("Proof length: " + proof.length);
+
+    // Process each proof in the array
+    for (int i = 0; i < proof.length; i++) {
+      processProof(proof[i], 0, i + 1);
+    }
+  }
+
+  @Test
+  public void cvc5ProofToStringTest() {
+    // (declare-fun q1 () Bool)
+    // (declare-fun q2 () Bool)
+    // (assert (or (not q1) q2))
+    // (assert q1)
+    // (assert (not q2))
+    Sort booleanSort = tm.getBooleanSort();
+    Term q1 = solver.declareFun("q1", new Sort[] {}, booleanSort);
+    Term q2 = solver.declareFun("q2", new Sort[] {}, booleanSort);
+
+    solver.assertFormula(tm.mkTerm(Kind.OR, (tm.mkTerm(Kind.NOT, q1)), q2));
+    solver.assertFormula(q1);
+    solver.assertFormula(tm.mkTerm(Kind.NOT, q2));
+
+    assertThat(solver.checkSat().isUnsat()).isTrue();
+
+    Proof[] proof = solver.getProof();
+
+    System.out.println("Proof length: " + proof.length);
+
+    for (Proof p : proof) {
+      System.out.println(solver.proofToString(p));
+    }
+  }
 }
