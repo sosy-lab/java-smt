818fe70d687ae20c56d37acd7b899ff789f18bbd
Gabriel Carpio <gabriel.carpio.99@gmail.com>
2025-03-21 19:45:53 +0100

    Class `ProofRuleRegistry` now allows to get a proof rule from any solver by the name of the rule thorugh `fromName` in `ProofRule`

--- src/org/sosy_lab/java_smt/api/proofs/ProofRule.java
+++ src/org/sosy_lab/java_smt/api/proofs/ProofRule.java
@@ -10,20 +10,30 @@
 
 package org.sosy_lab.java_smt.api.proofs;
 
-/**
- * A proof rule from a given proof format.
- */
+import org.sosy_lab.java_smt.ProofRuleRegistry;
+
+/** A proof rule from a given proof format. */
 public interface ProofRule {
 
-  /**
-   * Get the name of the proof rule.
-   */
+  /** Get the name of the proof rule. */
   String getName();
 
-  /**
-   * Get the formula of the proof rule.
-   */
+  /** Get the formula of the proof rule. */
   default String getFormula() {
     return "no formula available";
+  }
+
+  /**
+   * Get a proof rule from its name.
+   *
+   * @return A proof rule.
+   */
+  static <T extends ProofRule> T fromName(Class<T> clazz, String name) {
+    ProofRule rule = ProofRuleRegistry.fromName(clazz, name);
+    if (rule == null) {
+      throw new IllegalArgumentException(
+          "Unknown proof rule in " + clazz.getSimpleName() + ": " + name);
+    }
+    return clazz.cast(rule);
   }
 }
