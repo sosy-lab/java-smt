f929bdc9a92360db958ad3c8459c800462d6c26b
Karlheinz Friedberger <kfriedberger@gmail.com>
2025-03-28 23:17:36 +0100

    CVC5: update documentation.

--- src/org/sosy_lab/java_smt/solvers/cvc5/CVC5SolverContext.java
+++ src/org/sosy_lab/java_smt/solvers/cvc5/CVC5SolverContext.java
@@ -207,8 +207,6 @@ public final class CVC5SolverContext extends AbstractSolverContext {
   public void close() {
     if (creator != null) {
       closed = true;
-      // Don't use Context.deletePointers(); as it deletes statically information from all
-      // existing contexts, not only this one!
       termManager.deletePointer();
       solver.deletePointer();
       creator = null;
