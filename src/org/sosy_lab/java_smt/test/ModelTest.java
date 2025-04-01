cbddd8b22fcfd6b2b40429dd57e3e4b85de22fa8
Daniel Raffler <daniel-raffler@gmx.de>
2025-04-01 12:14:51 +0200

    SmtInterpol: Disable arrayTest5() as the solvers is too slow

--- src/org/sosy_lab/java_smt/test/ModelTest.java
+++ src/org/sosy_lab/java_smt/test/ModelTest.java
@@ -2241,7 +2241,7 @@ public class ModelTest extends SolverBasedTest0.ParameterizedSolverBasedTest0 {
     assume()
         .withMessage("Solver is quite slow for this example")
         .that(solverToUse())
-        .isNotEqualTo(Solvers.PRINCESS);
+        .isNoneOf(Solvers.PRINCESS, Solvers.SMTINTERPOL);
 
     BooleanFormula formula =
         context
