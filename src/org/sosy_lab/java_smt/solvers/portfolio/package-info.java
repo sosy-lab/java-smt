30480552491371a072f14dc114fdbb9477055d75
BaierD <daniel.baier@sosy.ifi.lmu.de>
2025-05-15 19:27:46 +0200

    Add Portfolio package and info

---
+++ src/org/sosy_lab/java_smt/solvers/portfolio/package-info.java
@@ -0,0 +1,14 @@
+// This file is part of JavaSMT,
+// an API wrapper for a collection of SMT solvers:
+// https://github.com/sosy-lab/java-smt
+//
+// SPDX-FileCopyrightText: 2025 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+/** Interface for the SMT solver portfolio. */
+@com.google.errorprone.annotations.CheckReturnValue
+@javax.annotation.ParametersAreNonnullByDefault
+@org.sosy_lab.common.annotations.FieldsAreNonnullByDefault
+@org.sosy_lab.common.annotations.ReturnValuesAreNonnullByDefault
+package org.sosy_lab.java_smt.solvers.portfolio;
