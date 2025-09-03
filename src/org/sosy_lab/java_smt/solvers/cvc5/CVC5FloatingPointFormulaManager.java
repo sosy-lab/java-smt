06fb659eca7c8618902f73e013514f78c72205b6
BaierD <daniel.baier@sosy.ifi.lmu.de>
2025-09-03 15:55:14 +0200

    Change exception name in fromIeeeBitvectorImpl() again because of checkstyle :D

--- src/org/sosy_lab/java_smt/solvers/cvc5/CVC5FloatingPointFormulaManager.java
+++ src/org/sosy_lab/java_smt/solvers/cvc5/CVC5FloatingPointFormulaManager.java
@@ -416,9 +416,9 @@ public class CVC5FloatingPointFormulaManager
               pTargetType.getExponentSize(),
               pTargetType.getMantissaSize() + 1), // add sign bit
           pBitvector);
-    } catch (CVC5ApiException pCVC5ApiException) {
+    } catch (CVC5ApiException cvc5ApiException) {
       // This seems to only be thrown for wrong exponent and mantissa sizes
-      throw new RuntimeException(pCVC5ApiException);
+      throw new RuntimeException(cvc5ApiException);
     }
   }
 
