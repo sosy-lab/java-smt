df00d8d40d572d0517a5d7983008993b3715baca
BaierD <daniel.baier@sosy.ifi.lmu.de>
2025-10-28 16:52:25 +0100

    CVC5 Model: add improved array evaluation from Daniel Raffler with minor modifications from 0d463c83410a332ed81ff4f7628d25ae729b3782

--- src/org/sosy_lab/java_smt/solvers/cvc5/CVC5Model.java
+++ src/org/sosy_lab/java_smt/solvers/cvc5/CVC5Model.java
@@ -9,6 +9,7 @@
 package org.sosy_lab.java_smt.solvers.cvc5;
 
 import com.google.common.base.Preconditions;
+import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import io.github.cvc5.CVC5ApiException;
@@ -21,6 +22,7 @@ import java.util.Collection;
 import org.sosy_lab.java_smt.api.BooleanFormula;
 import org.sosy_lab.java_smt.api.Formula;
 import org.sosy_lab.java_smt.api.FormulaManager;
+import org.sosy_lab.java_smt.basicimpl.AbstractEvaluator;
 import org.sosy_lab.java_smt.basicimpl.AbstractModel;
 
 public class CVC5Model extends AbstractModel<Term, Sort, TermManager> {
@@ -81,7 +83,7 @@ public class CVC5Model extends AbstractModel<Term, Sort, TermManager> {
         // Vars and UFs, as well as bound vars in UFs!
         // In CVC5 consts are variables! Free variables (in CVC5s notation, we call them bound
         // variables, created with mkVar() can never have a value!)
-        builder.add(getAssignment(expr));
+        builder.addAll(getAssignment(expr));
       } else if (kind == Kind.FORALL || kind == Kind.EXISTS) {
         // Body of the quantifier, with bound vars!
         Term body = expr.getChild(1);
@@ -169,7 +171,97 @@ public class CVC5Model extends AbstractModel<Term, Sort, TermManager> {
         keyFormula, valueFormula, equation, nameStr, value, argumentInterpretationBuilder.build());
   }
 
-  private ValueAssignment getAssignment(Term pKeyTerm) {
+  /** Takes a (nested) select statement and returns its indices. */
+  private Iterable<Term> getArgs(Term array) throws CVC5ApiException {
+    if (array.getKind().equals(Kind.SELECT)) {
+      return FluentIterable.concat(getArgs(array.getChild(0)), ImmutableList.of(array.getChild(1)));
+    } else {
+      return ImmutableList.of();
+    }
+  }
+
+  /** Takes a select statement with multiple indices and returns the variable name at the bottom. */
+  private String getVar(Term array) throws CVC5ApiException {
+    if (array.getKind().equals(Kind.SELECT)) {
+      return getVar(array.getChild(0));
+    } else {
+      return array.getSymbol();
+    }
+  }
+
+  /** Build assignment for an array value. */
+  private Iterable<ValueAssignment> buildArrayAssignment(Term expr, Term value) {
+    // CVC5 returns values such as "(Store (Store ... i1,1 e1,1) i1,0 e1,0)" where the i1,x match
+    // the first index of the array and the elements e1,Y can again be arrays (if there is more
+    // than one index). We need "pattern match" this values to extract assignments from it.
+    // Initially we have:
+    //  arr = (Store (Store ... i1,1 e1,1) i1,0 e1,0)
+    // where 'arr' is the name of the variable. By applying (Select i1,0 ...) to both side we get:
+    // (Select i1,0 arr) = (Select i1,0 (Store (Store ... i1,1 e1,1) i1,0 e1,0))
+    // The right side simplifies to e1,0 as the index matches. We need to continue with this for
+    // all other matches to the first index, that is
+    //  (Select i1,1 arr) = (Select i1,0 (Store (Store ... i1,1 e1,1) i1,0 e1,0))
+    //                    = (Select i1,0 (Store ... i1,1 e1,1))
+    //                    = e1,1
+    // until all matches are explored and the bottom of the Store chain is reached. If the array
+    // has more than one dimension we also have to descend into the elements to apply the next
+    // index there. For instance, let's consider a 2-dimensional array with type (Array Int ->
+    // (Array Int -> Int)). After matching the first index just as in the above example we might
+    // have:
+    //  (Select i1,0 arr) = (Select i1,0 (Store (Store ... i1,1 e1,1) i1,0 e1,0)) = e1,0
+    // In this case e1,0 is again a Store chain, let's say e1,0 := (Store (Store ... i2,1 e2,1)
+    // i2,0 e2,0), and we now continue with the second index:
+    //  (Select i2,1 (Select i1,0 arr)) = (Select i2,1 (Store (Store ... i2,1 e2,1) i2,0 e2,0)) =
+    //                                  = e2,1
+    // This again has to be done for all possible matches.
+    // Once we've reached the last index, the successor element will be a non-array value. We
+    // then create the final assignments and return:
+    //  (Select iK,mK ... (Select i2,1 (Select i1,0 arr)) = eik,mK
+    try {
+      if (value.getKind().equals(Kind.STORE)) {
+        // This is a Store node for the current index. We need to follow the chain downwards to
+        // match this index, while also exploring the successor for the other indices
+        Term index = value.getChild(1);
+        Term element = value.getChild(2);
+
+        Term select = creator.getEnv().mkTerm(Kind.SELECT, expr, index);
+
+        Iterable<ValueAssignment> current;
+        if (expr.getSort().getArrayElementSort().isArray()) {
+          current = buildArrayAssignment(select, element);
+        } else {
+          Term equation = creator.getEnv().mkTerm(Kind.EQUAL, select, element);
+
+          current =
+              FluentIterable.of(
+                  new ValueAssignment(
+                      creator.encapsulate(creator.getFormulaType(element), select),
+                      creator.encapsulate(creator.getFormulaType(element), element),
+                      creator.encapsulateBoolean(equation),
+                      getVar(expr),
+                      creator.convertValue(element, element),
+                      FluentIterable.from(getArgs(select))
+                          .transform(this::evaluateImpl)
+                          .toList()));
+        }
+        return FluentIterable.concat(current, buildArrayAssignment(expr, value.getChild(0)));
+
+      } else if (value.getKind().equals(Kind.CONST_ARRAY)) {
+        // We've reached the end of the Store chain
+        return ImmutableList.of();
+
+      } else {
+        // Should be unreachable
+        // We assume that array values are made up of "const" and "store" nodes with non-array
+        // constants as leaves
+        throw new IllegalArgumentException();
+      }
+    } catch (CVC5ApiException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private Iterable<ValueAssignment> getAssignment(Term pKeyTerm) {
     ImmutableList.Builder<Object> argumentInterpretationBuilder = ImmutableList.builder();
     for (int i = 0; i < pKeyTerm.getNumChildren(); i++) {
       try {
@@ -193,13 +285,23 @@ public class CVC5Model extends AbstractModel<Term, Sort, TermManager> {
     }
 
     Term valueTerm = solver.getValue(pKeyTerm);
-    Formula keyFormula = creator.encapsulateWithTypeOf(pKeyTerm);
-    Formula valueFormula = creator.encapsulateWithTypeOf(valueTerm);
-    BooleanFormula equation =
-        creator.encapsulateBoolean(termManager.mkTerm(Kind.EQUAL, pKeyTerm, valueTerm));
-    Object value = creator.convertValue(pKeyTerm, valueTerm);
-    return new ValueAssignment(
-        keyFormula, valueFormula, equation, nameStr, value, argumentInterpretationBuilder.build());
+    if (valueTerm.getSort().isArray()) {
+      return buildArrayAssignment(pKeyTerm, valueTerm);
+    } else {
+      Formula keyFormula = creator.encapsulateWithTypeOf(pKeyTerm);
+      Formula valueFormula = creator.encapsulateWithTypeOf(valueTerm);
+      BooleanFormula equation =
+          creator.encapsulateBoolean(termManager.mkTerm(Kind.EQUAL, pKeyTerm, valueTerm));
+      Object value = creator.convertValue(pKeyTerm, valueTerm);
+      return ImmutableList.of(
+          new ValueAssignment(
+              keyFormula,
+              valueFormula,
+              equation,
+              nameStr,
+              value,
+              argumentInterpretationBuilder.build()));
+    }
   }
 
   @Override
