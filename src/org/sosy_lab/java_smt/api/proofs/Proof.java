d7f226a7647db0162c0c2ac71d6cdbb7610de3e7
Gabriel Carpio <gabriel.carpio.99@gmail.com>
2025-12-20 09:05:25 +0100

    Added interfaces for handling general proofs of unsatisfiability: `Proof`, `ProofRule`.

---
+++ src/org/sosy_lab/java_smt/api/proofs/Proof.java
@@ -0,0 +1,37 @@
+/*
+ * This file is part of JavaSMT,
+ * an API wrapper for a collection of SMT solvers:
+ * https://github.com/sosy-lab/java-smt
+ *
+ * SPDX-FileCopyrightText: 2024 Dirk Beyer <https://www.sosy-lab.org>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.sosy_lab.java_smt.api.proofs;
+
+import java.util.Optional;
+import java.util.Set;
+import org.sosy_lab.java_smt.api.Formula;
+
+/**
+ * A proof node in the proof DAG of a proof.
+ *
+ * @author Gabriel Carpio
+ */
+public interface Proof {
+
+  /** Get the children of the proof node. */
+  Set<Proof> getChildren();
+
+  boolean isLeaf();
+
+  /**
+   * Get the formula of the proof node.
+   *
+   * @return The formula of the proof node.
+   */
+  Optional<Formula> getFormula();
+
+  ProofRule getRule();
+}
