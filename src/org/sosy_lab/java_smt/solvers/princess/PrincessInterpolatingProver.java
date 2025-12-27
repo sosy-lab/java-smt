7262b51b6d60406e5db31bab2b6550474321e112
Daniel Raffler <daniel-raffler@gmx.de>
2025-12-27 12:50:13 +0100

    Princess: Rewrite interpolants to work around some issues in Princess

--- src/org/sosy_lab/java_smt/solvers/princess/PrincessInterpolatingProver.java
+++ src/org/sosy_lab/java_smt/solvers/princess/PrincessInterpolatingProver.java
@@ -14,9 +14,26 @@ import static scala.collection.JavaConverters.asJava;
 import static scala.collection.JavaConverters.asScala;
 
 import ap.api.SimpleAPI;
+import ap.basetypes.IdealInt;
 import ap.basetypes.Tree;
+import ap.parser.BooleanCompactifier;
 import ap.parser.IBoolLit;
+import ap.parser.IEquation;
+import ap.parser.IExpression;
 import ap.parser.IFormula;
+import ap.parser.IFunApp;
+import ap.parser.IIntFormula;
+import ap.parser.IIntLit;
+import ap.parser.IIntRelation;
+import ap.parser.IPlus;
+import ap.parser.ISortedQuantified;
+import ap.parser.ISortedVariable;
+import ap.parser.ITimes;
+import ap.parser.PartialEvaluator;
+import ap.parser.Rewriter;
+import ap.theories.bitvectors.ModuloArithmetic;
+import ap.types.Sort;
+import ap.types.Sort.Integer$;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
@@ -118,10 +135,112 @@ class PrincessInterpolatingProver extends PrincessAbstractProver<Integer>
         : "There should be (n-1) interpolants for n partitions";
 
     // convert data-structure back
-    // TODO check that interpolants do not contain abbreviations we did not introduce ourselves
     final List<BooleanFormula> result = new ArrayList<>();
     for (final IFormula itp : asJava(itps)) {
-      result.add(mgr.encapsulateBooleanFormula(itp));
+      var simplified = PartialEvaluator.apply(BooleanCompactifier.apply(itp));
+      var unsigned =
+          Rewriter.rewrite(
+              simplified,
+              term -> {
+                if (term instanceof IFunApp) {
+                  // Rewrite casts to signed bitvectors as casts to unsigned bitvectors
+                  var app = (IFunApp) term;
+                  if (app.fun().name().equals("mod_cast")) {
+                    var par1 = (IIntLit) app.apply(0);
+                    var par2 = (IIntLit) app.apply(1);
+                    if (par1.value().$less(IdealInt.ZERO())) {
+                      var diff = par2.value().$minus(par1.value());
+                      return new IFunApp(
+                          app.fun(),
+                          PrincessEnvironment.toITermSeq(
+                              IExpression.i(0), IExpression.i(diff), app.apply(2)));
+                    }
+                  }
+                }
+                return term;
+              });
+      var quantifiedVariableFix =
+          Rewriter.rewrite(
+              unsigned,
+              term -> {
+                // Rewrite quantifiers with missing variable sorts
+                if (term instanceof ISortedQuantified) {
+                  var quantified = (ISortedQuantified) term;
+                  if (quantified.sort().name().equals("any")) {
+                    return new ISortedQuantified(
+                        quantified.quan(), Integer$.MODULE$, quantified.subformula());
+                  }
+                }
+                if (term instanceof ISortedVariable) {
+                  var variable = (ISortedVariable) term;
+                  if (variable.sort().name().equals("any")) {
+                    return new ISortedVariable(variable.index(), Integer$.MODULE$);
+                  }
+                }
+                return term;
+              });
+      var selectBugFixed =
+          Rewriter.rewrite(
+              quantifiedVariableFix,
+              term -> {
+                // Add missing conversions for select(arr, idx) if the arrays maps to bitvectors
+                if (term instanceof ITimes) {
+                  var times = (ITimes) term;
+                  if (!PrincessEnvironment.getFormulaType(times.subterm()).isIntegerType()) {
+                    return new ITimes(times.coeff(), ModuloArithmetic.cast2Int(times.subterm()));
+                  }
+                }
+                if (term instanceof IPlus) {
+                  var plus = (IPlus) term;
+                  var t1 = plus.t1();
+                  if (!PrincessEnvironment.getFormulaType(t1).isIntegerType()) {
+                    t1 = ModuloArithmetic.cast2Int(t1);
+                  }
+                  var t2 = plus.t2();
+                  if (!PrincessEnvironment.getFormulaType(t2).isIntegerType()) {
+                    t2 = ModuloArithmetic.cast2Int(t2);
+                  }
+                  if (t1 != plus.t1() || t2 != plus.t2()) {
+                    return new IPlus(t1, t2);
+                  }
+                }
+                if (term instanceof IEquation) {
+                  var eq = (IEquation) term;
+                  var sort1 = Sort.sortOf(eq.left());
+                  var sort2 = Sort.sortOf(eq.right());
+                  if (sort1.equals(Integer$.MODULE$) && !sort2.equals(Integer$.MODULE$)) {
+                    return new IEquation(eq.left(), ModuloArithmetic.cast2Int(eq.right()));
+                  }
+                  if (!sort1.equals(Integer$.MODULE$) && sort2.equals(Integer$.MODULE$)) {
+                    return new IEquation(ModuloArithmetic.cast2Int(eq.left()), eq.right());
+                  }
+                }
+                if (term instanceof IIntFormula) {
+                  var formula = (IIntFormula) term;
+                  if (formula.rel().equals(IIntRelation.GeqZero())
+                      || formula.rel().equals(IIntRelation.EqZero())) {
+                    if (!Sort.sortOf(formula.t()).equals(Integer$.MODULE$)) {
+                      return new IIntFormula(formula.rel(), ModuloArithmetic.cast2Int(formula.t()));
+                    }
+                  }
+                }
+                return term;
+              });
+      var eqZero =
+          Rewriter.rewrite(
+              selectBugFixed,
+              term -> {
+                // Rewrite (= term 0) as (=0 term)
+                if (term instanceof IEquation) {
+                  var equation = (IEquation) term;
+                  if (equation.right() instanceof IIntLit
+                      && ((IIntLit) equation.right()).value().isZero()) {
+                    return IExpression.eqZero(equation.left());
+                  }
+                }
+                return term;
+              });
+      result.add(mgr.encapsulateBooleanFormula(eqZero));
     }
     return result;
   }
