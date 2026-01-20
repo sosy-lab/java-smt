ab4e4851dd3ba37c3a4026935caa17cdf4f12024
gcarpio21 <gabriel.carpio.99@gmail.com>
2026-01-20 15:30:09 +0000

    Proofs: added `Z3_WITH_INTERPOLATION` to the solvers to exclude during testing.

--- src/org/sosy_lab/java_smt/test/SolverBasedTest0.java
+++ src/org/sosy_lab/java_smt/test/SolverBasedTest0.java
@@ -439,6 +439,7 @@ public abstract class SolverBasedTest0 {
             Solvers.CVC4,
             Solvers.PRINCESS,
             Solvers.MATHSAT5,
+            Solvers.Z3_WITH_INTERPOLATION,
             Solvers.Z3,
             Solvers.OPENSMT,
             Solvers.SMTINTERPOL);
