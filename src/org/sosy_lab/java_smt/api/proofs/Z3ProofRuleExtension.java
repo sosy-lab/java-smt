c71137b157bb9b568d5569b22a9a907f2c93d9bb
gcarpio21 <gabriel.carpio.99@gmail.com>
2026-02-24 17:12:44 +0000

    Proofs: javadoc.

--- src/org/sosy_lab/java_smt/api/proofs/Z3ProofRuleExtension.java
+++ src/org/sosy_lab/java_smt/api/proofs/Z3ProofRuleExtension.java
@@ -17,21 +17,21 @@ import com.google.common.collect.ImmutableList;
  * proof rules include optional "parameters", these add information about the proof rule. One of
  * them is the TH_LEMMA rule:
  *
- *  - Z3_OP_PR_TH_LEMMA: Generic proof for theory lemmas. The theory lemma function comes with one
- *    or more parameters. The first parameter indicates the name of the theory. For the theory of
- *    arithmetic, additional parameters provide hints for checking the theory lemma. The hints for
- *    arithmetic are:
+ * <p>- Z3_OP_PR_TH_LEMMA: Generic proof for theory lemmas. The theory lemma function comes with one
+ * or more parameters. The first parameter indicates the name of the theory. For the theory of
+ * arithmetic, additional parameters provide hints for checking the theory lemma. The hints for
+ * arithmetic are:
  *
- *      - farkas - followed by rational coefficients. Multiply the coefficients to the inequalities in
- *      the lemma, add the (negated) inequalities and obtain a contradiction.
+ * <p>- farkas - followed by rational coefficients. Multiply the coefficients to the inequalities in
+ * the lemma, add the (negated) inequalities and obtain a contradiction.
  *
- *      - triangle-eq - Indicates a lemma related to the equivalence:
+ * <p>- triangle-eq - Indicates a lemma related to the equivalence:
  *
- *        (iff (= t1 t2) (and (<= t1 t2) (<= t2 t1)))
+ * <p> {@code (iff (= t1 t2) (and (<= t1 t2) (<= t2 t1)))}
  *
- *      - gcd-test - Indicates an integer linear arithmetic lemma that uses a gcd test.
+ * <p>- gcd-test - Indicates an integer linear arithmetic lemma that uses a gcd test.
  *
- * As other solvers do not work in this way, an extension specifically for Z3 is provided.
+ * <p>As other solvers do not work in this way, an extension specifically for Z3 is provided.
  */
 public interface Z3ProofRuleExtension {
   ImmutableList<Z3ProofRuleParameter<?>> getParameters();
