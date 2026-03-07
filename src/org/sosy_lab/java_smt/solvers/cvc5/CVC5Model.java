be4d4bf06e4ffcd59f5ba280fe5019a41a1e6012
Karlheinz Friedberger <kfriedberger@gmail.com>
2026-03-07 23:14:18 +0100

    chore: code improvements by using proper collection constructors.

--- src/org/sosy_lab/java_smt/solvers/cvc5/CVC5Model.java
+++ src/org/sosy_lab/java_smt/solvers/cvc5/CVC5Model.java
@@ -56,8 +56,7 @@ public class CVC5Model extends AbstractModel<Term, Sort, TermManager> {
   private Set<Term> collectModelTerms(Collection<Term> asserted) {
     ImmutableSet.Builder<Term> builder = ImmutableSet.builder();
     var cache = new HashSet<Term>();
-    var work = new ArrayDeque<Term>();
-    work.addAll(asserted);
+    var work = new ArrayDeque<>(asserted);
     while (!work.isEmpty()) {
       var term = work.pop();
       if (cache.add(term)) {
