685ecfd4e74b29863440794fdec754e285399c60
gcarpio21 <gabriel.carpio.99@gmail.com>
2026-03-14 04:12:13 +0000

    Cmdline: Added package info to `cmdline`.

---
+++ src/org/sosy_lab/java_smt/cmdline/package-info.java
@@ -0,0 +1,14 @@
+/*
+ * This file is part of JavaSMT,
+ * an API wrapper for a collection of SMT solvers:
+ * https://github.com/sosy-lab/java-smt
+ *
+ * SPDX-FileCopyrightText: 2026 Dirk Beyer <https://www.sosy-lab.org>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * The frontend of JavaSMT for using it as a standalone application on the command line.
+ */
+package org.sosy_lab.java_smt.cmdline;
