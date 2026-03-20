2190f46e30a78261c5e9f818c7dcdc585aad5c61
BaierD <daniel.baier@sosy.ifi.lmu.de>
2026-03-20 14:51:52 +0100

    Add check to parse() to return a proper error message in case only 1 formula is returned

--- src/org/sosy_lab/java_smt/api/FormulaManager.java
+++ src/org/sosy_lab/java_smt/api/FormulaManager.java
@@ -193,6 +193,9 @@ public interface FormulaManager {
   default BooleanFormula parse(String s) throws IllegalArgumentException {
     List<BooleanFormula> formulas = parseAll(s);
     checkArgument(!formulas.isEmpty(), "No assertion found in the SMTLIB string.");
+    checkArgument(
+        formulas.size() == 1,
+        "More than one assertion found in the SMT-LIB string, " + "please use parseAll(String).");
     return Objects.requireNonNull(Iterables.getOnlyElement(formulas));
   }
 
