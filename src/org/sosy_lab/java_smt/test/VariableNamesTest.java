ce363b6988aa33020bff7390d0c33a735577417e
BaierD <daniel.baier@sosy.ifi.lmu.de>
2026-03-23 11:42:38 +0100

    Disable a VariableNamesTest for Boolector, as it returns an empty string

--- src/org/sosy_lab/java_smt/test/VariableNamesTest.java
+++ src/org/sosy_lab/java_smt/test/VariableNamesTest.java
@@ -541,6 +541,11 @@ public class VariableNamesTest extends SolverBasedTest0.ParameterizedSolverBased
   @Test
   public void testEqBoolVariableDump() {
     // FIXME: Rewrite test? Most solvers will simplify the formula to `true`.
+
+    // What is happening internally is that the input is returned as "(= java-smt java-smt)",
+    // which is thrown away by our sanitization due to no assertion
+    assume().withMessage("Boolector simplifies the input so much that it gives out no "
+        + "assertions").that(solverToUse()).isNotEqualTo(Solvers.BOOLECTOR);
     for (String name : getAllNames()) {
       BooleanFormula var = createVariableWith(bmgr::makeVariable, name);
       if (var != null) {
