a8eead251761d9997676b20502e2903ff5e7824a
Daniel Raffler <daniel-raffler@gmx.de>
2026-04-03 14:40:39 +0200

    CVC4: Never free memory to work around the memory management issues in the CVC4 java bindings

--- src/org/sosy_lab/java_smt/solvers/cvc4/CVC4TheoremProver.java
+++ src/org/sosy_lab/java_smt/solvers/cvc4/CVC4TheoremProver.java
@@ -37,6 +37,10 @@ import org.sosy_lab.java_smt.basicimpl.ShutdownHook;
 class CVC4TheoremProver extends AbstractProverWithAllSat<Void>
     implements ProverEnvironment, BasicProverEnvironment<Void> {
 
+  // Keep a global list of all provers to prevent the garbge collector from doing its job
+  // See https://github.com/sosy-lab/java-smt/issues/169
+  private static List<CVC4TheoremProver> provers = new ArrayList<>();
+
   private final CVC4FormulaCreator creator;
   private final int randomSeed;
   SmtEngine smtEngine; // final except for SL theory
@@ -63,6 +67,10 @@ class CVC4TheoremProver extends AbstractProverWithAllSat<Void>
       Set<ProverOptions> pOptions,
       BooleanFormulaManager pBmgr) {
     super(pOptions, pBmgr, pShutdownNotifier);
+
+    synchronized (CVC4TheoremProver.class) {
+      provers.add(this);
+    }
 
     creator = pFormulaCreator;
     randomSeed = pRandomSeed;
@@ -237,9 +245,7 @@ class CVC4TheoremProver extends AbstractProverWithAllSat<Void>
   @Override
   public void close() {
     if (!closed) {
-      exportMapping.delete();
-      // smtEngine.delete();
-      exprManager.delete();
+      // Never release a prover
     }
     super.close();
   }
