c1daa59b5eb9631cfe623f873a1312fdd48683cc
BaierD <daniel.baier@sosy.ifi.lmu.de>
2026-04-07 14:32:40 +0200

    Add suppression for PackageSanityTest and TODO

--- src/org/sosy_lab/java_smt/basicimpl/PackageSanityTest.java
+++ src/org/sosy_lab/java_smt/basicimpl/PackageSanityTest.java
@@ -16,13 +16,17 @@ import org.sosy_lab.java_smt.SolverContextFactory;
 import org.sosy_lab.java_smt.SolverContextFactory.Solvers;
 import org.sosy_lab.java_smt.api.FormulaType;
 
+@SuppressWarnings("resource")
 public class PackageSanityTest extends AbstractPackageSanityTests {
 
   {
     setDistinctValues(FormulaType.class, FormulaType.BooleanType, FormulaType.IntegerType);
     setDefault(ShutdownNotifier.class, ShutdownManager.create().getNotifier());
     try {
+      // Due to solver independent interpolation we need a default solver for AbstractSolverContext
       // Use Princess as it is always available
+      // TODO: look into this some more. Can we get rid of this? What is going on with the
+      //  ressource suppression?
       setDefault(
           AbstractSolverContext.class,
           (AbstractSolverContext) SolverContextFactory.createSolverContext(Solvers.PRINCESS));
