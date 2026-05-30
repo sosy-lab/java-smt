b5ca507ea344b04c2710da7ec0ed0d4c7397a7c6
Daniel Raffler <daniel-raffler@gmx.de>
2026-05-30 11:36:19 +0200

    Add support for other platforms in Ivy example project

--- doc/Example-Ivy-Project/src/test/org/sosy_lab/java_smt_example/SudokuTest.java
+++ doc/Example-Ivy-Project/src/test/org/sosy_lab/java_smt_example/SudokuTest.java
@@ -10,12 +10,14 @@ package org.sosy_lab.java_smt_example;
 
 import com.google.common.base.Joiner;
 import com.google.common.base.Splitter;
+import com.google.common.base.StandardSystemProperty;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.Parameter;
 import org.junit.jupiter.params.ParameterizedClass;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.sosy_lab.common.NativeLibraries;
 import org.sosy_lab.common.ShutdownNotifier;
 import org.sosy_lab.common.configuration.Configuration;
 import org.sosy_lab.common.configuration.InvalidConfigurationException;
@@ -30,6 +32,7 @@ import org.sosy_lab.java_smt.example.Sudoku.SudokuSolver;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.logging.Level;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -86,19 +89,16 @@ public class SudokuTest {
   @Parameter(0)
   public Solvers solver;
 
-  private static final String OS = System.getProperty("os.name").toLowerCase().replace(" ", "");
-  private static final boolean IS_LINUX = OS.startsWith("linux");
-  private static final boolean IS_X64 = System.getProperty("os.arch").contains("64");
+  private static final String OS =
+          StandardSystemProperty.OS_NAME.value().toLowerCase(Locale.getDefault()).replace(" ", "");
+  private static final String ARCH =
+          StandardSystemProperty.OS_ARCH.value().toLowerCase(Locale.getDefault()).replace(" ", "");
 
-  /**
-   * Disable some checks on certain combinations of operating systems and solvers, because of missing dependencies.
-   */
-  private static boolean isOperatingSystemSupported(Solvers solver) {
-    return switch (solver) {
-      case SMTINTERPOL, PRINCESS -> true; // Java-based solvers should work on all platforms
-      default -> IS_LINUX && IS_X64; // this example only includes Linux x64 binaries for native solvers
-    };
-  }
+  protected static final boolean IS_WINDOWS = OS.startsWith("windows");
+  private static final boolean IS_MAC = OS.startsWith("macos");
+  private static final boolean IS_LINUX = OS.startsWith("linux");
+
+  private static final boolean IS_ARCH_ARM64 = ARCH.equals("aarch64");
 
   private Configuration config;
   private LogManager logger;
@@ -147,10 +147,52 @@ public class SudokuTest {
     }
   }
 
+  private boolean isSufficientVersionOfLibcxx(String library) {
+    try {
+      NativeLibraries.loadLibrary(library);
+    } catch (UnsatisfiedLinkError e) {
+      for (String dependency : getRequiredLibcxx(library)) {
+        if (e.getMessage().contains("version `" + dependency + "' not found")) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  private String[] getRequiredLibcxx(String library) {
+    return switch (library) {
+      case "z3" -> new String[]{"GLIBC_2.34", "GLIBCXX_3.4.26", "GLIBCXX_3.4.29"};
+      case "bitwuzlaj" -> new String[]{"GLIBC_2.33", "GLIBCXX_3.4.26", "GLIBCXX_3.4.29"};
+      case "opensmtj" -> new String[]{"GLIBC_2.33", "GLIBCXX_3.4.26", "GLIBCXX_3.4.29"};
+      case "mathsat5j" -> new String[]{"GLIBC_2.33", "GLIBC_2.38"};
+      case "cvc5jni" -> new String[]{"GLIBC_2.32"};
+      case "yices2java" -> new String[]{"GLIBC_2.34"};
+      default -> new String[]{};
+    };
+  }
+
+  private boolean isSupportedOperatingSystemAndArchitecture(Solvers solver) {
+    return switch (solver) {
+      case SMTINTERPOL, PRINCESS ->
+        // Any operating system and any architecture is allowed, Java is sufficient
+              true;
+      case BOOLECTOR, CVC4 -> IS_LINUX && !IS_ARCH_ARM64;
+      case YICES2 -> (IS_LINUX && !IS_ARCH_ARM64 && isSufficientVersionOfLibcxx("yices2java"))
+              || (IS_WINDOWS && !IS_ARCH_ARM64);
+      case CVC5 -> (IS_LINUX && isSufficientVersionOfLibcxx("cvc5jni")) || IS_WINDOWS || IS_MAC;
+      case OPENSMT -> IS_LINUX && isSufficientVersionOfLibcxx("opensmtj");
+      case BITWUZLA -> (IS_LINUX && isSufficientVersionOfLibcxx("bitwuzlaj")) || (IS_WINDOWS && !IS_ARCH_ARM64);
+      case MATHSAT5 -> (IS_LINUX && isSufficientVersionOfLibcxx("mathsat5j")) || (IS_WINDOWS && !IS_ARCH_ARM64);
+      case Z3 -> (IS_LINUX && isSufficientVersionOfLibcxx("z3")) || IS_WINDOWS || IS_MAC;
+      case Z3_WITH_INTERPOLATION -> IS_LINUX && !IS_ARCH_ARM64;
+    };
+  }
+
   @Test
   public void checkSudoku()
           throws InvalidConfigurationException, InterruptedException, SolverException {
-    assumeTrue(isOperatingSystemSupported(solver));
+    assumeTrue(isSupportedOperatingSystemAndArchitecture(solver));
 
     logger.log(Level.INFO, "Executing " + solver + "...");
 
